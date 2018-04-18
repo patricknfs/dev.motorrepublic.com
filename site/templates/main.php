0fe7edde883d1c9de315375b76c68996efb63716
Patrick <patrick.ogoeman@nationalfleetservices.net>
2018-04-18 15:53:04 +0100

    Connect foundation javascript

--- site/templates/main.php
+++ site/templates/main.php
@@ -159,29 +159,29 @@ ERROR_REPORTING(E_ALL);
 	</body>
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 	<?php
-  if ($page->id == 1074) {
-    ?>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRbP0cMxwBeFZFe4WCGwzHWYz-w2sPIiA"></script>
-    <script type="text/javascript">
-      function initialize() {
-      var map_canvas = document.getElementById('map_canvas');
-      var myLatlng = new google.maps.LatLng(52.45135748,-2.04321563);
-        var map_options = {
-          center: myLatlng,
-          zoom: 16,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-        var map = new google.maps.Map(map_canvas, map_options);
-        var marker = new google.maps.Marker({
-          position: myLatlng,
-          map: map,
-          title: ''
-        });
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
-    </script>
-    <?php
-  }
+  // if ($page->id == 1074) {
+  //   ?>
+  //   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRbP0cMxwBeFZFe4WCGwzHWYz-w2sPIiA"></script>
+  //   <script type="text/javascript">
+  //     function initialize() {
+  //     var map_canvas = document.getElementById('map_canvas');
+  //     var myLatlng = new google.maps.LatLng(52.45135748,-2.04321563);
+  //       var map_options = {
+  //         center: myLatlng,
+  //         zoom: 16,
+  //         mapTypeId: google.maps.MapTypeId.ROADMAP
+  //       }
+  //       var map = new google.maps.Map(map_canvas, map_options);
+  //       var marker = new google.maps.Marker({
+  //         position: myLatlng,
+  //         map: map,
+  //         title: ''
+  //       });
+  //     }
+  //     google.maps.event.addDomListener(window, 'load', initialize);
+  //   </script>
+  //   <?php
+  // }
   ?>
   <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
