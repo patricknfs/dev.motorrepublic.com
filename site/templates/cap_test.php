0d76c1858f32d57a157e5d6fba74e9f12b352f8f
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-05-25 13:21:01 +0100

    testing api

--- site/templates/cap_test.php
+++ site/templates/cap_test.php
@@ -62,7 +62,7 @@ function get_soap_client(){
 
 try
     {
-        $params = array('justCurrentManufacturers'); //define your parameters here
+        $params = array(); //define your parameters here
         $client = get_soap_client();
         $response = $client->__soapCall('WSDL',$params);
     }
