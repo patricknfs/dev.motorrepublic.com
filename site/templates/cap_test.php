74a3870f794293cf3cde67ab136916547a658cb7
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-05-25 15:20:13 +0100

    test script

--- site/templates/cap_test.php
+++ site/templates/cap_test.php
@@ -41,7 +41,7 @@ function get_soap_client(){
         'uri'=>'http://schemas.xmlsoap.org/soap/envelope/',
         'style'=>SOAP_RPC,
         'use'=>SOAP_ENCODED,
-        'soap_version'=>SOAP_1_1,
+        'soap_version'=>SOAP_1_2,
         'cache_wsdl'=>WSDL_CACHE_NONE,
         'connection_timeout'=>15,
         'trace'=>true,
