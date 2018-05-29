ba71de87e40108405f54283d5afda9e78a43c507
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-05-29 14:05:37 +0100

    Test api credentials

--- site/templates/cap_test.php
+++ site/templates/cap_test.php
@@ -59,9 +59,9 @@ function get_soap_client(){
 try
     {   
         $username = '173210';
-        $params2 = array('subscriberId' => $username ); //define your parameters here
-        $client = get_soap_client();
-        $client->GetCapMan($params2);
+        // $params = array('subscriberId' => $username ); //define your parameters here
+        // $client = get_soap_client();
+        // $client->GetCapMan($params);
         // $client->__getLastResponse();
     }
 
