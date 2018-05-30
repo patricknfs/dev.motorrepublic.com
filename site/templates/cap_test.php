67e723525640ac717f9380bba095f18fceb933dc
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-05-30 15:00:24 +0100

    Test api credentials

--- site/templates/cap_test.php
+++ site/templates/cap_test.php
@@ -60,7 +60,7 @@ try
     {   
         $username = '173210';
         
-        $params = array('justCurrentManufacturers' => false,'subscriberId' => $username, 'database' => 'car' ); //define your parameters here
+        $params = array('justCurrentManufacturers' => true,'subscriberId' => $username, 'database' => 'car' ); //define your parameters here
         $client = get_soap_client();
         $result = $client->GetCapMan($params);
         // return($result);
