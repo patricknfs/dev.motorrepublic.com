462eb75f54dc3decb7f32655b8d5812cc7a2606a
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-06-14 16:03:19 +0100

    checking data feed

--- site/templates/cap_test.php
+++ site/templates/cap_test.php
@@ -87,7 +87,7 @@ try
         // var_dump($result);
         $data = objectToArray($client);
         foreach($data as $item){
-            echo $item[id];
+            echo $item[0];
         }
     }
 
