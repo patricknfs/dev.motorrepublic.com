1706f308190cc5f9b5a09e2443997fed1f6c7c71
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-06-15 15:06:43 +0100

    testing node access

--- site/templates/cap_test.php
+++ site/templates/cap_test.php
@@ -83,11 +83,9 @@ try
         $params = array('justCurrentManufacturers' => true,'subscriberId' => $username, 'password' => $password, 'database' => 'car', 'bodyStyleFilter' => '' ); //define your parameters here
         $client->GetCapMan($params);
         $data = $client->__getLastResponse();
-        $xml = simplexml_load_string($data);
-
-print_r($xml);
+        $xml = simplexml_load_string($client);
+        print_r($xml);
         $marques = new SimpleXMLElement($data);
-        echo $marques->CMan_Name;
         // echo "Response:\n" . $client->__getLastResponse() . "\n";
         // var_dump($data);
         // $data_array = objectToArray($data);
