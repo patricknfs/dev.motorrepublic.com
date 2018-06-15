3728c0eebbbac63d2f9f8804179249750d71d168
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-06-15 17:00:50 +0100

    testing node access

--- site/templates/cap_test.php
+++ site/templates/cap_test.php
@@ -89,7 +89,8 @@ try
         echo "<pre>";
             print_r($xml);
         echo"</pre>";
-        $data   = simplexml_load_string($xml);
+        // $data   = simplexml_load_string($xml);
+        $data = new SimpleXMLElement($string);
         $marques  = $data->diffgram->NewDataSet->Table;
         print "We have " . count($marques) . " rooms: \n";
         foreach($marques as $key => $item){
