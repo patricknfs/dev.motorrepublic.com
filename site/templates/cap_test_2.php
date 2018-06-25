8d10fbf259124bdcc6d76296be641a9083cd25cf
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-06-25 11:31:53 +0100

    alternative codes and description

--- site/templates/cap_test_2.php
+++ site/templates/cap_test_2.php
@@ -1,24 +1,24 @@
 <?php
-function objectToArray($d) {
-    if (is_object($d)) {
-        // Gets the properties of the given object
-        // with get_object_vars function
-        $d = get_object_vars($d);
-    }
+// function objectToArray($d) {
+//     if (is_object($d)) {
+//         // Gets the properties of the given object
+//         // with get_object_vars function
+//         $d = get_object_vars($d);
+//     }
     
-    if (is_array($d)) {
-        /*
-        * Return array converted to object
-        * Using __FUNCTION__ (Magic constant)
-        * for recursive call
-        */
-        return array_map(__FUNCTION__, $d);
-    }
-    else {
-        // Return array
-        return $d;
-    }
-}
+//     if (is_array($d)) {
+//         /*
+//         * Return array converted to object
+//         * Using __FUNCTION__ (Magic constant)
+//         * for recursive call
+//         */
+//         return array_map(__FUNCTION__, $d);
+//     }
+//     else {
+//         // Return array
+//         return $d;
+//     }
+// }
 
 function AddWSSUsernameToken($client, $username, $password)
 {
@@ -55,7 +55,7 @@ function get_soap_client(){
     global  $username, $password;
     $username = '173210';
     $password = 'NfS4Je';
-    $wsdl = 'https://soap.cap.co.uk/Vehicles/CapVehicles.asmx?WSDL';
+    $wsdl = 'https://soap.cap.co.uk/Vehicles/DataDownload_Webservice.asmx?WSDL';
 
     $options = array(
         'uri'=>'http://schemas.xmlsoap.org/soap/envelope/',
@@ -80,46 +80,24 @@ try
         $password = 'NfS4Je';
         
         $client = get_soap_client();
-        $params = array('justCurrentManufacturers' => true,'subscriberId' => $username, 'password' => $password, 'database' => 'car', 'bodyStyleFilter' => '' ); //define your parameters here
+        $params = array('subscriberId' => $username, 'password' => $password); //define your parameters here
         $client->GetCapMan($params);
         $data = $client->__getLastResponse();
         $xml    = str_replace(array("diffgr:","msdata:"),'', trim($data));
-        // Wrap into root element to make it standard XML
-        // $xml    = "<package>" . $xml . "</package>";
-        // echo "<pre>";
-        //     print_r($xml);
-        // echo"</pre>";
+        echo "<pre>";
+            print_r($xml);
+        echo"</pre>";
         // $data   = simplexml_load_string($xml);
         $data = new SimpleXMLElement($xml);
         $marques  = $data->xpath('//Table');
         // print "We have " . count($marques) . " marques: \n";
-        foreach($marques as $item){
-            echo "ManCode: " . $item->CMan_Code . " and Manufacturer: " . $item->CMan_Name . "<br />";
-            $range_params = array('justCurrentRanges' => true,'subscriberId' => $username, 'password' => $password, 'database' => 'car', 'manCode' => $item->CMan_Code, 'bodyStyleFilter' => '' ); //define your parameters here
-            $client->GetCapRange_IncludeOnRunout($range_params);
-            $data_range = $client->__getLastResponse();
-            $xml_range    = str_replace(array("diffgr:","msdata:"),'', trim($data_range));
-            // echo "<pre>";
-            //     print_r($xml_range);
-            // echo"</pre>";
-            $data_range = new SimpleXMLElement($xml_range);
-            $ranges  = $data_range->xpath('//Table');
-            foreach($ranges as $range){
-                echo "RangeCode: " . $range->CRan_Code . " Range: " . $item->CMan_Name . " " . $range->CRan_Name . "<br />";
-                $mod_params = array('justCurrentModels' => true,'subscriberId' => $username, 'password' => $password, 'database' => 'car', 'manRanCodeIsMan' => false, 'manRanCode' => $range->CRan_Code, 'bodyStyleFilter' => '' ); //define your parameters here
-                // $client->GetCapMod_IncludeOnRunout($mod_params);
-                // $data_mod = $client->__getLastResponse();
-                // $xml_mod    = str_replace(array("diffgr:","msdata:"),'', trim($data_mod));
-                // echo "<pre>";
-                //     print_r($xml_mod);
-                // echo"</pre>";
-                // $data_mod = new SimpleXMLElement($xml_mod);
-                // $models  = $data_mod->xpath('//Table');
-                // foreach ($models AS $model){
-                //     echo "ModCode: " . $range->CRan_Code . " Model: " . $item->CMan_Name . " " . $range->CRan_Name . " " . $model->CMod_Name . "<br />";
-                // }
-            }
-        }
+        // foreach($marques as $item){
+        //     echo "ManCode: " . $item->CMan_Code . " and Manufacturer: " . $item->CMan_Name . "<br />";
+        //     $range_params = array('justCurrentRanges' => true,'subscriberId' => $username, 'password' => $password, 'database' => 'car', 'manCode' => $item->CMan_Code, 'bodyStyleFilter' => '' ); //define your parameters here
+        //     $client->GetCapRange_IncludeOnRunout($range_params);
+        //     $data_range = $client->__getLastResponse();
+        //     $xml_range    = str_replace(array("diffgr:","msdata:"),'', trim($data_range));
+        // }
     }
 
     catch(Exception $e){ 
