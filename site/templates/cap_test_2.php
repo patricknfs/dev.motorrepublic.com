c52eee66de1f3e2973133a42562cc8ed4c6015e1
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-06-25 12:45:25 +0100

    alternative codes and description

--- site/templates/cap_test_2.php
+++ site/templates/cap_test_2.php
@@ -89,7 +89,7 @@ try
       echo"</pre>";
       // $data   = simplexml_load_string($data);
       $data = new SimpleXMLElement($data);
-      $products  = $data->xpath('//Products');
+      $products  = $data->xpath('//Product');
       print "We have " . count($products) . " products: \n";
       foreach($products as $item){
         echo "Code: " . $item->ProductID . "<br />";
