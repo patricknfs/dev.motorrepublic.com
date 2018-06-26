9999cbd5d1b8e2b1d4905abf22143774f8d3ca07
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-06-26 15:49:53 +0100

    alternative codes and description

--- site/templates/cap_test_2.php
+++ site/templates/cap_test_2.php
@@ -86,6 +86,7 @@ try
       // echo "Response:\n" . $client->__getLastResponse() . "\n";
       // $xml    = str_replace(array("diffgr:","msdata:"),'', trim($data));
       $data = new SimpleXMLElement($result);
+      print_r($data);
       echo $data->xpath('//Products/Product[0]/ProductName');
       echo "<pre>";
         var_dump($data);
