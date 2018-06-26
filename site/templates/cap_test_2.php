2052878e9b9e54b4e26dd4c03b4c340f416a74b3
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-06-26 13:03:53 +0100

    alternative codes and description

--- site/templates/cap_test_2.php
+++ site/templates/cap_test_2.php
@@ -62,9 +62,9 @@ function get_soap_client(){
         'style'=>SOAP_RPC,
         'use'=>SOAP_ENCODED,
         'soap_version'=>SOAP_1_2,
-        // 'cache_wsdl'=>WSDL_CACHE_NONE,
+        'cache_wsdl'=>WSDL_CACHE_NONE,
         'connection_timeout'=>15,
-        'trace'=>1,
+        'trace'=>true,
         'encoding'=>'UTF-8',
         'exceptions'=>true,
     );
