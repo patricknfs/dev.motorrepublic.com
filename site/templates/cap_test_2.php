6048c198a36f17bf4b26e6f07d0221469196eee6
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-06-26 12:04:29 +0100

    alternative codes and description

--- site/templates/cap_test_2.php
+++ site/templates/cap_test_2.php
@@ -85,7 +85,7 @@ try
       $result = $client->__getLastResponse();
     //   echo "Response:\n" . $client->__getLastResponse() . "\n";
       // $xml    = str_replace(array("diffgr:","msdata:"),'', trim($data));
-      $data = new simplexml_load_string($result);
+      $data = simplexml_load_string($result);
       echo "<pre>";
         print_r($data);
       echo"</pre>";
