49c0df7dd18ec78ca4e8dd4086fd9462727baece
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-06-26 12:56:34 +0100

    alternative codes and description

--- site/templates/cap_test_2.php
+++ site/templates/cap_test_2.php
@@ -55,7 +55,7 @@ function get_soap_client(){
     global  $username, $password;
     $username = '173210';
     $password = 'NfS4Je';
-    $wsdl = 'https://soap.cap.co.uk/DataDownload/DataDownload_Webservice.asmx?WSDL';
+    $wsdl = 'https://soap.cap.co.uk/DataDownload/DataDownload_Webservice.asmx';
 
     $options = array(
         'uri'=>'http://schemas.xmlsoap.org/soap/envelope/',
