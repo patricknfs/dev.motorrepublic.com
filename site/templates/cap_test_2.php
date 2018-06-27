05b627f8d7fbd7d4894a38a2b31439e4808229fe
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-06-27 12:44:33 +0100

    alternative codes and description

--- site/templates/cap_test_2.php
+++ site/templates/cap_test_2.php
@@ -82,13 +82,13 @@ try
       $client = get_soap_client();
       $params = array('SubscriberID' => $username, 'Password' => $password); //define your parameters here
       $client->List_AllSubscriberProducts($params);
-    //   $result = $client->__getLastResponse();
+      // $result = $client->__getLastResponse();
       echo "<pre>";
         var_dump($result);
       echo"</pre>";
       // echo "Response:\n" . $client->__getLastResponse() . "\n";
       // $xml    = str_replace(array("diffgr:","msdata:"),'', trim($data));
-      $data = new SimpleXMLElement($client);
+      $data = new SimpleXMLElement($client->children());
       echo "<pre>";
         var_dump($data->children());
       echo"</pre>";
