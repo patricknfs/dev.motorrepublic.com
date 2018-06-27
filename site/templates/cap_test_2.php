da65d23a3ce29420a506f030b0cc6ea3625bc4eb
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-06-27 14:34:47 +0100

    alternative codes and description

--- site/templates/cap_test_2.php
+++ site/templates/cap_test_2.php
@@ -88,7 +88,7 @@ try
       echo"</pre>";
       $xml = simplexml_load_string($result);
       echo "<pre>";
-        var_dump($xml->products);
+        print_r($xml);
       echo"</pre>";
       foreach($xml->xpath('//products') as $key => $item){
         echo "Code: " . $item->productid . "<br />";
