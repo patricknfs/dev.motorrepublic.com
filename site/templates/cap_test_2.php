3d573341e56d829e39721b8a73782546126384aa
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-06-27 14:57:36 +0100

    alternative codes and description

--- site/templates/cap_test_2.php
+++ site/templates/cap_test_2.php
@@ -87,7 +87,7 @@ try
       echo "<pre>";
         print_r($result);
       echo"</pre>";
-      $xml = simplexml_load_string($result);
+    //   $xml = simplexml_load_string($result);
       libxml_use_internal_errors(true);//load if improperly formatted
       $xml = new DOMDocument();
   if ($xml->load($result))
