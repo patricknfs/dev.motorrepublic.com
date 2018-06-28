1d59c4ae0853ead1319d242070b2fba993745c63
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-06-28 14:06:30 +0100

    read csv to db

--- site/templates/cap_read.php
+++ site/templates/cap_read.php
@@ -22,7 +22,7 @@ if (($handle = fopen("inc/cap_cars.csv", "r")) !== FALSE) {
     // print_r($data);
     $num = count($data);
     $insert = "INSERT INTO `team`.`vehicles` (`cap_code`,`cap_id`,`manufacturer`,`model``description`) VALUES (" . $data[0] . "," . $data[1] . "," . $data[2] . "," . $data[4] . "," . $data[8] . ")";
-    $results = mysqli_query($conn, $insert);
+    $result2 = mysqli_query($conn, $insert);
     $result2->close();
     $row++;
   }
