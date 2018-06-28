24d46a15aefca797239ce439762f8191a727477d
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-06-28 14:23:35 +0100

    read csv to db

--- site/templates/cap_read.php
+++ site/templates/cap_read.php
@@ -21,7 +21,7 @@ if (($handle = fopen("inc/cap_cars.csv", "r")) !== FALSE) {
   while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
     // print_r($data);
     $num = count($data);
-    $insert = "INSERT INTO `team`.`vehicles` (`cap_code`,`cap_id`,`manufacturer`,`model``description`) VALUES ('" . $data[0] . "','" . $data[1] . "','" . $data[2] . "','" . $data[4] . "','" . $data[8] . "')";
+    $insert = "INSERT INTO `team`.`vehicles` (`cap_code`,`cap_id`,`manufacturer`,`model`,`description`) VALUES ('" . $data[0] . "','" . $data[1] . "','" . $data[2] . "','" . $data[4] . "','" . $data[8] . "')";
     echo $insert . "<br />";
     $result2 = mysqli_query($conn, $insert);
     $row++;
