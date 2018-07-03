ffd360f94fa6b1723fb7c1b6578fe22548be7b5e
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-07-03 15:11:46 +0100

    format ald rates read page

--- site/templates/ald_rates_read.php
+++ site/templates/ald_rates_read.php
@@ -34,7 +34,7 @@ if (($handle = fopen($csv , "r")) !== FALSE) {
           WHEN 24 THEN `24_8K_PA_rental` = " . $data[12] . ", `24_8K_PA_service` = " . ($data[12]-$data[11]) . ";
           WHEN 36 THEN `36_8K_PA_rental` = " . $data[12] . ",`36_8K_PA_service` = " . ($data[12]-$data[11]) . ";
           WHEN 48 THEN `48_8K_PA_rental` = " . $data[12] . ", `48_8K_PA_service` = " . ($data[12]-$data[11]) . ";
-          WHEN 60:THEN `60_8K_PA_rental` = " . $data[12] . ",`60_8K_PA_service` = " . ($data[12]-$data[11]);
+          WHEN 60 THEN `60_8K_PA_rental` = " . $data[12] . ",`60_8K_PA_service` = " . ($data[12]-$data[11]);
         END CASE)
         ON DUPLICATE KEY UPDATE
           `cap_id` = " . $data[24] . "
