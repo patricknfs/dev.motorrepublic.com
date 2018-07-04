7f2fd9fe3a8a4efa50772505b624006e3855fa7d
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-07-04 12:42:06 +0100

    Add FormBuilder module

--- site/templates/ald_rates_read.php
+++ site/templates/ald_rates_read.php
@@ -30,15 +30,24 @@ if (($handle = fopen($csv , "r")) !== FALSE) {
         SET
         `cap_id` = 'test',
         `updated` = NOW(),
-        (CASE 
-          WHEN 24 THEN `24_8K_PA_rental` = " . $data[12] . ", `24_8K_PA_service` = " . ($data[12]-$data[11]) . ";
-          WHEN 36 THEN `36_8K_PA_rental` = " . $data[12] . ",`36_8K_PA_service` = " . ($data[12]-$data[11]) . ";
-          WHEN 48 THEN `48_8K_PA_rental` = " . $data[12] . ", `48_8K_PA_service` = " . ($data[12]-$data[11]) . ";
-          WHEN 60 THEN `60_8K_PA_rental` = " . $data[12] . ",`60_8K_PA_service` = " . ($data[12]-$data[11]) . ";
-        END CASE)
+        . "
+        switch($data[1]){
+          case 24:
+          echo "`24_8K_PA_rental` = " . $data[12] . ", `24_8K_PA_service` = " . ($data[12]-$data[11];
+          break;
+          case 36:
+          echo "`36_8K_PA_rental` = " . $data[12] . ", `36_8K_PA_service` = " . ($data[12]-$data[11];
+          break;
+          case 48
+          echo "`48_8K_PA_rental` = " . $data[12] . ", `48_8K_PA_service` = " . ($data[12]-$data[11];
+          break;
+          case 60
+          echo "`60_8K_PA_rental` = " . $data[12] . ", `60_8K_PA_service` = " . ($data[12]-$data[11];
+          break;
+        }
+        " .
         ON DUPLICATE KEY UPDATE
           `cap_id` = " . $data[24];
-        break;
       }
       echo $update . "<br />";
       // $result2 = mysqli_query($conn, $update);
