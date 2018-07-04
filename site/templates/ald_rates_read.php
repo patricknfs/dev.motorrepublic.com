4872a47badeb81777d056adf2ce6557e7689accc
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-07-04 15:34:48 +0100

    Adding ALD uploads form

--- site/templates/ald_rates_read.php
+++ site/templates/ald_rates_read.php
@@ -17,7 +17,7 @@ $result = mysqli_query($conn, $truncate);
 $row = 1;
 $csv = "inc/ald_rates_cars_8k.csv";
 if (($handle = fopen($csv , "r")) !== FALSE) {
-  while (($rawdata = fgetcsv($handle, 0, ",")) !== FALSE) {
+  while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
     // print_r($data);
     $num = count($data);
     if($row > 2){
