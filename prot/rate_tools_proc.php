5f75f6e17f2b102a2c83c41dcf9e948a4c1a0029
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-07-16 16:15:54 +0100

    Start best-rates script

--- prot/rate_tools_proc.php
+++ prot/rate_tools_proc.php
@@ -4,7 +4,7 @@
 date_default_timezone_set('CET');
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
 require_once(MR_PATH . "/inc/conn.php");
-echo "test";
+// echo "test";
 if ($result = $mysqli->query("SELECT * FROM rates_arval WHERE `cap_id` = 71163 LIMIT 1")) {
   printf("Select returned %d rows.\n", $result->num_rows);
 
