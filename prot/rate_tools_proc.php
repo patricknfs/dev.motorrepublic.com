0633f17069336ee8cfa7833c9457d47cc188046c
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-07-16 16:04:33 +0100

    Start best-rates script

---
+++ prot/rate_tools_proc.php
@@ -0,0 +1,8 @@
+<?php
+// best rates_main.php
+
+date_default_timezone_set('CET');
+require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
+require_once(MR_PATH . "/inc/conn.php");
+echo "test";
+?>
