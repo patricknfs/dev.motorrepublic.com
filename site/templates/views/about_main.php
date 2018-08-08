b04231b54ceb24ce7dfcb6b5047e7276f0098be3
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-08-08 09:17:23 +0100

    testing accordion html

--- site/templates/views/about_main.php
+++ site/templates/views/about_main.php
@@ -2,4 +2,9 @@
 // about_main.php
 ?>
 <?=$page->body?>
-We are looking for a person to look after our social media and paid media accounts as well as a competence andf understanding of real world SEO. You need to have moved on from the initial learning period and be Adwords, Bing and Analytics cerified. It would be important to be able to demonstrate your knowledge. As a person you need to be creative but also analytical, conscientious and organised.
+<details>
+			<summary>{{ question }}</summary>
+			<div>
+				{{ answer | markdown }}
+			</div>
+		</details>
