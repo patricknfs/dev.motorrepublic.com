b99172ba352d1e5f4c5166b040a285fa6cd58373
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-08-20 12:05:46 +0100

    designing nav and structure

--- site/templates/main.php
+++ site/templates/main.php
@@ -38,94 +38,59 @@ ERROR_REPORTING(E_ALL);
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Fira+Sans:300|Open+Sans:400" />
   </head>
   <body>
-    <div class="wrapper">
-      <div class="headnav">
-        <header class="header">
-          <div class="header__logo">
-            <img src="<?=$config->urls->assets?>/graphics/logo.svg" alt="Logo" />
-          </div>
-          <nav class="header__nav" role="navigation">
-            <?php
-            $treeMenu = $modules->get("MarkupSimpleNavigation"); // load the module
-            $options = array(
-              'parent_class' => '',
-              'current_class' => '',
-              'has_children_class' => '',
-              'levels' => false,
-              'levels_prefix' => 'level-',
-              'max_levels' => null,
-              'firstlast' => false,
-              'collapsed' => false,
-              'show_root' => false,
-              'selector' => '',
-              'outer_tpl' => '<ul id="" class="header__nav__list">||',
-              'inner_tpl' => '<ul class="">||</ul>',
-              'list_tpl' => '<li%s>||</li>||</li>',
-              'list_field_class' => 'header__nav__item',
-              'item_tpl' => '<a class="header__nav__item__link" href="{url}">{title}</a>',
-              'item_current_tpl' => '<a href="{url}">{title}</a>',
-              'xtemplates' => '',
-              'xitem_tpl' => '<span>{title}</span>',
-              'xitem_current_tpl' => '<span>{title}</span>'
-            );
-            echo $treeMenu->render($options);
-            ?>
-            <div class="header__nav__search js-nav-search">
-              <form action="#" class="header__nav__search__form">
-                <input type="search" placeholder="Search" class="header__nav__search__form__text" autofocus/>
-                <input type="submit" class="header__nav__search__form__submit" value="Rechercher"/>
-              </form>
-              <button class="header__nav__search__close js-nav-search-close"></button>
-            </div>
-          </nav>
-          
-          <button href="#" class="nav-toggle js-nav-toggle" title="navigation menu" aria-label="navigation menu" role="button" aria-controls="navigation" aria-expanded="false">
-            <div class="nav-toggle__content">
-              <div class="nav-toggle__close"></div>
-              <div class="nav-toggle__open">menu</div>
-            </div>
-          </button>
-          
-        </header>
-        
-        <nav class="nav js-nav" role="first navigation">
-          <?php
-          $treeMenu = $modules->get("MarkupSimpleNavigation"); // load the module
-          $options = array(
-            'parent_class' => '',
-            'current_class' => '',
-            'has_children_class' => '',
-            'levels' => false,
-            'levels_prefix' => 'level-',
-            'max_levels' => null,
-            'firstlast' => false,
-            'collapsed' => false,
-            'show_root' => true,
-            'selector' => '',
-            'outer_tpl' => '<ul id="" class="nav__list">||',
-            'inner_tpl' => '<ul class="">||</ul>',
-            'list_tpl' => '<li%s>||</li>||</li>',
-            'list_field_class' => 'nav__item',
-            'item_tpl' => '<a class="nav__item__wrapper" href="{url}">{title}</a>',
-            'item_current_tpl' => '<a href="{url}">{title}</a>',
-            'xtemplates' => '',
-            'xitem_tpl' => '<span>{title}</span>',
-            'xitem_current_tpl' => '<span>{title}</span>'
-          );
-          echo $treeMenu->render($options);
-          ?>
-        </nav>
-        <nav class="nav--right js-second-nav">
-          <p class="nav--right__content">I display the content of <span class="nav--right__content__child-name"></span></p>
-        </nav>
+    <header>
+      <div class="header__logo">
+        <img src="<?=$config->urls->assets?>/graphics/logo.svg" alt="Logo" />
       </div>
-      <div class="content">
+      <nav class="header__nav" role="navigation">
         <?php
-        if($page->main){
-          echo $page->main;
-        }
+        $treeMenu = $modules->get("MarkupSimpleNavigation"); // load the module
+        $options = array(
+          'parent_class' => '',
+          'current_class' => '',
+          'has_children_class' => '',
+          'levels' => false,
+          'levels_prefix' => 'level-',
+          'max_levels' => null,
+          'firstlast' => false,
+          'collapsed' => false,
+          'show_root' => false,
+          'selector' => '',
+          'outer_tpl' => '<ul id="" class="header__nav__list">||',
+          'inner_tpl' => '<ul class="">||</ul>',
+          'list_tpl' => '<li%s>||</li>||</li>',
+          'list_field_class' => 'header__nav__item',
+          'item_tpl' => '<a class="header__nav__item__link" href="{url}">{title}</a>',
+          'item_current_tpl' => '<a href="{url}">{title}</a>',
+          'xtemplates' => '',
+          'xitem_tpl' => '<span>{title}</span>',
+          'xitem_current_tpl' => '<span>{title}</span>'
+        );
+        echo $treeMenu->render($options);
         ?>
-      </div>
+        <div class="header__nav__search js-nav-search">
+          <form action="#" class="header__nav__search__form">
+            <input type="search" placeholder="Search" class="header__nav__search__form__text" autofocus/>
+            <input type="submit" class="header__nav__search__form__submit" value="Rechercher"/>
+          </form>
+          <button class="header__nav__search__close js-nav-search-close"></button>
+        </div>
+      </nav>
+      
+      <button href="#" class="nav-toggle js-nav-toggle" title="navigation menu" aria-label="navigation menu" role="button" aria-controls="navigation" aria-expanded="false">
+        <div class="nav-toggle__content">
+          <div class="nav-toggle__close"></div>
+          <div class="nav-toggle__open">menu</div>
+        </div>
+      </button>
+    </header>
+
+    <div class="content">
+      <?php
+      if($page->main){
+        echo $page->main;
+      }
+      ?>
     </div>
 	</body>
   <script type="text/javascript" src="<?=$config->urls->templates?>/scripts/built.min.js"></script>
