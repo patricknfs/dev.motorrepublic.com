241e3c9d6983f7974062576ba738ae9062f4d0b3
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-08-21 14:03:42 +0100

    install foundation

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -2,7 +2,7 @@
 // home_main.php
 // include('inc/functions.inc');
 ?>
-<div class="grid-x"">
+<div class="grid-x grid-padding-x">
 	<div class="cell medium-6">
 		<h3><?=$page->title?></h3>
 	</div>
