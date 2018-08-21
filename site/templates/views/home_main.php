785b25fa4af31cb1be80df68beccb7d9e6025b42
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-08-21 13:46:59 +0100

    install foundation

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -2,12 +2,14 @@
 // home_main.php
 // include('inc/functions.inc');
 ?>
-<div class="home_content">
-	<div class="feature">
+<div class="grid-x"">
+	<div class="cell-6">
 		<h3><?=$page->feature_title?></h3>
 		<div class="feature_img">
 			<img src="">
 		</div>
+	</div>
+	<div class="cell-6">
 		<div class="feature_text">
 			<?=$page->feature?>
 		</div>
