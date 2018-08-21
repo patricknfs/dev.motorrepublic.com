ad3bb8c9d248d5a90f2e4b1df51f2cfd79e72179
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-08-21 09:57:00 +0100

    install foundation

--- site/templates/main.php
+++ site/templates/main.php
@@ -33,8 +33,8 @@ ERROR_REPORTING(E_ALL);
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/zf/dt-1.10.18/b-1.5.2/b-colvis-1.5.1/b-html5-1.5.2/b-print-1.5.2/cr-1.5.0/fc-3.2.5/fh-3.1.4/kt-2.4.0/r-2.2.2/sc-1.5.0/sl-1.2.6/datatables.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/select/1.2.7/js/dataTables.select.min.js"></script>
-
-    <link rel="stylesheet" type="text/css" href="<?=$config->urls->templates?>styles/styles.min.css" />
+    <script type="text/javascript" src="<?=$config->urls->templates?>scripts/app.js" />
+    <link rel="stylesheet" type="text/css" href="<?=$config->urls->templates?>styles/css/app.css" />
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Fira+Sans:300|Open+Sans:400" />
   </head>
   <body>
