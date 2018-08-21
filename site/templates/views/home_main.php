95b21f256218f6446072350f078e4ae659321a02
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-08-21 14:53:26 +0100

    testing video background

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -2,15 +2,13 @@
 // home_main.php
 // include('inc/functions.inc');
 ?>
-<div class="promo">
+<div class="grid-x">
 	<video autoplay loop muted poster="/assets/img/video-path.jpg" id="video_bg">
 	<source src="<?=$config->urls->assets?>videos/multicolored-particles_zkh9ksnxr__WL.mp4" type="video/mp4">
 	</video>
-	<div class="grid-x">
-		<div class="cell small-8" id="message">
-			<h1>Title Goes Here</h1>
-			<h2>Subtitle goes here.</h2>
-		</div>
+	<div class="cell small-8" id="message">
+		<h1>Title Goes Here</h1>
+		<h2>Subtitle goes here.</h2>
 	</div>
 </div>
 
