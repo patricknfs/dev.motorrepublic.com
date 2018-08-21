1a4b44a6031fac1008f2b05f8c7c1efeb9f5a926
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-08-21 12:34:47 +0100

    install foundation

--- site/templates/main.php
+++ site/templates/main.php
@@ -38,7 +38,7 @@ ERROR_REPORTING(E_ALL);
   </head>
   <body>
     <header>
-      <div class="header_logo">
+      <div class="logoimg">
         <img src="<?=$config->urls->assets?>/graphics/logo.svg" alt="Logo" />
       </div>
       <nav role="navigation">
