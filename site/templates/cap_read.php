10fd7845174c560bd7128e7411913ab2f54d13ec
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-08-22 14:36:22 +0100

    update cap data

--- site/templates/cap_read.php
+++ site/templates/cap_read.php
@@ -3,12 +3,7 @@
 date_default_timezone_set('CET');
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
 require_once(MR_PATH . "/inc/conn.php");
-// Open remote file
-// $ch = file("https://mojo.affilired.com/callback/affiliate.php?private_key=B6999833687CBCA403A4C5EBD85633B9&from_date=" . $yesterday . "&to_date=" . $yesterday . "&separator=",FILE_IGNORE_NEW_LINES);
-// // $ch = file("https://mojo.affilired.com/callback/affiliate.php?private_key=B6999833687CBCA403A4C5EBD85633B9&from_date=2017/06/14&to_date=2017/04/28&separator=",FILE_IGNORE_NEW_LINES);
 
-//Open local file to write to
-// $fp = fopen("/var/www/vhosts/dootet.com/stats.dootet.com/data/affilired.csv", "w");
 $adminEmail = "patrick.ogorman@nationalfleetservices.net";
 $AdminMessage = "MR CSV Upload Report\n";
 // Now open the local file and loop through it.
@@ -22,13 +17,32 @@ if (($handle = fopen("inc/cap_cars.csv", "r")) !== FALSE) {
   while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
     // print_r($data);
     $num = count($data);
-    $insert = "INSERT INTO `team`.`vehicles` (`cap_code`,`cap_id`,`manufacturer`,`model`,`description`) VALUES ('" . $data[0] . "','" . $data[1] . "','" . $data[2] . "','" . $data[4] . "','" . $data[8] . "')";
-    // echo $insert . "<br />"
-    $result2 = mysqli_query($conn, $insert);
-    $row++;
+    if($data[11] == "Y"){
+      $insert = "INSERT INTO `team`.`vehicles` (`cap_code`,`cap_id`,`manufacturer`,`model`,`description`) VALUES ('" . $data[0] . "','" . $data[1] . "','" . $data[2] . "','" . $data[4] . "','" . $data[8] . "')";
+      // echo $insert . "<br />"
+      $result2 = mysqli_query($conn, $insert);
+      $row++;
+    }
   }
   $AdminMessage .= $num . " rows inserted\n";
-  mail($adminEmail,"Dootet - Affilired Commission Monitor",$AdminMessage,"From: Dootet Server");
+  mail($adminEmail,"CAP cars upload",$AdminMessage,"From: MR Server");
+  fclose($handle);
+}
+elseif (($handle = fopen("inc/cap_lcvs.csv", "r")) !== FALSE) {
+  fgets($handle);
+
+  while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
+    // print_r($data);
+    $num = count($data);
+    if($data[11] == "Y"){
+      $insert = "INSERT INTO `team`.`vehicles` (`cap_code`,`cap_id`,`manufacturer`,`model`,`description`) VALUES ('" . $data[0] . "','" . $data[1] . "','" . $data[2] . "','" . $data[4] . "','" . $data[8] . "')";
+      // echo $insert . "<br />"
+      $result2 = mysqli_query($conn, $insert);
+      $row++;
+    }
+  }
+  $AdminMessage .= $num . " rows inserted\n";
+  mail($adminEmail,"CAP LCV\'s upload",$AdminMessage,"From: MR Server");
   fclose($handle);
 }
 else {
