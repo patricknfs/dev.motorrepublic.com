6af56b47d67bd27b5635499fcca00d28b43d4853
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-08-24 10:32:23 +0100

    update row combined

--- prot/get_row_details_combined.php
+++ prot/get_row_details_combined.php
@@ -24,7 +24,7 @@ $result2 = $conn->query($truncate) or die(mysqli_error());
 
 
 $query = "SELECT 
-  mr1.code AS `cap_id`, mr1.capcode AS `cap_code`, mr2.src AS source , mr1.man AS `manufacturer`, mr1.mod AS `model`, mr1.cap_desc AS `descr`, mr2.term AS `term`, mr2.mileage AS `mileage`, mr2.rent AS `rental`
+  mr1.code AS `cap_id`, mr1.capcode AS `cap_code`, mr2.src AS source , mr1.man AS `manufacturer`, mr1.mod AS `model`, mr1.cap_desc AS `descr`, mr2.vlp AS `vehicle_list_price`, mr1.votrp AS `vehicle_otr_price`, mr1.p11p AS `p11d_price`, mr1.co2 AS `CO2`, mr2.term AS `term`, mr2.mileage AS `mileage`, mr2.rent AS `rental`
   FROM
   (
     SELECT 
