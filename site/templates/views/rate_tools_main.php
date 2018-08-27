0456538dd8182250465eddb4e1d74ed591e2ef7f
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-08-27 18:07:18 +0100

    adding under row to table

--- site/templates/views/rate_tools_main.php
+++ site/templates/views/rate_tools_main.php
@@ -34,23 +34,23 @@
 </table>
 <script type="text/javascript">
   /* Formatting function for row details - modify as you need */
-  function format ( d ) {
-    // `d` is the original data object for the row
-    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
-        '<tr>'+
-            '<td>Full name:</td>'+
-            '<td>'+d.co2+'</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<td>Extension number:</td>'+
-            '<td>'+d.p11d+'</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<td>Extra info:</td>'+
-            '<td>And any further details here (images etc)...</td>'+
-        '</tr>'+
-    '</table>';
-  };
+  // function format ( d ) {
+  //   // `d` is the original data object for the row
+  //   return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
+  //       '<tr>'+
+  //           '<td>Full name:</td>'+
+  //           '<td>'+d.co2+'</td>'+
+  //       '</tr>'+
+  //       '<tr>'+
+  //           '<td>Extension number:</td>'+
+  //           '<td>'+d.p11d+'</td>'+
+  //       '</tr>'+
+  //       '<tr>'+
+  //           '<td>Extra info:</td>'+
+  //           '<td>And any further details here (images etc)...</td>'+
+  //       '</tr>'+
+  //   '</table>';
+  // };
   $(document).ready(function() {
 		var oTable = $('#example').DataTable( {
 			"language": {
@@ -94,20 +94,20 @@
       "select": true
 		});
     // Add event listener for opening and closing details
-    $('#example tbody').on('click', 'td.details-control', function () {
-      var tr = $(this).closest('tr');
-      var row = table.row( tr );
+    // $('#example tbody').on('click', 'td.details-control', function () {
+    //   var tr = $(this).closest('tr');
+    //   var row = table.row( tr );
 
-      if ( row.child.isShown() ) {
-          // This row is already open - close it
-          row.child.hide();
-          tr.removeClass('shown');
-      }
-      else {
-          // Open this row
-          row.child( format(row.data()) ).show();
-          tr.addClass('shown');
-      }
-    });
+    //   if ( row.child.isShown() ) {
+    //       // This row is already open - close it
+    //       row.child.hide();
+    //       tr.removeClass('shown');
+    //   }
+    //   else {
+    //       // Open this row
+    //       row.child( format(row.data()) ).show();
+    //       tr.addClass('shown');
+    //   }
+    // });
 	});
 </script>
