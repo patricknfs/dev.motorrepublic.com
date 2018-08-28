dd61c77f03d188eaa9afda202c48f18a7d51d6be
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-08-28 14:23:39 +0100

    adding under row to table

--- site/templates/views/rate_tools_main.php
+++ site/templates/views/rate_tools_main.php
@@ -40,15 +40,15 @@
     // `d` is the original data object for the row
     return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
         '<tr>'+
-            '<td>CO2:</td>'+
-            '<td>'+d.CO2+'</td>'+
-            '<td>P11d:</td>'+
+            '<td width="50%"><b>CO2:</b></td>'+
+            '<td width="50%">'+d.CO2+'</td>'+
+            '<td><b>P11d:</b></td>'+
             '<td>'+d.p11d_price+'</td>'+
         '</tr>'+
         '<tr>'+
-            '<td>List Price:</td>'+
-            '<td>'+d.vehicle_list_price+'</td>'+
-            '<td>Funder OTR Price (Not to be used with other funders):</td>'+
+            '<td width="50%"><b>List Price:</b></td>'+
+            '<td width="50%">'+d.vehicle_list_price+'</td>'+
+            '<td><b>Funder OTR Price (Not to be used with other funders):</b></td>'+
             '<td>'+d.vehicle_otr_price+'</td>'+
         '</tr>'+
     '</table>';
