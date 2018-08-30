df8ee30c472141285d591be42cf99ef2f5660737
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-08-30 15:13:09 +0100

    specials form upload

--- site/templates/views/uploads_main.php
+++ site/templates/views/uploads_main.php
@@ -6,6 +6,11 @@
     <div class="cell medium-6">
       <h1><?php echo $page->title; ?></h1>
       <?php echo $forms->embed('specials_upload');?>
-      </div>
+    </div>
   </div>
+    <div class="grid-x">
+      <div class="cell medium-6">
+      test
+      </div>
+    </div>
 </div>
