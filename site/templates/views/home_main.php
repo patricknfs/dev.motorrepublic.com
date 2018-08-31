c44358ecc7f04801932d75886cd70680409a30b5
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-08-31 15:17:26 +0100

    Add specials

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -2,13 +2,7 @@
 // home_main.php
 // include('inc/functions.inc');
 ?>
-<div class="grid-x">
+
 	<video autoplay loop muted poster="/assets/img/video-path.jpg" id="video_bg">
 		<source src="<?=$config->urls->assets?>videos/videoblocks-animated-car-dashboard-start-engine_hubtnue___WL.mp4" type="video/mp4">
 	</video>
-	<div class="cell small-8" id="message">
-		<h1>Title Goes Here</h1>
-		<h2>Subtitle goes here.</h2>
-	</div>
-</div>
-
