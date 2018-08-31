84860567bbdeb988e80e7a620e1ab8f3ff4958f4
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-08-31 09:24:13 +0100

    Add specials

--- prot/get_row_details_specials.php
+++ prot/get_row_details_specials.php
@@ -7,7 +7,6 @@ date_default_timezone_set('CET');
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/conn.php';
 
-<?php
 // iterate over every row
 $row = 1;
 
