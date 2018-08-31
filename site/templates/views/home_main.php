682ea47dc44fa6cb1dcb837583a9dfd89cdf7415
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-08-31 15:18:30 +0100

    Add specials

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -3,6 +3,7 @@
 // include('inc/functions.inc');
 ?>
 
+
 	<video autoplay loop muted poster="/assets/img/video-path.jpg" id="video_bg">
 		<source src="<?=$config->urls->assets?>videos/videoblocks-animated-car-dashboard-start-engine_hubtnue___WL.mp4" type="video/mp4">
 	</video>
