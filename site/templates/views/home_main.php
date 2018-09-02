0d1a595394c7fef2ba337fb6733cfa74262981f6
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-02 12:45:08 +0100

    Add specials

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -2,12 +2,13 @@
 // home_main.php
 ?>
 <section class="promo">
-	<video autoplay loop muted poster="/assets/img/video-path.jpg" id="video_bg">
-		<source src="<?=$config->urls->assets?>videos/videoblocks-animated-car-dashboard-start-engine_hubtnue___WL.mp4" type="video/mp4">
-	</video>
-	<div id="message">
+<div id="message">
 		<h1>Deal of the Day</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta dictum turpis, eu mollis justo gravida ac. Proin non eros blandit, rutrum est a, cursus quam. Nam ultricies, velit ac suscipit vehicula, turpis eros sollicitudin lacus, at convallis mauris magna non justo. Etiam et suscipit elit. Morbi eu ornare nulla, sit amet ornare est. Sed vehicula ipsum a mattis dapibus. Etiam volutpat vel enim at auctor.</p>
 		<p>Aenean pharetra convallis pellentesque. Vestibulum et metus lectus. Nunc consectetur, ipsum in viverra eleifend, erat erat ultricies felis, at ultricies mi massa eu ligula. Suspendisse in justo dapibus metus sollicitudin ultrices id sed nisl.</p>
 	</div>
+	<video autoplay loop muted poster="/assets/img/video-path.jpg" id="video_bg">
+		<source src="<?=$config->urls->assets?>videos/videoblocks-animated-car-dashboard-start-engine_hubtnue___WL.mp4" type="video/mp4">
+	</video>
+	
 </section>
