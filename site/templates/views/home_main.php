b2f95a7c28d984522539147d186215e79eb239fe
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-02 13:23:35 +0100

    Working on home page

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -13,7 +13,7 @@
 </section>
 <section>
 	<div class="grid-container">
-		<div class="grid-x">
+		<div class="grid-x grid-margin-x">
 			<div class="cell small-12 medium-4">
 				<h3>We happy?</h3>
 				<p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
