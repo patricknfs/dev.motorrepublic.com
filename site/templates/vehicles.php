2c662a4e37dcafd27b6f03eb5b366a627489f8ed
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-06 20:12:47 +0100

    working on vehicles page

--- site/templates/vehicles.php
+++ site/templates/vehicles.php
@@ -4,13 +4,13 @@
 // $products = $pages->find("$selector, limit=12, sort=sequence, sort=colour");
 
 // $pagination = $products->renderPager(array(
-  'nextItemLabel' => "Next",
-  'previousItemLabel' => "Prev",
-  'listMarkup' => "<ul class='MarkupPagerNav pagination text-center'role='navigation' aria-label='Pagination'>{out}</ul>",
-  'currentItemClass' => "current",
-  'itemMarkup' => "<li class='{class}'>{out}</li>",
-  'linkMarkup' => "<a href='{url}'><span>{out}</span></a>"  
-));
+//   'nextItemLabel' => "Next",
+//   'previousItemLabel' => "Prev",
+//   'listMarkup' => "<ul class='MarkupPagerNav pagination text-center'role='navigation' aria-label='Pagination'>{out}</ul>",
+//   'currentItemClass' => "current",
+//   'itemMarkup' => "<li class='{class}'>{out}</li>",
+//   'linkMarkup' => "<a href='{url}'><span>{out}</span></a>"  
+// ));
 ob_start();
 include('views/vehicles_main.php');
 $page->main = ob_get_clean();
