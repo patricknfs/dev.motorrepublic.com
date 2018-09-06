970fd798bdaa1db411dd7a1a56c210dc10b15080
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-06 18:18:52 +0100

    upload 1900 tesla image

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -54,15 +54,6 @@
 				<h1>Yeah, and if you were the pope they'd be all</h1>
 				<p>It's a T. It goes "tuh". We're rescuing ya. You've killed me! Oh, you've killed me! I had more, but you go ahead.</p>
 				<p>When I was first asked to make a film about my nephew, Hubert Farnsworth, I thought "Why should I? " Then later, Leela made the film. <strong> But if I did make it, you can bet there would have been more topless women on motorcycles.</strong> <em> Roll film!</em> Why would a robot need to drink?</p>
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
 			</div>
 			<div class="cell small-12 medium-6"></div>
 		</div>
