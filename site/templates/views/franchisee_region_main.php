6cabe478d030ef96c601a4854548384d923d9b0d
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-09 18:22:28 +0100

    Trying to access $forms

--- site/templates/views/franchisee_region_main.php
+++ site/templates/views/franchisee_region_main.php
@@ -4,7 +4,7 @@
 <div class="grid-container">
   <div class="grid-x">
     <div class="cell">
-      <h1><?php echo $page->title; ?></h1>
+      <h4><?php echo $page->title; ?></h4>
       <?=$page->body?>
       </div>
   </div>
