841bbfbd73c9aa088d0a0affb4c515b8e0f658cc
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-09 19:07:50 +0100

    Trying to access $forms

--- site/templates/views/franchisee_region_main.php
+++ site/templates/views/franchisee_region_main.php
@@ -5,7 +5,7 @@
   <div class="grid-x">
     <div class="cell">
       <h3><?php echo $page->title; ?></h3>
-      <img src="<?=$page->profile_image?>" />
+      <img src="<?=$page->profile_image->url?>" />
       <?=$page->body?>
       </div>
   </div>
