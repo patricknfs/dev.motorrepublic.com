586f868594e4b5fe3ca3f8e963a5f7ca8aa83a4f
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-09 17:31:55 +0100

    Trying to access $forms

--- site/templates/get_row_details_specials.php
+++ site/templates/get_row_details_specials.php
@@ -2,11 +2,11 @@
 <?php
 // get_row_details_specials.php
 // print_r($_GET);
-header('Content-type: application/json; charset=utf-8');
-session_start();
-date_default_timezone_set('CET');
-require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
-require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/conn.php';
+// header('Content-type: application/json; charset=utf-8');
+// session_start();
+// date_default_timezone_set('CET');
+// require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
+// require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/conn.php';
 
 // iterate over every row
 $row = 1;
