c3cd1e069dd4f14e646217c9c995001520235db1
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-09 19:40:20 +0100

    Creating franchisee main page

--- site/templates/views/franchisee_region_main.php
+++ site/templates/views/franchisee_region_main.php
@@ -1,12 +1,14 @@
 <?php
 // basic-page_main.php
 ?>
-<div class="grid-container">
-  <div class="grid-x">
-    <div class="cell small-6 medium-4">
-      <h3><?php echo $page->title; ?></h3>
-      <img src="<?=$page->profile_image->url?>" alt="<?=$page->profile_image->description?>" />
-      <?=$page->body?>
-      </div>
+<section id="intro" style="background: url(<?=$config->urls->assets?>images/banner_bg.jpg)">
+  <div class="grid-container">
+    <div class="grid-x">
+      <div class="cell small-6 medium-4">
+        <h3><?php echo $page->title; ?></h3>
+        <img src="<?=$page->profile_image->url?>" alt="<?=$page->profile_image->description?>" />
+        <?=$page->body?>
+        </div>
+    </div>
   </div>
-</div>
+</section>
