3b3cd6a7081f28b7ca88e18da624d14b4f9e21a4
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-10 22:27:33 +0100

    designing vehicles page

--- site/templates/views/vehicles_main.php
+++ site/templates/views/vehicles_main.php
@@ -2,7 +2,7 @@
 	<div class="grid-x">
 		<div class="cell small-12">
 			<div class="text-center">
-				<h3><?=$page->title?></h3>
+				<!-- <h3><?=$page->title?></h3> -->
 				<?php echo $forms->embed('vehicle_power_search'); ?>
 			</div>
 		</div>
