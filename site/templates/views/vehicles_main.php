bbdbdce8c8d9fedfa03b5480a3e6479bd1984287
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-10 07:27:03 +0100

    pulling results for vehicle page

--- site/templates/views/vehicles_main.php
+++ site/templates/views/vehicles_main.php
@@ -1,41 +1,43 @@
-<div class="grid-x">
-	<div class="cell small-12">
-		<div class="callout text-center">
-			<h3><?=$page->title?></h3>
+<div class="grid-container">
+	<div class="grid-x">
+		<div class="cell small-12">
+			<div class="callout text-center">
+				<h3><?=$page->title?></h3>
+				
+			</div>
+		</div>
+		<div class="cell small-12">
 			
 		</div>
 	</div>
-	<div class="cell small-12">
-		
-	</div>
-</div>
-<div class="grid-x">
-	<?php
-	foreach($result AS $vehicle) {
-		?>
-		<div class="cell small-6 medium-3" itemscope itemtype="http://schema.org/Product">
-			<?php
-			$options = array(
-				'quality' => 80,
-				'upscaling' => false       
-			); 
-			?>
-			<div>
-				<?=$vehicle['cap_id']?>
-				<h6>
-					<?=$vehicle['manufacturer']?> <?=$vehicle['model']?>
-				</h6>
-				<p><?=$vehicle['descr']?></p>
-				<h4><?=$vehicle['rental']?></h4>
-			</div>
-		</div>
+	<div class="grid-x">
 		<?php
-	}
-	?>
-	<div class="clearer">&nbsp</div>
-</div>
-<div class="grid-x">
-	<div class="cell small-12">
+		foreach($result AS $vehicle) {
+			?>
+			<div class="cell small-6 medium-3" itemscope itemtype="http://schema.org/Product">
+				<?php
+				$options = array(
+					'quality' => 80,
+					'upscaling' => false       
+				); 
+				?>
+				<div>
+					<?=$vehicle['cap_id']?>
+					<h6>
+						<?=$vehicle['manufacturer']?> <?=$vehicle['model']?>
+					</h6>
+					<p><?=$vehicle['descr']?></p>
+					<h4><?=$vehicle['rental']?></h4>
+				</div>
+			</div>
+			<?php
+		}
+		?>
+		<div class="clearer">&nbsp</div>
+	</div>
+	<div class="grid-x">
+		<div class="cell small-12">
 
+		</div>
 	</div>
 </div>
