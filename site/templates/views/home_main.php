77da7447778400a6e002b7b7e039ab4d572ad928
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-10 22:32:31 +0100

    designing vehicles page

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -33,15 +33,15 @@
 	<div class="grid-container">
 		<div class="grid-x grid-margin-x">
 			<div class="cell small-12 medium-4">
-				<h3>We happy?</h3>
+				<h3>Top 5 Deals</h3>
 				<p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
 			</div>
 			<div class="cell small-12 medium-4">
-				<h3>We happy?</h3>
+				<h3>Magazine Article</h3>
 				<p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
 			</div>
 			<div class="cell small-12 medium-4">
-				<h3>We happy?</h3>
+				<h3>Regional Focus?</h3>
 				<p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
 			</div>
 		</div>
