d45a21ef773d7ee79283fcb88cf2492c8f8945fc
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-11 15:58:29 +0100

    testing CAP images

--- site/templates/views/vehicles_main.php
+++ site/templates/views/vehicles_main.php
@@ -20,7 +20,7 @@
 			// Set some options - we are passing in a useragent too here
 			curl_setopt_array($curl, array(
 					CURLOPT_RETURNTRANSFER => 1,
-					CURLOPT_URL => 'https://soap.cap..co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=498B0751B9DA01C22242AE2E039E1B16&DB=CAR&CAPID=64851&DATE=2018/09/11&WIDTH=1024&HEIGHT=768&IMAGETEXT=test&VIEWPOINT=',
+					CURLOPT_URL => 'https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=498B0751B9DA01C22242AE2E039E1B16&DB=CAR&CAPID=64851&DATE=2018/09/11&WIDTH=1024&HEIGHT=768&IMAGETEXT=test&VIEWPOINT=',
 					CURLOPT_USERAGENT => 'User Agent X'
 			));
 			// Send the request & save response to $resp
