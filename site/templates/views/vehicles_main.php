f3ffe4f4a6f5e2a13f967ce1322a70430182c51e
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-11 23:43:11 +0100

    testing CAP images

--- site/templates/views/vehicles_main.php
+++ site/templates/views/vehicles_main.php
@@ -19,17 +19,18 @@
 			$imagefile = $vehicle['cap_id'];
 			// Get cURL resource
 			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, "https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=" . strtoupper($hashcode) . "&DB=CAR&CAPID=" . $vehicle['cap_id'] . "&DATE=2018/09/11&WIDTH=1024&HEIGHT=768&IMAGETEXT=test&VIEWPOINT=");
 			curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
-$output = curl_exec ($ch);
-curl_close ($ch);
-echo $output;
-// $fp = fopen($imagefile . ".jpg",'w');
-// fwrite($fp, $rawdata);
-// fclose($fp);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
+			$output = curl_exec ($ch);
+			curl_close ($ch);
+			echo $output;
+			// $fp = fopen($imagefile . ".jpg",'w');
+			// fwrite($fp, $rawdata);
+			// fclose($fp);
 
-readfile($imagefile . ".jpg");
+			readfile($imagefile . ".jpg");
 			?>
 			<div class="cell">
 				<a href="<?=$config->urls->templates?>vehicle/<?=$vehicle['cap_id']?>">
