fdfe461df091b712124d599d6dd125976c341056
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-12 00:13:47 +0100

    testing CAP images

--- site/templates/views/vehicles_main.php
+++ site/templates/views/vehicles_main.php
@@ -23,6 +23,7 @@
 			curl_setopt($ch, CURLOPT_HEADER, 0);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 			$output = curl_exec ($ch);
 			curl_close ($ch);
 			// echo $output;
