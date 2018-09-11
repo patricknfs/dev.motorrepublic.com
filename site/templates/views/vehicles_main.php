35bf111d2161c8e5696ca1ae083f0998a8ec9854
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-11 23:04:57 +0100

    testing CAP images

--- site/templates/views/vehicles_main.php
+++ site/templates/views/vehicles_main.php
@@ -17,34 +17,19 @@
 			$rental = (($vehicle['rental'] * $vehicle['term']) + 300) / $vehicle['term'];
 			$hashcode = md5("173210NfS4JeCAR" . $vehicle['cap_id']);
 			$imagefile = $vehicle['cap_id'];
-			$headers[] = 'Accept: image/gif, image/x-bitmap, image/jpeg, image/pjpeg';              
-			$headers[] = 'Connection: Keep-Alive';         
-			$headers[] = 'Content-type: application/x-www-form-urlencoded;charset=UTF-8';   
-			$useragent = 'php';
 			// Get cURL resource
 			$curl = curl_init();
 			// Set some options - we are passing in a useragent too here
 			curl_setopt_array($curl, array(
 				CURLOPT_URL => "https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=" . strtoupper($hashcode) . "&DB=CAR&CAPID=" . $vehicle['cap_id'] . "&DATE=2018/09/11&WIDTH=1024&HEIGHT=768&IMAGETEXT=test&VIEWPOINT=",
-				CURLOPT_HTTPHEADER => $headers,
-				CURLOPT_HEADER => 0,
-				CURLOPT_USERAGENT => $useragent,
-				CURLOPT_TIMEOUT => 30,
 				CURLOPT_RETURNTRANSFER => 1,
-				CURLOPT_BINARYTRANSFER => 1,
-				CURLOPT_FOLLOWLOCATION => 1,
-				CURLOPT_SSL_VERIFYPEER => 0,
-				CURLOPT_SSLVERSION => 3
 			));
 			// Send the request & save response to $resp
 			$rawdata = curl_exec($curl);
 			// Close request to clear up some resources
 			// print_r($image);
 			curl_close($curl);
-
-			$fp = fopen($imagefile . ".jpg",'w');
-			fwrite($fp, $rawdata); 
-			fclose($fp);
+			$fp = file_put_contents($imagefile . ".jpg",$rawdata);
 			?>
 			<div class="cell">
 				<a href="<?=$config->urls->templates?>vehicle/<?=$vehicle['cap_id']?>">
