c57f0666b01a822a3e7e50fc6a6d97e06bf8b7b8
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-11 16:13:59 +0100

    testing CAP images

--- site/templates/views/vehicles_main.php
+++ site/templates/views/vehicles_main.php
@@ -15,16 +15,17 @@
 				'upscaling' => false       
 			); 
 			$rental = (($vehicle['rental'] * $vehicle['term']) + 300) / $vehicle['term'];
+			$hashcode = md5("173210NfS4JeCAR" . $vehicle['cap_id']);
 			// Get cURL resource
 			$curl = curl_init();
 			// Set some options - we are passing in a useragent too here
 			curl_setopt_array($curl, array(
 					CURLOPT_RETURNTRANSFER => 1,
-					CURLOPT_URL => 'https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=498B0751B9DA01C22242AE2E039E1B16&DB=CAR&CAPID=64851&DATE=2018/09/11&WIDTH=1024&HEIGHT=768&IMAGETEXT=test&VIEWPOINT=',
+					CURLOPT_URL => "https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=" . $hashcode . "=CAR&CAPID=64851&DATE=2018/09/11&WIDTH=1024&HEIGHT=768&IMAGETEXT=test&VIEWPOINT=",
 					CURLOPT_USERAGENT => 'User Agent X'
 			));
 			// Send the request & save response to $resp
-			$resp = curl_exec($curl);
+			$image = curl_exec($curl);
 			// Close request to clear up some resources
 			print_r($resp);
 			curl_close($curl);
@@ -33,7 +34,7 @@
 				<a href="<?=$config->urls->templates?>vehicle/<?=$vehicle['cap_id']?>">
 					<div class="card">
 						<!-- <?=$vehicle['cap_id']?> -->
-						<img src="<?=$resp?>">
+						<img src="<?=$imagep?>">
 						<div class="card-section">
 							<h6>
 								<?=$vehicle['manufacturer']?> <?=$vehicle['model']?>
