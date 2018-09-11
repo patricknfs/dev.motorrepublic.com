b945e8001f4d92c23e66abb3304d0cf42ac5a6c6
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-11 08:01:38 +0100

    designing vehicles page

--- site/templates/views/vehicles_main.php
+++ site/templates/views/vehicles_main.php
@@ -28,7 +28,7 @@
 							<p><?=$vehicle['descr']?></p>
 						</div>
 						<div class="card-divider">
-							<h4><?=$rental?></h4>
+							<h4>&#stg;<?=$rental?></h4>
 						</div>
 					</div>
 				</a>
