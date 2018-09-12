ab73cc51953701c6a0eef6f55c42b441b3089bf6
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-12 03:13:36 +0100

    testing CAP images

--- site/templates/views/vehicles_main.php
+++ site/templates/views/vehicles_main.php
@@ -29,8 +29,8 @@
 						</div>
 						<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$vehicle['cap_id']?>&DATE=2018/09/11&WIDTH=300&HEIGHT=225&IMAGETEXT=&VIEWPOINT=">
 						<div class="card-section">
-							<h6>£<?=$bch?></h6>
-							<h6>£<?=round($pch, 2)?></h6>
+							<h6>Business Clients £<?=$bch?></h6>
+							<h6>Personal Clients £<?=round($pch, 2)?></h6>
 						</div>
 					</div>
 				</a>
