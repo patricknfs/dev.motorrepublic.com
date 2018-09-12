159eec7626389dfe532ef1cce27f30af0d0e689f
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-12 11:55:17 +0100

    testing fonts

--- site/templates/views/vehicle_main.php
+++ site/templates/views/vehicle_main.php
@@ -7,25 +7,23 @@
 			</div>
 		</div>
 	</div>
-	<div class="grid-x grid-padding-x small-up-2 medium-up-4">
+	<div class="grid-x grid-padding-x">
 		<?php
 		$bch = (($data['rental'] * $data['term'] + 300) / $data['term']);
 		$pch = ((($data['rental'] * $data['term']) + 300) / $data['term'])*1.2;
 		$hashcode = strtoupper(md5("173210NfS4JeCAR" . $input->urlSegment1));
 		?>
+		<div class="cell small-12 medium-8">
 		<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$input->urlSegment1;?>&DATE=2018/09/11&WIDTH=1024&HEIGHT=768&IMAGETEXT=&VIEWPOINT=">
-		<div>
+		</div>
+		
+		<div class="cell small-12 medium-4">
 			<h6>
 				<?=$data['manufacturer']?> <?=$data['model']?>
 			</h6>
 			<p><?=$data['descr']?></p>>
-		<h6>Business Clients £<?=$bch?></h6>
-		<h6>Personal Clients £<?=round($pch, 2)?></h6>
-		</div>
-	</div>
-	<div class="grid-x">
-		<div class="cell small-12">
-
+			<h6>Business Clients £<?=$bch?></h6>
+			<h6>Personal Clients £<?=round($pch, 2)?></h6>
 		</div>
 	</div>
 </div>
