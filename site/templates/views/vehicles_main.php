818fbb9937ae0c9d86b57482e38d822211f66c9e
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-12 15:11:11 +0100

    testing fonts

--- site/templates/views/vehicles_main.php
+++ site/templates/views/vehicles_main.php
@@ -14,8 +14,8 @@
 				'quality' => 80,
 				'upscaling' => false       
 			); 
-			$bch = (($vehicle['rental'] * $vehicle['term']) + 300) / $vehicle['term'];
-			$pch = ((($vehicle['rental'] * $vehicle['term']) + 300) / $vehicle['term'])*1.2;
+			$bch = round((($vehicle['rental'] * $vehicle['term']) + 300) / $vehicle['term'],2);
+			$pch = round(((($vehicle['rental'] * $vehicle['term']) + 300) / $vehicle['term'])*1.2,2);
 			$hashcode = strtoupper(md5("173210NfS4JeCAR" . $vehicle['cap_id']));
 			?>
 			<div class="cell">
@@ -30,7 +30,7 @@
 						<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$vehicle['cap_id']?>&DATE=2018/09/11&WIDTH=300&HEIGHT=225&IMAGETEXT=&VIEWPOINT=">
 						<div class="card-section">
 							<h6>Business Clients £<?=$bch?></h6>
-							<h6>Personal Clients £<?=round($pch, 2)?></h6>
+							<h6>Personal Clients £<?=$pch?></h6>
 						</div>
 					</div>
 				</a>
