2d8fe3ac4538755a09ca22a552f880e442883e58
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-12 02:28:36 +0100

    testing CAP images

--- site/templates/views/vehicles_main.php
+++ site/templates/views/vehicles_main.php
@@ -9,39 +9,18 @@
 	</div>
 	<div class="grid-x grid-padding-x small-up-2 medium-up-4">
 		<?php
-		function gets($url){
-			$headers[] = 'Accept: image/gif, image/x-bitmap, image/jpeg, image/pjpeg';              
-			$headers[] = 'Connection: Keep-Alive';         
-			$headers[] = 'Content-type: application/x-www-form-urlencoded;charset=UTF-8';
-
-			$userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36';
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
-			// curl_setopt($ch, CURLOPT_HEADER, 0);
-			curl_setopt($ch, CURLOPT_VERBOSE, 0);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
-			curl_setopt($ch, CURLOPT_ENCODING, '');
-			$output = curl_exec($ch);
-			curl_close($ch);
-			return $output;
-		}
 		foreach($result AS $vehicle) {
 			$options = array(
 				'quality' => 80,
 				'upscaling' => false       
 			); 
 			$rental = (($vehicle['rental'] * $vehicle['term']) + 300) / $vehicle['term'];
-			$hashcode = md5("173210NfS4JeCAR" . $vehicle['cap_id']);
-			$imagefile = $vehicle['cap_id'];
-			// Get cURL resource
-			$img = "https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=" . strtoupper($hashcode) . "&DB=CAR&CAPID=" . $vehicle['cap_id'] . "&DATE=2018/09/11&WIDTH=800&HEIGHT=600&IMAGETEXT=&VIEWPOINT=";
+			$hashcode = strtoupper(md5("173210NfS4JeCAR" . $vehicle['cap_id']));
 			?>
 			<div class="cell">
 				<a href="<?=$config->urls->templates?>vehicle/<?=$vehicle['cap_id']?>">
 					<div class="card">
-						<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=strtoupper($hashcode)?>&DB=CAR&CAPID=<?=$vehicle['cap_id']?>&DATE=2018/09/11&WIDTH=800&HEIGHT=600&IMAGETEXT=&VIEWPOINT=">
+						<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$vehicle['cap_id']?>&DATE=2018/09/11&WIDTH=800&HEIGHT=600&IMAGETEXT=&VIEWPOINT=">
 						
 						<div class="card-section">
 							<h6>
