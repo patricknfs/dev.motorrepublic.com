19ec21d77200e49c2719f5898c3d510f07375a94
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-12 11:52:43 +0100

    testing fonts

--- site/templates/views/vehicle_main.php
+++ site/templates/views/vehicle_main.php
@@ -14,13 +14,11 @@
 		$hashcode = strtoupper(md5("173210NfS4JeCAR" . $input->urlSegment1));
 		?>
 		<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$input->urlSegment1;?>&DATE=2018/09/11&WIDTH=1024&HEIGHT=768&IMAGETEXT=&VIEWPOINT=">
-		<div class="card-section">
+		<div>
 			<h6>
 				<?=$data['manufacturer']?> <?=$data['model']?>
 			</h6>
-			<p><?=$data['descr']?></p>
-		</div>
-		<div class="card-divider">
+			<p><?=$data['descr']?></p>>
 		<h6>Business Clients £<?=$bch?></h6>
 		<h6>Personal Clients £<?=round($pch, 2)?></h6>
 		</div>
