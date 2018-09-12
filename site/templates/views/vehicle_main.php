74f0a23d2c46c5d061b7e626ce12ae88c685b933
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-12 12:07:46 +0100

    testing fonts

--- site/templates/views/vehicle_main.php
+++ site/templates/views/vehicle_main.php
@@ -19,9 +19,7 @@
 		<div class="cell small-12 medium-4">
 			<div class="card">
 				<div class="card-section">
-					<h6>
-						<?=$data['manufacturer']?> <?=$data['model']?>
-					</h6>
+					<h6><?=$data['manufacturer']?> <?=$data['model']?></h6>
 					<p><?=$data['descr']?></p>
 					<h6>Business Clients £<?=round($bch, 2)?></h6>
 					<h6>Personal Clients £<?=round($pch, 2)?></h6>
