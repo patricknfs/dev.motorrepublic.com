8ba4e5f98369c6b523b1a4d57ebd58928d63c1ea
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-12 01:03:54 +0100

    testing CAP images

--- site/templates/views/vehicles_main.php
+++ site/templates/views/vehicles_main.php
@@ -9,23 +9,23 @@
 	</div>
 	<div class="grid-x grid-padding-x small-up-2 medium-up-4">
 		<?php
-					function gets($url){
-						$headers[] = 'Accept: image/gif, image/x-bitmap, image/jpeg, image/pjpeg';              
-						$headers[] = 'Connection: Keep-Alive';         
-						$headers[] = 'Content-type: application/x-www-form-urlencoded;charset=UTF-8';
-			
-						$userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36';
-						$ch = curl_init();
-						curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
-						curl_setopt($ch, CURLOPT_HEADER, 0);
-						curl_setopt($ch, CURLOPT_VERBOSE, 0);
-						curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-						curl_setopt($ch, CURLOPT_URL, $url);
-						curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
-						$output = curl_exec($ch);
-						curl_close($ch);
-						return $output;
-				}
+		function gets($url){
+			$headers[] = 'Accept: image/gif, image/x-bitmap, image/jpeg, image/pjpeg';              
+			$headers[] = 'Connection: Keep-Alive';         
+			$headers[] = 'Content-type: application/x-www-form-urlencoded;charset=UTF-8';
+
+			$userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36';
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
+			curl_setopt($ch, CURLOPT_HEADER, 0);
+			curl_setopt($ch, CURLOPT_VERBOSE, 0);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
+			$output = curl_exec($ch);
+			curl_close($ch);
+			return $output;
+		}
 		foreach($result AS $vehicle) {
 			$options = array(
 				'quality' => 80,
@@ -35,18 +35,13 @@
 			$hashcode = md5("173210NfS4JeCAR" . $vehicle['cap_id']);
 			$imagefile = $vehicle['cap_id'];
 			// Get cURL resource
-
-
-
-		$img = "https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=" . strtoupper($hashcode) . "&DB=CAR&CAPID=" . $vehicle['cap_id'] . "&DATE=2018/09/11&WIDTH=1024&HEIGHT=768&IMAGETEXT=test&VIEWPOINT=";
-		// header('Content-type: image/jpeg');
-		// echo gets($img);
+			$img = "https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=" . strtoupper($hashcode) . "&DB=CAR&CAPID=" . $vehicle['cap_id'] . "&DATE=2018/09/11&WIDTH=1024&HEIGHT=768&IMAGETEXT=test&VIEWPOINT=";
 			?>
 			<div class="cell">
 				<a href="<?=$config->urls->templates?>vehicle/<?=$vehicle['cap_id']?>">
 					<div class="card">
 						<!-- <?=$vehicle['cap_id']?> -->
-						<img src="<?=gets($img)?>">
+						<?=gets($img)?>
 						<div class="card-section">
 							<h6>
 								<?=$vehicle['manufacturer']?> <?=$vehicle['model']?>
