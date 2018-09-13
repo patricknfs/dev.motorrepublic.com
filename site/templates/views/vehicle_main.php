c989b1871db15854487b235a0655ea4b8afc765b
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-13 22:01:35 +0100

    rounding vehicle prices

--- site/templates/views/vehicle_main.php
+++ site/templates/views/vehicle_main.php
@@ -9,11 +9,7 @@
 	</div>
 	<div class="grid-x grid-padding-x">
 		<?php
-		// $rental = number_format((float)$data['rental'], 2, '.', '');
-		$bch = ((($data['rental'] * $data['term']) + 300) / ($data['term']+2));
 
-		$pch = ((($data['rental'] * $data['term']) + 300) / ($data['term']+2)*1.2);
-		$hashcode = strtoupper(md5("173210NfS4JeCAR" . $input->urlSegment1));
 		?>
 		<div class="cell small-12 medium-8">
 			<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$input->urlSegment1;?>&DATE=2018/09/11&WIDTH=800&HEIGHT=600&IMAGETEXT=&VIEWPOINT=">
@@ -24,11 +20,11 @@
 					<h3><?=$data['manufacturer']?> <?=$data['model']?></h3>
 					<p><?=$data['descr']?></p>
 					<hr>
-						<h6>Business Clients - From <span class="price">£<?=$bch?></span> *</h6>
-						<h6>Personal Clients - From <span class="price">£<?=$pch?></span> **</h6>
+						<h6>Business Clients - From <span class="price">£<?=$bch_rental?></span> *</h6>
+						<h6>Personal Clients - From <span class="price">£<?=$pch_rental?></span> **</h6>
 					<hr>
-					<small>* Based on an initial rental of £<?=number_format(($bch), 2, '.', ',')?> followed by <?=str_replace("M", '', $data['term'])-1?> rentals of £<?=$bch?> and <?=str_replace("K",",000",$data['mileage'])?> miles annually.</small>
-					<br /><small>** Based on an initial rental of £<?=number_format(($pch*3), 2, '.', ',')?> followed by <?=str_replace("M", '', $data['term'])-1?> rentals of £<?=$pch?> and <?=str_replace("K",",000",$data['mileage'])?> miles annually.</small>
+					<small>* Based on an initial rental of £<?=$bch_initial?> followed by <?=str_replace("M", '', $data['term'])-1?> monthly rentals of £<?=$bch_rental?> and <?=str_replace("K",",000",$data['mileage'])?> miles annually.</small>
+					<br /><small>** Based on an initial rental of £<?=$pch_initial?> followed by <?=str_replace("M", '', $data['term'])-1?> monthly rentals of £<?=$pch_rental?> and <?=str_replace("K",",000",$data['mileage'])?> miles annually.</small>
 				</div>
 			</div>
 		</div>
