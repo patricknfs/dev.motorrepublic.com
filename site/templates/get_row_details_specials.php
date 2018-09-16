8b621bc6659b730bcb17ef3659860ab0ce92a3f2
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-16 17:21:55 +0100

    vehicles database feed

--- site/templates/get_row_details_specials.php
+++ site/templates/get_row_details_specials.php
@@ -11,7 +11,7 @@ require_once(MR_PATH . "/inc/conn.php");
 
 // iterate over every row
 $row = 1;
-$forms = $wire('forms');
+$forms = wire('forms');
 $form = $forms->get('Specials_upload');
 
 $entries = array();
