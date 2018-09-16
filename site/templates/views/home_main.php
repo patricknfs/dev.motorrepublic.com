22d69e62b89d6c1d6518aa6a0812451cabe6c371
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-16 16:20:08 +0100

    vehicles database feed

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -55,7 +55,9 @@
 							</h6>
 							<p><?=$vehicle['descr']?></p>
 						</div>
-						<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$vehicle['cap_id']?>&DATE=2018/09/11&WIDTH=300&HEIGHT=225&IMAGETEXT=&VIEWPOINT=">
+						<div class="card-image">
+							<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$vehicle['cap_id']?>&DATE=2018/09/11&WIDTH=300&HEIGHT=225&IMAGETEXT=&VIEWPOINT=">
+						</div>
 						<div class="card-section">
 							<h6>Business Clients <span class="price">£<?=$bch_rental?></span> excl. VAT</h6>
 							<h6>Personal Clients <span class="price">£<?=$pch_rental?></span> inc. VAT</h6>
