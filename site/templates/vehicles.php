e5d941ee67aea6b7a5b9527e44c78ff3992edc96
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-16 19:42:21 +0100

    pagination

--- site/templates/vehicles.php
+++ site/templates/vehicles.php
@@ -41,6 +41,7 @@ if( isset($_POST['manufacturer']) ) {
 }
 else {
   $total_pages_sql = "SELECT COUNT(*) FROM `team`.`rates_combined` GROUP BY `cap_id` ";
+  echo $total_pages_sql;
   $count = mysqli_query($conn,$total_pages_sql);
   $total_rows = mysqli_fetch_array($count)[0];
   $total_pages = ceil($total_rows / $no_of_records_per_page);
