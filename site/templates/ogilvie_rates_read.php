0c27888c61f7f11a8b6714e5b153f88d9d70115a
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-20 16:43:12 +0100

    ogilvie rates

--- site/templates/ogilvie_rates_read.php
+++ site/templates/ogilvie_rates_read.php
@@ -151,7 +151,7 @@ if (($handle = fopen($csv , "r")) !== FALSE) {
         `cap_id` = " . $data[2] . ",
         `CO2` = " . $data[14] . ",
         `vehicle_list_price` = " . $data[7] . ",
-        `vehicle_otr_price` = "",
+        `vehicle_otr_price` = "0",
         `p11d_price` = " . $data[8] . ",
         `updated` = NOW(),
         " . $insert . "
@@ -182,7 +182,7 @@ if (($handle = fopen($csv , "r")) !== FALSE) {
         `cap_id` = " . $data[2] . ",
         `CO2` = " . $data[14] . ",
         `vehicle_list_price` = " . $data[7] . ",
-        `vehicle_otr_price` = "",
+        `vehicle_otr_price` = "0",
         `p11d_price` = " . $data[8] . ",
         `updated` = NOW(),
         " . $insert . "
@@ -213,7 +213,7 @@ if (($handle = fopen($csv , "r")) !== FALSE) {
         `cap_id` = " . $data[2] . ",
         `CO2` = " . $data[14] . ",
         `vehicle_list_price` = " . $data[7] . ",
-        `vehicle_otr_price` = "",
+        `vehicle_otr_price` = "0",
         `p11d_price` = " . $data[8] . ",
         `updated` = NOW(),
         " . $insert . "
@@ -244,7 +244,7 @@ if (($handle = fopen($csv , "r")) !== FALSE) {
         `cap_id` = " . $data[2] . ",
         `CO2` = " . $data[14] . ",
         `vehicle_list_price` = " . $data[7] . ",
-        `vehicle_otr_price` = "",
+        `vehicle_otr_price` = "0",
         `p11d_price` = " . $data[8] . ",
         `updated` = NOW(),
         " . $insert . "
@@ -277,7 +277,7 @@ if (($handle = fopen($csv , "r")) !== FALSE) {
         `cap_id` = " . $data[2] . ",
         `CO2` = " . $data[14] . ",
         `vehicle_list_price` = " . $data[16] . ",
-        `vehicle_otr_price` = "",
+        `vehicle_otr_price` = "0",
         `p11d_price` = " . $data[8] . ",
         `updated` = NOW(),
         " . $insert . "
