bb1a939b9b54dbb402c93e26b60933dea0e434d4
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-23 15:29:29 +0100

    improving franchisee home pages

---
+++ site/templates/testimonials.php
@@ -0,0 +1,7 @@
+<?php
+// about.php
+ob_start();
+include('views/basic-page_main.php');
+$page->main = ob_get_clean();
+include("./main.php"); 
+?>
---
+++ site/templates/views/testimonials_main.php
@@ -0,0 +1,14 @@
+<?php
+// testimonials_main.php
+?>
+<div class="grid-container">
+  <div class="grid-x">
+    <div class="cell">
+      <h1><?php echo $page->title; ?></h1>
+      <?=$page->body?>
+      <div class="sig">
+        <?=$page->sig?>
+      </div>
+    </div>
+  </div>
+</div>
