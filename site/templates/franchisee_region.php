1d59d50b75c38da812669f8215aec3a740d99b29
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-23 17:06:35 +0100

    improving franchisee home pages

--- site/templates/franchisee_region.php
+++ site/templates/franchisee_region.php
@@ -7,18 +7,8 @@ $testimonial_blurb = truncateText($testimonial->body, 250);
 date_default_timezone_set('CET');
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
 require_once(MR_PATH . "/inc/conn.php");
-// if (isset($_GET['pageno'])) {
-//   $pageno = $_GET['pageno'];
-// } else {
-//   $pageno = 1;
-// }
-// $no_of_records_per_page = 4;
-// $offset = ($pageno-1) * $no_of_records_per_page;
-// $total_pages_sql = "SELECT COUNT(*) FROM `team`.`rates_combined` GROUP BY `cap_id` ";
-// $countres = $conn->query($total_pages_sql);
-// $total_rows = $countres->num_rows;
-// $total_pages = ceil($total_rows / $no_of_records_per_page);
-$query = "SELECT `id`,`cap_id`,`cap_code`,`source`,`manufacturer`,`model`,`descr`,`term`,`mileage`,min(`rental`) AS `rental`,`vehicle_list_price`,`vehicle_otr_price`,`p11d_price`,`CO2`,`deal_notes` FROM `team`.`rates_combined` GROUP BY `cap_id` ORDER BY `rental` ASC LIMIT 4";
+
+$query = "SELECT `id`,`cap_id`,`cap_code`,`source`,`manufacturer`,`model`,`descr`,`term`,`mileage`,min(`rental`) AS `rental`,`vehicle_list_price`,`vehicle_otr_price`,`p11d_price`,`CO2`,`deal_notes` FROM `team`.`rates_combined` WHERE `manufacturer` LIKE 'mercedes||jaguar||bmw' GROUP BY `cap_id` ORDER BY `rental` ASC LIMIT 4";
 
 // echo $query;
 $result = $conn->query($query) or die(mysqli_error($conn));
