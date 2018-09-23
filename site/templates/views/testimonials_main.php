8aa1f5542af51acc9ce09307d9fad5725565f687
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-23 15:40:33 +0100

    improving franchisee home pages

--- site/templates/views/testimonials_main.php
+++ site/templates/views/testimonials_main.php
@@ -7,7 +7,12 @@
       <h1><?php echo $page->title; ?></h1>
       <?=$page->body?>
       <div class="sig">
-        <?=$page->sig?>
+        <?php
+        $max_testimonials = 1;
+        $testimonial = $pages->find("template=testimonial, sort=random, limit=$max");
+        ?>
+        <?=$testimonial->body?>
+        <?=$testimonial->sig?>
       </div>
     </div>
   </div>
