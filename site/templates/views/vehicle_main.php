e5a0dfc36a25cf4951bd8cdbb35827aa676448ef
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-24 19:14:29 +0100

    looping out the standard specification

--- site/templates/views/vehicle_main.php
+++ site/templates/views/vehicle_main.php
@@ -58,7 +58,7 @@
 						foreach($groups2 AS $group2){
 							echo "<ul><strong>" . $group2 . "</strong>";
 							foreach($tech_data as $item2){
-								echo $item->Dc_Description . " = " . $group . "<br />";
+								// echo $item->Dc_Description . " = " . $group . "<br />";
 								if(trim($item2->Dc_Description) === trim($group2)){
 									echo "<li><label>" . $item2->DT_LongDescription . "</label>".  $item2->tech_value_string . "</li>";
 								}
