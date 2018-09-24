de47cae7d59487359e087aa10c0b67f0af1af1fc
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-24 09:41:15 +0100

    hooking up cap data vehicle spec

--- site/templates/views/vehicle_main.php
+++ site/templates/views/vehicle_main.php
@@ -9,7 +9,7 @@
 	</div>
 	<div class="grid-x grid-padding-x">
 		<?php
-		var_dump($data);
+		// var_dump($data);
 		?>
 		<div class="cell small-12 medium-8">
 			<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$input->urlSegment1;?>&DATE=2018/09/11&WIDTH=800&HEIGHT=600&IMAGETEXT=&VIEWPOINT=">
