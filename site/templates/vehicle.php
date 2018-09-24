55d2056158690cfb4638aa4b4a03b8648fb32ca4
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-24 10:57:24 +0100

    hooking up cap data vehicle spec

--- site/templates/vehicle.php
+++ site/templates/vehicle.php
@@ -16,26 +16,26 @@ require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templ
 require_once(MR_PATH . "/inc/conn.php");
 include "inc/functions.php";
 // print_r($_POST);
-// try
-// {
-//   if(isset($input->urlSegment1)) {
-//     $query = "SELECT `id`,`cap_id`,`cap_code`,`source`,`manufacturer`,`model`,`descr`,`term`,`mileage`,min(`rental`) AS `rental`,`vehicle_list_price`,`vehicle_otr_price`,`p11d_price`,`CO2`,`deal_notes` FROM `team`.`rates_combined` WHERE `cap_id` = " . $input->urlSegment1 . " ORDER BY `rental` ASC LIMIT 1";
-//     echo $query;
-//     $result = $conn->query($query) or die(mysqli_error($conn));
-//     $data = $result->fetch_assoc();
+try
+{
+  if(isset($input->urlSegment1)) {
+    $query = "SELECT `id`,`cap_id`,`cap_code`,`source`,`manufacturer`,`model`,`descr`,`term`,`mileage`,min(`rental`) AS `rental`,`vehicle_list_price`,`vehicle_otr_price`,`p11d_price`,`CO2`,`deal_notes` FROM `team`.`rates_combined` WHERE `cap_id` = " . $input->urlSegment1 . " ORDER BY `rental` ASC LIMIT 1";
+    echo $query;
+    $result = $conn->query($query) or die(mysqli_error($conn));
+    $data = $result->fetch_assoc();
 
-//     $bch_rental = number_format(((($data['rental'] * $data['term']) + 300) / ($data['term']+2)), 2, '.', ',');
-//     $pch_rental = number_format(((($data['rental'] * $data['term']) + 300) / ($data['term']+2)*1.2), 2, '.', ',');
-//     $bch_initial = number_format((((($data['rental'] * $data['term']) + 300) / ($data['term']+2))*3), 2, '.', ',');
-//     $pch_initial = number_format((((($data['rental'] * $data['term']) + 300) / ($data['term']+2)*1.2)*3), 2, '.', ',');
+    $bch_rental = number_format(((($data['rental'] * $data['term']) + 300) / ($data['term']+2)), 2, '.', ',');
+    $pch_rental = number_format(((($data['rental'] * $data['term']) + 300) / ($data['term']+2)*1.2), 2, '.', ',');
+    $bch_initial = number_format((((($data['rental'] * $data['term']) + 300) / ($data['term']+2))*3), 2, '.', ',');
+    $pch_initial = number_format((((($data['rental'] * $data['term']) + 300) / ($data['term']+2)*1.2)*3), 2, '.', ',');
 
-//     $hashcode = strtoupper(md5("173210NfS4JeCAR" . $input->urlSegment1));
-//   }
-// }
-// catch(Exception $e){ 
-//   echo $e->getCode(). '<br />'. $e->getMessage();
-//   echo "We 're out of umbrellas, but the drink itself is fine";
-// }
+    $hashcode = strtoupper(md5("173210NfS4JeCAR" . $input->urlSegment1));
+  }
+}
+catch(Exception $e){ 
+  echo $e->getCode(). '<br />'. $e->getMessage();
+  echo "We 're out of umbrellas, but the drink itself is fine";
+}
 
 try
 {
