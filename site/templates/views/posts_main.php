659cefc3b69ee5745e05378aaa40c1e8e6b54228
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-25 22:02:50 +0100

    Setting up regional blog

--- site/templates/views/posts_main.php
+++ site/templates/views/posts_main.php
@@ -10,17 +10,19 @@
 		//$rev_articles = array_reverse( $articles );
 		foreach ($articles as $article) { ?>
 			<div class="article-item grid-container">
-			   	<div class="grid-x small-12 large-3">
-			   		<a href="<?=$article->url?>">
-			     		<img src="<?=$article->images->first()->url?>" />
-			     	</a>
-			    </div>
-			    <div class="grid-x small-12 large-9" itemprop="blogPost">
-			    	<a href="<?=$article->url?>">
-			     		<h3 ><?= $article->title ?></h3>
-			     	</a>
-			     	<p><?=$article->summary?> <i><a href="<?=$article->url?>">read more...</a></i></p>
-			   	</div>	
+        <div class="grid-x">
+          <div class="cell small-12 medium-4">
+            <a href="<?=$article->url?>">
+              <img src="<?=$article->images->first()->url?>" />
+            </a>
+          </div>
+          <div class="cell small-12 medium-8" itemprop="blogPost">
+            <a href="<?=$article->url?>">
+              <h3 ><?= $article->title ?></h3>
+            </a>
+            <p><?=$article->summary?> <i><a href="<?=$article->url?>">read more...</a></i></p>
+          </div>	
+        </div>
 			</div>
 			<hr />
 		<?php 
