1ce7c8c373e58cb9d7f3ea015e78bbc1e0aae147
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-25 11:22:59 +0100

    looping out the standard specification

--- prot/server_processing.php
+++ prot/server_processing.php
@@ -42,7 +42,7 @@ $columns = array(
 	array( 'db' => 'vehicle_otr_price', 'dt' => 'vehicle_otr_price' ),
 	array( 'db' => 'p11d_price', 'dt' => 'p11d_price' ),
 	array( 'db' => 'CO2', 'dt' => 'CO2' ),
-	array( 'db' => 'deal_notes', 'deal_notes' => 'CO2' )
+	array( 'db' => 'deal_notes', 'dt' => 'deal_notes' )
 );
 
 // SQL server connection information
