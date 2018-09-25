3c7fbcb2aff9bdcdb3111992b8bbe44baf5f9276
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-09-25 22:35:56 +0100

    Setting up regional blog

--- site/templates/views/posts_main.php
+++ site/templates/views/posts_main.php
@@ -1,32 +1,34 @@
 <?php
 //posts_main.php template
 ?>
-<div class="grid-container" itemscope itemtype="http://schema.org/Blog">
-	<div class="grid-x small-12">
-		<h1>The Latest News from Motor Republic Gravesend</h1>
-	</div>
-	<div class="grid-x small-12">
-		<?php
-		//$rev_articles = array_reverse( $articles );
-		foreach ($articles as $article) { ?>
-			<div class="article-item grid-container">
-        <div class="grid-x">
-          <div class="cell small-12 medium-4">
-            <a href="<?=$article->url?>">
-              <img src="<?=$article->images->first()->url?>" />
-            </a>
+<section id="posts">
+  <div class="grid-container" itemscope itemtype="http://schema.org/Blog">
+    <div class="grid-x small-12">
+      <h1>The Latest News from Motor Republic Gravesend</h1>
+    </div>
+    <div class="grid-x small-12">
+      <?php
+      //$rev_articles = array_reverse( $articles );
+      foreach ($articles as $article) { ?>
+        <div class="article-item grid-container">
+          <div class="grid-x">
+            <div class="cell small-12 medium-4">
+              <a href="<?=$article->url?>">
+                <img src="<?=$article->images->first()->url?>" />
+              </a>
+            </div>
+            <div class="cell small-12 medium-8" itemprop="blogPost">
+              <a href="<?=$article->url?>">
+                <h3 ><?= $article->title ?></h3>
+              </a>
+              <p><?=$article->summary?> <i><a href="<?=$article->url?>">read more...</a></i></p>
+            </div>	
           </div>
-          <div class="cell small-12 medium-8" itemprop="blogPost">
-            <a href="<?=$article->url?>">
-              <h3 ><?= $article->title ?></h3>
-            </a>
-            <p><?=$article->summary?> <i><a href="<?=$article->url?>">read more...</a></i></p>
-          </div>	
         </div>
-			</div>
-			<hr />
-		<?php 
-		}
-		?>
-	</div>
-</div>
+        <hr />
+      <?php 
+      }
+      ?>
+    </div>
+  </div>
+</section>
--- site/templates/views/vehicles_main.php
+++ site/templates/views/vehicles_main.php
@@ -1,64 +1,66 @@
-<div class="grid-container">
-	<div class="grid-x">
-		<div class="cell small-12">
-			<div class="text-center">
-				<!-- <h3><?=$page->title?></h3> -->
-				<?php echo $forms->embed('vehicle_power_search'); ?>
+<section id="vehicles">
+	<div class="grid-container">
+		<div class="grid-x">
+			<div class="cell small-12">
+				<div class="text-center">
+					<!-- <h3><?=$page->title?></h3> -->
+					<?php echo $forms->embed('vehicle_power_search'); ?>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="grid-x align-center">
-		<nav class="nav_pagination" aria-label="Pagination">
-			<ul class="pagination text-center">
-				<li><a href="?pageno=1">First</a></li>
-				<li class="pagination-previous <?php if($pageno <= 1){ echo 'disabled'; } ?>">
-					<a href="<?php echo ($pageno <= 1 ? '#' : '?pageno=' . ($pageno - 1)) ?>">Previous <span class="show-for-sr">page</span></a>
-				</li>
-				<li class="pagination-next <?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
-					<a href="<?php echo ($pageno >= $total_pages ? '#' : '?pageno=' . ($pageno + 1)) ?>"  aria-label="Next page">Next <span class="show-for-sr">page</span></a>
-				</li>
-				<li><a href="?pageno=<?php echo $total_pages; ?>">Last</a></li>
-			<ul>
-		</nav>
-	</div>
-	<div class="grid-x grid-padding-x small-up-2 medium-up-4">
-		<?php
-		foreach($result AS $vehicle) {
-			$options = array(
-				'quality' => 80,
-				'upscaling' => false       
-			);
+		<div class="grid-x align-center">
+			<nav class="nav_pagination" aria-label="Pagination">
+				<ul class="pagination text-center">
+					<li><a href="?pageno=1">First</a></li>
+					<li class="pagination-previous <?php if($pageno <= 1){ echo 'disabled'; } ?>">
+						<a href="<?php echo ($pageno <= 1 ? '#' : '?pageno=' . ($pageno - 1)) ?>">Previous <span class="show-for-sr">page</span></a>
+					</li>
+					<li class="pagination-next <?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
+						<a href="<?php echo ($pageno >= $total_pages ? '#' : '?pageno=' . ($pageno + 1)) ?>"  aria-label="Next page">Next <span class="show-for-sr">page</span></a>
+					</li>
+					<li><a href="?pageno=<?php echo $total_pages; ?>">Last</a></li>
+				<ul>
+			</nav>
+		</div>
+		<div class="grid-x grid-padding-x small-up-2 medium-up-4">
+			<?php
+			foreach($result AS $vehicle) {
+				$options = array(
+					'quality' => 80,
+					'upscaling' => false       
+				);
 
-			$bch_rental = number_format(((($vehicle['rental'] * $vehicle['term']) + 300) / ($vehicle['term']+2)), 2, '.', ',');
-			$pch_rental = number_format(((($vehicle['rental'] * $vehicle['term']) + 300) / ($vehicle['term']+2)*1.2), 2, '.', ',');
+				$bch_rental = number_format(((($vehicle['rental'] * $vehicle['term']) + 300) / ($vehicle['term']+2)), 2, '.', ',');
+				$pch_rental = number_format(((($vehicle['rental'] * $vehicle['term']) + 300) / ($vehicle['term']+2)*1.2), 2, '.', ',');
 
-			$hashcode = strtoupper(md5("173210NfS4JeCAR" . $vehicle['cap_id']));
+				$hashcode = strtoupper(md5("173210NfS4JeCAR" . $vehicle['cap_id']));
+				?>
+				<div class="cell">
+					<a href="/vehicle/<?=$vehicle['cap_id']?>">
+						<div class="card">
+							<div class="card-section">
+								<h6>
+									<?=$vehicle['manufacturer']?> <?=$vehicle['model']?>
+								</h6>
+								<p><?=$vehicle['descr']?></p>
+							</div>
+							<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$vehicle['cap_id']?>&DATE=2018/09/11&WIDTH=300&HEIGHT=225&IMAGETEXT=&VIEWPOINT=">
+							<div class="card-section">
+								<h6>Business Clients<br /><span class="price">£<?=$bch_rental?></span>/mth excl. VAT</h6>
+								<h6>Personal Clients<br /><span class="price">£<?=$pch_rental?></span>/mth inc. VAT</h6>
+								<small>click for more details...</small>
+							</div>
+						</div>
+					</a>
+				</div>
+			<?php
+			}
 			?>
-			<div class="cell">
-				<a href="/vehicle/<?=$vehicle['cap_id']?>">
-					<div class="card">
-						<div class="card-section">
-							<h6>
-								<?=$vehicle['manufacturer']?> <?=$vehicle['model']?>
-							</h6>
-							<p><?=$vehicle['descr']?></p>
-						</div>
-						<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$vehicle['cap_id']?>&DATE=2018/09/11&WIDTH=300&HEIGHT=225&IMAGETEXT=&VIEWPOINT=">
-						<div class="card-section">
-							<h6>Business Clients<br /><span class="price">£<?=$bch_rental?></span>/mth excl. VAT</h6>
-							<h6>Personal Clients<br /><span class="price">£<?=$pch_rental?></span>/mth inc. VAT</h6>
-							<small>click for more details...</small>
-						</div>
-					</div>
-				</a>
-			</div>
-		<?php
-		}
-		?>
-	</div>
-	<div class="grid-x">
-		<div class="cell small-12">
+		</div>
+		<div class="grid-x">
+			<div class="cell small-12">
 
+			</div>
 		</div>
 	</div>
-</div>
+</section>
