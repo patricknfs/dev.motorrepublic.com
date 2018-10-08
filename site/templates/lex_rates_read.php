53614979e6bc913f7e5704bba87dad069f8e2e0a
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-10-08 14:45:36 +0100

    load cap vehicle data

--- site/templates/lex_rates_read.php
+++ site/templates/lex_rates_read.php
@@ -21,7 +21,7 @@ if (($handle = fopen($csv , "r")) !== FALSE) {
   while (($rawdata = fgetcsv($handle, 0, ",")) !== FALSE) {
     // print_r($rawdata);
     $num = count($rawdata);
-    $data = preg_replace('/\s+/', '', $rawdata);
+    // $data = preg_replace('/\s+/', '', $rawdata);
     // $data = str_replace('£','',$data);
     // $data = str_replace('#N/A',NULL,$data);
     if($row > 2){
