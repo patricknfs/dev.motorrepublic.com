34630819d037985a5ba9e311b09a1ac9d046f27b
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-10-14 18:51:06 +0100

    remove search box

--- site/templates/vehicles.php
+++ site/templates/vehicles.php
@@ -30,9 +30,7 @@ if( isset($_POST['manufacturer']) ) {
   $total_pages = ceil($total_rows / $no_of_records_per_page);
   $manuf = filter_var($_POST['manufacturer'], FILTER_SANITIZE_STRING);
   $model = filter_var($_POST['model'], FILTER_SANITIZE_STRING);
-  $mileage = filter_var($_POST['mileage'], FILTER_SANITIZE_STRING);
-  $months = filter_var($_POST['months'], FILTER_SANITIZE_STRING);
-  $query = "SELECT `id`,`cap_id`,`cap_code`,`source`,`manufacturer`,`model`,`descr`,`term`,`mileage`,min(`rental`) AS `rental`,`vehicle_list_price`,`vehicle_otr_price`,`p11d_price`,`CO2`,`deal_notes` FROM `team`.`rates_combined` WHERE `manufacturer` = '" . $manuf . "' AND `model` LIKE '%" . $model . "%' AND `term` = '" . $months . "' AND `mileage` = '" . $mileage . "'  GROUP BY `cap_id` ORDER BY `rental` ASC LIMIT $offset, $no_of_records_per_page";
+  $query = "SELECT `id`,`cap_id`,`cap_code`,`source`,`manufacturer`,`model`,`descr`,`term`,`mileage`,min(`rental`) AS `rental`,`vehicle_list_price`,`vehicle_otr_price`,`p11d_price`,`CO2`,`deal_notes` FROM `team`.`rates_combined` WHERE `manufacturer` = '" . $_POST['manufacturer'] . "' AND `model` LIKE '%" . $_POST['model'] . "%' GROUP BY `cap_id` ORDER BY `rental` ASC LIMIT $offset, $no_of_records_per_page";
 }
 else {
   $total_pages_sql = "SELECT COUNT(*) FROM `team`.`rates_combined` GROUP BY `cap_id` ";
