6ed55dbf71593b804c86bdbe79dd1bae6af66caf
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-10-15 22:02:19 +0100

    prize draw page

--- site/templates/views/prize_draw_main.php
+++ site/templates/views/prize_draw_main.php
@@ -1,5 +1,5 @@
 <?php
-// prize draw_main.php
+// basic-page_main.php
 ?>
 <div class="grid-container">
   <div class="grid-x">
@@ -68,67 +68,6 @@
     <div class="content__gdprLegal">
         <p>We use Mailchimp as our marketing platform. By clicking below to subscribe, you acknowledge that your information will be transferred to Mailchimp for processing. Learn more about Mailchimp's privacy practices <a href="https://mailchimp.com/legal/" target="_blank">here</a>.</p>
     </div>
-</div>
-	<div id="mce-responses" class="clear">
-		<div class="response" id="mce-error-response" style="display:none"></div>
-		<div class="response" id="mce-success-response" style="display:none"></div>
-	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_77b83085a473bef18ca626a93_528e7c6883" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-    </div>
-</form>
-</div>
-<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='POSTCODE';ftypes[3]='text';fnames[4]='COMPANY';ftypes[4]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-<!--End mc_embed_signup--><!-- Begin Mailchimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
-<style type="text/css">
-	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-</style>
-<style type="text/css">
-	#mc-embedded-subscribe-form input[type=checkbox]{display: inline; width: auto;margin-right: 10px;}
-	#mergeRow-gdpr {margin-top: 20px;}
-	#mergeRow-gdpr fieldset label {font-weight: normal;}
-	#mc-embedded-subscribe-form .mc_fieldset{border:none;min-height: 0px;padding-bottom:0px;}
-</style>
-<div id="mc_embed_signup">
-<form action="https://motorrepublic.us11.list-manage.com/subscribe/post?u=77b83085a473bef18ca626a93&amp;id=528e7c6883" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-	<h2>Subscribe to our mailing list</h2>
-<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-<div class="mc-field-group">
-	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-</label>
-	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-</div>
-<div class="mc-field-group">
-	<label for="mce-FNAME">First Name  <span class="asterisk">*</span>
-</label>
-	<input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
-</div>
-<div class="mc-field-group">
-	<label for="mce-LNAME">Last Name  <span class="asterisk">*</span>
-</label>
-	<input type="text" value="" name="LNAME" class="required" id="mce-LNAME">
-</div>
-<div class="mc-field-group">
-	<label for="mce-POSTCODE">First part of PostCode  <span class="asterisk">*</span>
-</label>
-	<input type="text" value="" name="POSTCODE" class="required" id="mce-POSTCODE">
-</div>
-<div class="mc-field-group">
-	<label for="mce-COMPANY">Company (If applicable) </label>
-	<input type="text" value="" name="COMPANY" class="" id="mce-COMPANY">
-</div>
-<div class="mc-field-group input-group">
-    <strong>I would like to be contacted </strong>
-    <ul><li><input type="checkbox" value="1" name="group[21][1]" id="mce-group[21]-21-0"><label for="mce-group[21]-21-0">Weekly Offers By Email</label></li>
-<li><input type="checkbox" value="2" name="group[21][2]" id="mce-group[21]-21-1"><label for="mce-group[21]-21-1">Monthly News Email</label></li>
-<li><input type="checkbox" value="4" name="group[21][4]" id="mce-group[21]-21-2"><label for="mce-group[21]-21-2">By Phone</label></li>
-<li><input type="checkbox" value="8" name="group[21][8]" id="mce-group[21]-21-3"><label for="mce-group[21]-21-3">By SMS</label></li>
-<li><input type="checkbox" value="16" name="group[21][16]" id="mce-group[21]-21-4"><label for="mce-group[21]-21-4">By Post</label></li>
-</ul>
 </div>
 	<div id="mce-responses" class="clear">
 		<div class="response" id="mce-error-response" style="display:none"></div>
