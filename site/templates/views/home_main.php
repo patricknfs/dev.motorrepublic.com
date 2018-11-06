7fc366828ed2f0c6d8fd1e6d91c22a7dea6ee7d4
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-06 13:48:45 +0000

    fixing megamenu

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -24,13 +24,7 @@
 		
 		</div>
 		<div class="cell small-12 medium-4">
-			<div id="message">
-				<h1>Deal of the Day</h1>
-				<img src="<?=$config->urls->assets?>images/ford_fiesta_st2.jpg">
-				<h4>THE ALL NEW FIESTA ST2</h4>
-				<p>Available in stock now from £215.99 inc. VAT</p>
-				<p><a href="#">More details...</a></p>
-			</div>
+
 		</div>
 	</div>
 </section>
@@ -81,6 +75,13 @@
 					</div>
 			</div>
 			<div class="cell small-12 medium-4">
+				<div id="message">
+					<h1>Deal of the Day</h1>
+					<img src="<?=$config->urls->assets?>images/ford_fiesta_st2.jpg">
+					<h4>THE ALL NEW FIESTA ST2</h4>
+					<p>Available in stock now from £215.99 inc. VAT</p>
+					<p><a href="#">More details...</a></p>
+				</div>
 				<h3>Magazine Article</h3>
 				<p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
 				<h3>Regional Strength?</h3>
