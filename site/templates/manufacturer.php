3eb34518ec1a3dcbe51d8c119d5bbe15c9040467
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-07 16:01:11 +0000

    sorting vehicle search forms

--- site/templates/manufacturer.php
+++ site/templates/manufacturer.php
@@ -4,7 +4,7 @@ date_default_timezone_set('CET');
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
 require_once(MR_PATH . "/inc/conn.php");
 
-$query = "SELECT DISTINCT(`manufacturer`) FROM `team`.`vehicles` ORDER BY `manufacturer` ASC"
+$query = "SELECT DISTINCT(`manufacturer`) FROM `team`.`vehicles` ORDER BY `manufacturer` ASC";
 $result = mysqli_query($conn, $query);
 ?>
 <ul>
