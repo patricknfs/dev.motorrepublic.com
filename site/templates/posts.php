3acdcac7b4472f22dab5927a46c68075b55e19b0
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-09 15:50:10 +0000

    regional pages

--- site/templates/posts.php
+++ site/templates/posts.php
@@ -3,12 +3,37 @@
 include("inc/functions.php"); 
 echo $page->parent->name;
 switch($page->parent->name){
+  case "motor-republic-ayrshire":
+  $region = "ayrshire";
+  break;
+  case "motor-republic-bristol":
+  $region = "bristol";
+  break;
   case "motor-republic-cardiff":
   $region = "cardiff";
   break;
-  case "motor-republic-bristol":
-  $region = "cardiff";
+  case "motor-republic-canary-wharf":
+  $region = "canary-wharf";
   break;
+  case "motor-republic-gravesend":
+  $region = "gravesend";
+  break;
+  case "motor-republic-maidstone":
+  $region = "maidstone";
+  break;
+  case "motor-republic-norwich":
+  $region = "norwich";
+  break;
+  case "motor-republic-portsmouth-iow":
+  $region = "portsmouth-iow";
+  break;
+  case "motor-republic-reading":
+  $region = "reading";
+  break;
+  case "motor-republic-sheffield-north":
+  $region = "sheffield-north";
+  break;
+
 }
 $articles = $pages->get("/regions/' . $region . '/blog/, limit=10, sort=-date")->children;
 wire()->addHook("Page::wordLimiter", null, "wordLimiter");
