458967aea15b9251de62e03b7fe46b226c6c93a9
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-13 14:50:23 +0000

    Sort basic page template

--- site/templates/views/basic-page_main.php
+++ site/templates/views/basic-page_main.php
@@ -4,11 +4,11 @@
 <div class="grid-container">
   <div class="grid-x">
     <div class="cell">
-    <?php
+      <?php
       $image = $page->images->first();
       ?>
-      <img src="<?=$image->url?>" />
-      <h1><?=$page->title; ?></h1>
+      <img class="floatright" src="<?=$image->url?>" />
+      <h1><?=$page->title?></h1>
       <?=$page->body?>
       </div>
   </div>
