204fb4aa809bfdde3d497f5e1701a04d30261f58
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-13 15:25:56 +0000

    Sort basic page template

--- site/templates/views/whatwedo_main.php
+++ site/templates/views/whatwedo_main.php
@@ -1,15 +1,15 @@
 <?php
 // whatwedo_main.php
+$image = $page->images->first();
 ?>
 <section id="whatwedo">
+  <div class="grid-container full" style="background-image: url('<?=$image->url?>)">
+  <h1><?=$page->title?></h1>  
+  <?=$page->summary?>
+  </div>
   <div class="grid-container">
     <div class="grid-x">
       <div class="cell">
-        <?php
-        $image = $page->images->first();
-        ?>
-        <img class="floatright" src="<?=$image->url?>" />
-        <h1><?=$page->title?></h1>
         <?=$page->body?>
         </div>
     </div>
