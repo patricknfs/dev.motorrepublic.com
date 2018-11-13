eec3333ed48efef19e043a3b1a2dee88a1fbc070
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-13 15:55:25 +0000

    Sort basic page template

--- site/templates/views/whatwedo_main.php
+++ site/templates/views/whatwedo_main.php
@@ -3,17 +3,19 @@
 $image = $page->images->first();
 ?>
 <section id="whatwedo">
-  <div class="grid-container full" style="background: url('<?=$image->url?>')  no-repeat center center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;">
-    <div class="grid-x hero">
-      <div class="cell small-12 medium-6">
-        <h1><?=$page->title?></h1>  
-        <?=$page->summary?>
-      </div>
-      <div class="cell small-12 medium-6">
-      
+  <div class="hero">
+    <div class="grid-container" style="background: url('<?=$image->url?>')  no-repeat center center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;">
+      <div class="grid-x hero">
+        <div class="cell small-12 medium-6">
+          <h1><?=$page->title?></h1>  
+          <?=$page->summary?>
+        </div>
+        <div class="cell small-12 medium-6">
+        
+        </div>
       </div>
     </div>
-  </div>
+</div>
   <div class="grid-container">
     <div class="grid-x">
       <div class="cell">
