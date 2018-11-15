e8ffde2e6f407358f1b7efb598780a28194e8b86
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-15 15:18:20 +0000

    working on dropdown

--- site/ready.php
+++ site/ready.php
@@ -1,6 +1,6 @@
 <?php
-$forms->addHookBefore('InputfieldForm::render', function($event) {
-  $processor = $event->object;
+$forms->addHookBefore('InputfieldForm::render', function($E) {
+  $processor = $e->object;
   $form = $e->arguments('vehicle_power_search');
   if($processor->formName == 'vehicle_power_search') {
     return "testing hook";
