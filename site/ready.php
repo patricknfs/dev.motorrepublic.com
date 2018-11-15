36d4d86b3e071ee66ed57993ed7c44a49bea4418
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-15 15:36:47 +0000

    working on dropdown

--- site/ready.php
+++ site/ready.php
@@ -3,7 +3,9 @@ $forms->addHookBefore('InputfieldForm::render', function($e) {
   $processor = $e->object;
   $form = $e->arguments('vehicle_power_search');
   if($processor->formName == 'vehicle_power_search') {
-    return "testing hook";
+    $f = $form->getChildByName('select_options');
+    if($f) $f->addOption('testing hook'); 
+    // return "testing hook";
   }
   // if(strpos($form->id, 'FormBuilder') !== 0 || $form->name != 'vehicle_power_search') return;
   // $f = $form->getChildByName('select_options');
