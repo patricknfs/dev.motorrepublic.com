421fc576fbf449c52bfa1cbb38a30a219b051c6c
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-16 10:25:52 +0000

    working on dropdown

--- site/ready.php
+++ site/ready.php
@@ -2,9 +2,9 @@
 $forms->addHookBefore('InputfieldForm::render', function($e) {
   $processor = $e->object;
   $form = $e->arguments('form');
-  if($processor->formName == 'vehicle_power_search') {
-    $f = $form->getChildByName('manufacturer');
-    if($f) $f->addOption('testing hook'); 
+  if($processor->formName == 'vehicle_power_search') { //form name
+    $f = $form->getChildByName('manufacturer'); //form field name
+    if($f) $f->addOption('testing hook'); //selector test - replace with resutls from non pw query query
   }
   // if(strpos($form->id, 'FormBuilder') !== 0 || $form->name != 'vehicle_power_search') return;
   // $f = $form->getChildByName('select_options');
