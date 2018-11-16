d4464d70356c688a59bf9c25f39b6bddf04374fc
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-16 12:30:00 +0000

    working on dropdown

--- site/ready.php
+++ site/ready.php
@@ -1,23 +1,27 @@
-<?php
-// $forms->addHookBefore('InputfieldForm::render', function($e) {
-//   $processor = $e->object;
-//   $form = $e->arguments('form');
-//   if($processor->formName == 'vehicle_power_search') { //form name
-//     $f = $form->getChildByName('manufacturer'); //form field name
-//     if($f) $f->addOption('testing hook'); //selector test - replace with resutls from non pw query query
-//   }
-// });
-
-$forms->addHookBefore('FormBuilderProcessor::render', function($e) {
+<!-- <?php
+$forms->addHookBefore('InputfieldForm::render', function($e) {
   if ($e->object->name != 'manufacturer') return; // quick exit if fieldname doesn't match
   $processor = $e->object;
-  $form = $processor->getInputfieldsForm();
-  if($form->name == 'vehicle_power_search') {
-    $my_select = $form->getChildByName('manufacturer');
-    print_r($my_select);
-    $options = ['red', 'blue', 'green']; // replace with your dynamic options
+  $form = $e->arguments('form');
+  if($processor->formName == 'vehicle_power_search') { //form name
+    $f = $form->getChildByName('manufacturer'); //form field name
+    $options = ['red', 'blue', 'green']; // replace with your dynamic options -->
     foreach($options as $option) {
       $my_select->addOption($option);
     }
   }
 });
+
+// $forms->addHookBefore('FormBuilderProcessor::render', function($e) {
+//   if ($e->object->name != 'manufacturer') return; // quick exit if fieldname doesn't match
+//   $processor = $e->object;
+//   $form = $processor->getInputfieldsForm();
+//   if($form->name == 'vehicle_power_search') {
+//     $my_select = $form->getChildByName('manufacturer');
+//     print_r($my_select);
+//     $options = ['red', 'blue', 'green']; // replace with your dynamic options
+//     foreach($options as $option) {
+//       $my_select->addOption($option);
+//     }
+//   }
+// });
