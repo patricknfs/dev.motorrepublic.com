2dbd783c91cc7cd15ca4bc55e11b58c161daa46f
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-16 11:01:51 +0000

    working on dropdown

--- site/ready.php
+++ site/ready.php
@@ -1,12 +1,21 @@
 <?php
-$forms->addHookBefore('InputfieldForm::render', function($e) {
-  $processor = $e->object;
-  $form = $e->arguments('form');
-  if($processor->formName == 'vehicle_power_search') { //form name
-    $f = $form->getChildByName('manufacturer'); //form field name
-    if($f) $f->addOption('testing hook'); //selector test - replace with resutls from non pw query query
+// $forms->addHookBefore('InputfieldForm::render', function($e) {
+//   $processor = $e->object;
+//   $form = $e->arguments('form');
+//   if($processor->formName == 'vehicle_power_search') { //form name
+//     $f = $form->getChildByName('manufacturer'); //form field name
+//     if($f) $f->addOption('testing hook'); //selector test - replace with resutls from non pw query query
+//   }
+// });
+
+$forms->addHookBefore('FormBuilderProcessor::render', function($event) {
+  $processor = $event->object;
+  $form = $processor->getInputfieldsForm();
+  if($form->name == 'vehicle_power_search') {
+      $my_select = $form->get('manufacturer');
+      $options = ['red', 'blue', 'green']; // replace with your dynamic options
+      foreach($options as $option) {
+          $my_select->addOption($option);
+      }
   }
-  // if(strpos($form->id, 'FormBuilder') !== 0 || $form->name != 'vehicle_power_search') return;
-  // $f = $form->getChildByName('select_options');
-  // if($f) $f->addOption('added option'); 
-});
+}); 
