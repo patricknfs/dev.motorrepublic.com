00cd64853390a5955f9d61d632a41a029cd241d9
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-16 14:44:52 +0000

    working on dropdown

--- site/ready.php
+++ site/ready.php
@@ -1,31 +1,31 @@
 <?php
-// $forms->addHookBefore('InputfieldForm::render', function($e) {
-//   // print_r($e->object->name);
-//   // if ($e->object->name != 'vehicle_power_search') return; // quick exit if form name doesn't match
-//   $processor = $e->object;
-//   $form = $e->arguments('vehicle_power_search');
-
-//   if($processor->formName == 'vehicle_power_search') { //form name
-//     $f = $form->getChildByName('manufacturer'); //form field name
-//     echo "form is " . $f;
-//     $options = ['red', 'blue', 'green']; // replace with your dynamic options -->
-//     foreach($options as $option) {
-//       $f->addOption($option);
-//     }
-//   }
-// });
-
-$forms->addHookBefore('FormBuilderProcessor::render', function($e) {
-  if ($e->object->name != 'vehicle_power_search') return; // quick exit if form name doesn't match
-  print_r($e->object->name);
+$forms->addHookBefore('InputfieldForm::render', function($e) {
+  // print_r($e->object->name);
+  // if ($e->object->name != 'vehicle_power_search') return; // quick exit if form name doesn't match
   $processor = $e->object;
-  $form = $processor->getInputfieldsForm();
-  if($form->name == 'vehicle_power_search') {
-    $my_select = $form->getChildByName('manufacturer');
-    print_r($form->getChildByName());
-    $options = ['red', 'blue', 'green']; // replace with your dynamic options
+  $form = $e->arguments('vehicle_power_search');
+
+  if($processor->formName == 'vehicle_power_search') { //form name
+    $f = $form->getChildByName('manufacturer'); //form field name
+    echo "form is " . $f;
+    $options = ['red', 'blue', 'green']; // replace with your dynamic options -->
     foreach($options as $option) {
-      $my_select->addOption($option);
+      $f->addOption($option);
     }
   }
 });
+
+// $forms->addHookBefore('FormBuilderProcessor::render', function($e) {
+//   if ($e->object->name != 'vehicle_power_search') return; // quick exit if form name doesn't match
+//   print_r($e->object->name);
+//   $processor = $e->object;
+//   $form = $processor->getInputfieldsForm();
+//   if($form->name == 'vehicle_power_search') {
+//     $my_select = $form->getChildByName('manufacturer');
+//     print_r($form->getChildByName());
+//     $options = ['red', 'blue', 'green']; // replace with your dynamic options
+//     foreach($options as $option) {
+//       $my_select->addOption($option);
+//     }
+//   }
+// });
