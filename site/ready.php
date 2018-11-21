92ed5b2d043ffcfb243ba01de4c61e24a319ae02
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-21 09:08:04 +0000

    working on dropdown

--- site/ready.php
+++ site/ready.php
@@ -1,19 +1,30 @@
 <?php
-$forms->addHookBefore('InputfieldForm::render', function($e) {
-  print_r($e);
-  print_r($e->object->name);
-  // if ($e->object->name != 'vehicle_power_search') return; // quick exit if form name doesn't match
+// $forms->addHookBefore('InputfieldForm::render', function($e) {
+//   print_r($e);
+//   print_r($e->object->name);
+//   // if ($e->object->name != 'vehicle_power_search') return; // quick exit if form name doesn't match
+//   $processor = $e->object;
+//   $form = $e->arguments('form');
+//   if($processor->formName == 'vehicle_power_search') { //form name
+//     $f = $form->getChildByName('manufacturer'); //form field name
+//     echo "form is " . $f;
+//     $options = ['red', 'blue', 'green']; // replace with your dynamic options -->
+//     foreach($options as $option) {
+//       $f->addOption($option);
+//     }
+//   }
+// });
+
+$forms->addHookBefore('FormBuilderProcessor::renderReady', function($e) {
   $processor = $e->object;
-  $form = $e->arguments('form');
-  if($processor->formName == 'vehicle_power_search') { //form name
-    $f = $form->getChildByName('manufacturer'); //form field name
-    echo "form is " . $f;
-    $options = ['red', 'blue', 'green']; // replace with your dynamic options -->
-    foreach($options as $option) {
-      $f->addOption($option);
-    }
-  }
-});
+  $form = $e->arguments(0);
+  if($processor->formName != 'vehicle_power_search') return;
+  $f = $form->getChildByName('manufacturer');
+  $f->options = [
+    'manufacturer1' => 'Manufacturer 1',
+    'manufacturer2' => 'Manufacturer 2', 
+  ];
+}); 
 
 // $forms->addHookBefore('FormBuilderProcessor::render', function($e) {
 //   if ($e->object->name != 'manufacturer') return; // quick exit if fieldname doesn't match
