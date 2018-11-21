8958c2670a63572ac9100a61578596d470ec427e
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-21 12:20:34 +0000

    working on dropdown

--- site/templates/manufacturer.php
+++ site/templates/manufacturer.php
@@ -1,8 +1,8 @@
 <?php
 // manufacturer.php
 date_default_timezone_set('CET');
-require('/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php');
-require(MR_PATH . '/inc/conn.php');
+require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
+require_once MR_PATH . '/inc/conn.php';
 
 $query = "SELECT DISTINCT(`manufacturer`) FROM `team`.`vehicles` ORDER BY `manufacturer` ASC";
 $result = mysqli_query($conn, $query);
