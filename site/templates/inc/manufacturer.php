97d68e6f0e4fab2a7783d85182bafff33c104afe
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-21 14:29:11 +0000

    working on dropdown

--- site/templates/inc/manufacturer.php
+++ site/templates/inc/manufacturer.php
@@ -13,7 +13,7 @@ $man = array();
   <?php
   while ($row = mysqli_fetch_assoc($result)) {
     echo "<li>" . $row['manufacturer'] . "</li>";
-    $man = $row['manufacturer'];
+    array_push($man, $row['manufacturer']);
   }
   echo '<pre>'; print_r($man); echo '</pre>';
   ?>
