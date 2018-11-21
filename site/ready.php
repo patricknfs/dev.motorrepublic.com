104c6f2901efc69fcdc3af002d3f81e954e10cd6
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-21 16:05:06 +0000

    working on dropdown

--- site/ready.php
+++ site/ready.php
@@ -1,41 +1,8 @@
 <?php
-
-// $forms->addHookBefore('InputfieldForm::render', function($e) {
-//   print_r($e);
-//   print_r($e->object->name);
-//   // if ($e->object->name != 'vehicle_power_search') return; // quick exit if form name doesn't match
-//   $processor = $e->object;
-//   $form = $e->arguments('form');
-//   if($processor->formName == 'vehicle_power_search') { //form name
-//     $f = $form->getChildByName('manufacturer'); //form field name
-//     echo "form is " . $f;
-//     $options = ['red', 'blue', 'green']; // replace with your dynamic options -->
-//     foreach($options as $option) {
-//       $f->addOption($option);
-//     }
-//   }
-// });
-// require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
-include '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/manufacturer.php';
 $forms->addHookBefore('FormBuilderProcessor::renderReady', function($e) {
   $processor = $e->object;
   $form = $e->arguments(0);
   if($processor->formName != 'vehicle_power_search') return;
   $f = $form->getChildByName('manufacturer');
-  $templates = ['vehicle', 'vehicles']; //array of templates where this hook should run
   $f->options = (object) $man;
 });
-
-// $forms->addHookBefore('FormBuilderProcessor::render', function($e) {
-//   if ($e->object->name != 'manufacturer') return; // quick exit if fieldname doesn't match
-//   $processor = $e->object;
-//   $form = $processor->getInputfieldsForm();
-//   if($form->name == 'vehicle_power_search') {
-//     $my_select = $form->getChildByName('manufacturer');
-//     print_r($my_select);
-//     $options = ['red', 'blue', 'green']; // replace with your dynamic options
-//     foreach($options as $option) {
-//       $my_select->addOption($option);
-//     }
-//   }
-// });
