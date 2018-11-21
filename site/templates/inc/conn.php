62f25b74abb546bd7e1718f6b9a0e5c5857df7b6
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-21 11:13:53 +0000

    working on dropdown

--- site/templates/inc/conn.php
+++ site/templates/inc/conn.php
@@ -5,8 +5,6 @@ $conn = new mysqli("localhost", MR_DB_USERNAME, MR_DB_PASSWORD);
 
 $conn->set_charset("utf8");
 
-// var_dump($conn2);
-
 if ($conn->connect_errno) {
     printf("Connect failed: %s\n", $conn->connect_error);
     exit();
