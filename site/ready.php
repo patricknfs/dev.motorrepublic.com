430204dfce26658b6d8d8b5445613fa9ecd85710
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-22 15:05:58 +0000

    working on dropdown

--- site/ready.php
+++ site/ready.php
@@ -1,13 +1,14 @@
 <?php
+$mans = $this->wire('manufs');
+var_dump($mans);
+foreach($mans AS $man){
+  echo $man;
+}
 $forms->addHookBefore('FormBuilderProcessor::renderReady', function($e) {
   $processor = $e->object;
   $form = $e->arguments(0);
   if($processor->formName != 'vehicle_power_search') return;
   $f = $form->getChildByName('manufacturer');
-  $mans = $this->wire('manufs');
-  var_dump($mans);
-  foreach($mans AS $man){
-    echo $man;
-  }
+
   $f->options = $mans;
 });
