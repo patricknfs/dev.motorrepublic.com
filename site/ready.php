3dce4ffb222ddf4926de94516da78ed88e2d9c84
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-22 14:34:27 +0000

    working on dropdown

--- site/ready.php
+++ site/ready.php
@@ -4,6 +4,6 @@ $forms->addHookBefore('FormBuilderProcessor::renderReady', function($e) {
   $form = $e->arguments(0);
   if($processor->formName != 'vehicle_power_search') return;
   $f = $form->getChildByName('manufacturer');
-  $man = $this->wire($manufs);
-  $f->options = (object) $man;
+  $mans = $this->wire($man);
+  $f->options = (object) $mans;
 });
