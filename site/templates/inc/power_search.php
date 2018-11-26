31b1441ff94b1b74f458ffd844db35e0bb2d2bfd
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-26 15:19:59 +0000

    Temporary form

--- site/templates/inc/power_search.php
+++ site/templates/inc/power_search.php
@@ -40,11 +40,10 @@ ob_start();
     <div class="cell small-12 medium-4">
       <select placeholder="model" id="second-choice">
         <option>Model (choose manufacturer first)</option>
-        <script type="text/javascript">
+      
           $("#first-choice").change(function() {
             $("#second-choice").load("getter.php?choice=" + $("#first-choice").val());
           });
-        </script>
       </select>
     </div>
     <div class="cell small-12 medium-4">
