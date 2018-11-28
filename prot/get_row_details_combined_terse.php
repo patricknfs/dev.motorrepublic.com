4877d0fe9259e24afc7826c092e4fc85c0ae46bb
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-28 13:37:49 +0000

    search problem

--- prot/get_row_details_combined_terse.php
+++ prot/get_row_details_combined_terse.php
@@ -7,7 +7,7 @@ date_default_timezone_set('CET');
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/conn.php';
 
-$truncate = "TRUNCATE TABLE `team`.`rates_combined`";
+$truncate = "TRUNCATE TABLE `team`.`rates_combined_terse`";
 $result2 = $conn->query($truncate) or die(mysqli_error());
 
 $query = "SELECT t1.*
