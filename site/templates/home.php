6193d868af4f968755a031885c6823d9770c97df
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-30 19:13:21 +0000

    dropdown issue

--- site/templates/home.php
+++ site/templates/home.php
@@ -3,7 +3,7 @@
 date_default_timezone_set('CET');
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
 require_once(MR_PATH . "/inc/conn.php");
-
+include "power_search.php";
 $manuf = $page->top_deal_manuf;
 $query = "SELECT `id`,`cap_id`,`cap_code`,`src`,`manufacturer`,`model`,`descr`,`term`,`mileage`,min(`rental`) AS `rental`,`vehicle_list_price`,`vehicle_otr_price`,`p11d_price`,`CO2` FROM `team`.`rates_combined` WHERE `manufacturer` = '" . strtoupper($manuf) . "' GROUP BY `model` ORDER BY `rental` ASC LIMIT 6";
 $result = $conn->query($query) or die(mysqli_error($conn));
