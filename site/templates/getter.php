5e1c15dc46927f073bf68cb6743778b8563a4d99
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-30 15:05:21 +0000

    dropdown issue

--- site/templates/getter.php
+++ site/templates/getter.php
@@ -1,19 +1,15 @@
 <?php
-date_default_timezone_set('CET');
-require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
-require_once(MR_PATH . "/inc/conn.php");
 $query = "SELECT DISTINCT(`manufacturer`) FROM `team`.`vehicles` ORDER BY `manufacturer` ASC";
 $result = mysqli_query($conn, $query);
-
+echo $_GET['choice'];
 // second query
-// $choice = "MERCEDES";
-$choice = $_GET['choice'] ?? '';
-$choice = $conn->real_escape_string($choice);
-  
-$query2 = "SELECT DISTINCT(`model`) FROM `team`.`vehicles` WHERE `manufacturer` = '" . strtoupper($choice) . "' ORDER BY `model` ASC";
-// echo $query2;
-$result2 = mysqli_query($conn, $query2);
-  
-while ($row = mysqli_fetch_array($result2)) {
-  echo "<option>" . $row{'model'} . "</option>";
+if ($_GET['choice']) {
+  $choice = $conn->real_escape_string($_GET['choice']);
+    
+  $query2 = "SELECT DISTINCT(`model`) FROM `team`.`vehicles` WHERE `manufacturer` = " . $choice . " ORDER BY `model` ASC";
+  $result2 = mysqli_query($conn, $query2);
+    
+  while ($row = mysqli_fetch_array($result2)) {
+      echo "<option>" . $row{'model'} . "</option>";
+  }
 }
