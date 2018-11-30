1e7fb6d29253b245cb5e33ebdfb07762f556b0eb
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-11-30 19:29:59 +0000

    dropdown issue

--- site/templates/views/vehicles_main.php
+++ site/templates/views/vehicles_main.php
@@ -23,36 +23,41 @@
 		</div>
 		<div class="grid-x grid-padding-x small-up-2 medium-up-4">
 			<?php
-			foreach($result AS $vehicle) {
-				$options = array(
-					'quality' => 80,
-					'upscaling' => false       
-				);
+			if($total_rows > 0){
+				foreach($result AS $vehicle) {
+					$options = array(
+						'quality' => 80,
+						'upscaling' => false       
+					);
 
-				$bch_rental = number_format(((($vehicle['rental'] * $vehicle['term']) + 300) / ($vehicle['term']+8)), 2, '.', ',');
-				$pch_rental = number_format(((($vehicle['rental'] * $vehicle['term']) + 300) / ($vehicle['term']+8)*1.2), 2, '.', ',');
+					$bch_rental = number_format(((($vehicle['rental'] * $vehicle['term']) + 300) / ($vehicle['term']+8)), 2, '.', ',');
+					$pch_rental = number_format(((($vehicle['rental'] * $vehicle['term']) + 300) / ($vehicle['term']+8)*1.2), 2, '.', ',');
 
-				$hashcode = strtoupper(md5("173210NfS4JeCAR" . $vehicle['cap_id']));
-				?>
-				<div class="cell">
-					<a href="/vehicle/<?=$vehicle['cap_id']?>">
-						<div class="card">
-							<div class="card-section">
-								<h6>
-									<?=$vehicle['manufacturer']?> <?=$vehicle['model']?>
-								</h6>
-								<p><?=$vehicle['descr']?></p>
+					$hashcode = strtoupper(md5("173210NfS4JeCAR" . $vehicle['cap_id']));
+					?>
+					<div class="cell">
+						<a href="/vehicle/<?=$vehicle['cap_id']?>">
+							<div class="card">
+								<div class="card-section">
+									<h6>
+										<?=$vehicle['manufacturer']?> <?=$vehicle['model']?>
+									</h6>
+									<p><?=$vehicle['descr']?></p>
+								</div>
+								<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$vehicle['cap_id']?>&DATE=2018/09/11&WIDTH=300&HEIGHT=225&IMAGETEXT=&VIEWPOINT=">
+								<div class="card-section">
+									<h6>Business Clients<br /><span class="price">£<?=$bch_rental?></span>/mth excl. VAT</h6>
+									<h6>Personal Clients<br /><span class="price">£<?=$pch_rental?></span>/mth inc. VAT</h6>
+									<small>click for more details...</small>
+								</div>
 							</div>
-							<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$vehicle['cap_id']?>&DATE=2018/09/11&WIDTH=300&HEIGHT=225&IMAGETEXT=&VIEWPOINT=">
-							<div class="card-section">
-								<h6>Business Clients<br /><span class="price">£<?=$bch_rental?></span>/mth excl. VAT</h6>
-								<h6>Personal Clients<br /><span class="price">£<?=$pch_rental?></span>/mth inc. VAT</h6>
-								<small>click for more details...</small>
-							</div>
-						</div>
-					</a>
-				</div>
-			<?php
+						</a>
+					</div>
+				<?php
+				}
+			}
+			else {
+				echo "Woah!!! No Vehicles! PLease try another search above.";
 			}
 			?>
 		</div>
