ee8b88417e3f76fa7f53abb9294799b4bb14adbb
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-12-01 08:59:04 +0000

    social icons

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -1,7 +1,7 @@
 <?php
 // home_main.php
 ?>
-<section id="search_block">
+<!-- <section id="search_block">
 	<div class="grid-container">
 		<div class="grid-x grid-margin-x">
 			<div class="cell small-12">
@@ -11,7 +11,7 @@
 			</div>
 		</div>
 	</div>
-</section>
+</section> -->
 <section id="promo" style="background: url(site/assets/files/1173/adobestock_80868279_preview.jpg) no-repeat center center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;">
 	<!-- <div class="overlay"></div> -->
 	<div class="grid-container">
