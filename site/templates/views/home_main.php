0b8796fb4d8366fcaedb1c6c5a466f31699af828
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-12-02 17:05:42 +0000

    fixing searchbar

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -16,7 +16,7 @@
 	<div class="grid-container">
 		<div class="grid-x grid-margin-x">
 			<div class="cell small-12 medium-8">
-				<h3>Top Mercedes Deals</h3>
+				<h3>Check OUt Our Specials</h3>
 				<div class="grid-x grid-margin-x">
 					<?php
 					foreach($result AS $vehicle) {
