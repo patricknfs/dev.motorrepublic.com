1335ffc4032096c5d4aebb3cde099bd33b4479d7
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-12-02 21:09:54 +0000

    fixing searchbar

--- site/templates/views/vehicle_main.php
+++ site/templates/views/vehicle_main.php
@@ -7,47 +7,47 @@
 			<div class="cell small-12 medium-8">
 				<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$input->urlSegment1;?>&DATE=2018/09/11&WIDTH=800&HEIGHT=600&IMAGETEXT=&VIEWPOINT=">
 				<section id="vehicle_details">
-		<div class="grid-container">
-			<div class="grid-x">
-				<div class="cell small-12">
-					<ul class="tabs" data-tabs id="example-tabs">
-						<li class="tabs-title is-active"><a href="#panel1" aria-selected="true">Standard Equipment</a></li>
-						<li class="tabs-title"><a data-tabs-target="panel2" href="#panel2">Technical Data</a></li>
-					</ul>
-					<div class="tabs-content" data-tabs-content="example-tabs">
-						<div class="tabs-panel is-active" id="panel1">
-							<?php
-							foreach($groups AS $group){
-								echo "<ul><strong>" . $group . "</strong>";
-								foreach($equipment as $item){
-									// echo $item->Dc_Description . " = " . $group . "<br />";
-									if(trim($item->Dc_Description) === trim($group)){
-										echo "<li>" . $item->Do_Description . "</li>";
-									}
-								}
-								echo "</ul>";
-							}
-							?>
+		<!-- `			<div class="grid-container"> -->
+						<div class="grid-x">
+							<div class="cell small-12">
+								<ul class="tabs" data-tabs id="example-tabs">
+									<li class="tabs-title is-active"><a href="#panel1" aria-selected="true">Standard Equipment</a></li>
+									<li class="tabs-title"><a data-tabs-target="panel2" href="#panel2">Technical Data</a></li>
+								</ul>
+								<div class="tabs-content" data-tabs-content="example-tabs">
+									<div class="tabs-panel is-active" id="panel1">
+										<?php
+										foreach($groups AS $group){
+											echo "<ul><strong>" . $group . "</strong>";
+											foreach($equipment as $item){
+												// echo $item->Dc_Description . " = " . $group . "<br />";
+												if(trim($item->Dc_Description) === trim($group)){
+													echo "<li>" . $item->Do_Description . "</li>";
+												}
+											}
+											echo "</ul>";
+										}
+										?>
+									</div>
+									<div class="tabs-panel" id="panel2">
+									<?php
+										foreach($groups2 AS $group2){
+											echo "<ul><strong>" . $group2 . "</strong>";
+											foreach($tech_data as $item2){
+												// echo $item->Dc_Description . " = " . $group . "<br />";
+												if(trim($item2->Dc_Description) === trim($group2)){
+													echo "<li class='grid-x'><div class='cell small-3'>" . $item2->DT_LongDescription . "</div><div class='cell small-3'>".  $item2->tech_value_string . "</div></li>";
+												}
+											}
+											echo "</ul>";
+										}
+										?>
+									</div>
+								</div>
+							</div>
 						</div>
-						<div class="tabs-panel" id="panel2">
-						<?php
-							foreach($groups2 AS $group2){
-								echo "<ul><strong>" . $group2 . "</strong>";
-								foreach($tech_data as $item2){
-									// echo $item->Dc_Description . " = " . $group . "<br />";
-									if(trim($item2->Dc_Description) === trim($group2)){
-										echo "<li class='grid-x'><div class='cell small-3'>" . $item2->DT_LongDescription . "</div><div class='cell small-3'>".  $item2->tech_value_string . "</div></li>";
-									}
-								}
-								echo "</ul>";
-							}
-							?>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+					<!-- </div> -->
+				</section>`
 			</div>
 			<div class="cell small-12 medium-4">
 				<div class="card card-2">
