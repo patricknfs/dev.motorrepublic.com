d3c6526795813d50b6f361bf9b055e14a9ba0e3d
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-12-02 13:51:55 +0000

    fixing functions

--- site/templates/views/vehicle_main.php
+++ site/templates/views/vehicle_main.php
@@ -1,11 +1,4 @@
 <div class="grid-container">
-	<div class="grid-x">
-		<div class="cell small-12">
-			<div class="text-center">
-				<?=$form_out?>
-			</div>
-		</div>
-	</div>
 	<div class="grid-x grid-padding-x">
 		<?php
 		// var_dump($data);
