24b03d8fdaa6e6c7beec8d7ed0e0cb76500609fd
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-12-02 13:50:58 +0000

    fixing functions

--- site/templates/main.php
+++ site/templates/main.php
@@ -4,7 +4,6 @@ ERROR_REPORTING(E_ALL);
 date_default_timezone_set('CET');
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
 require_once(MR_PATH . "/inc/conn.php");
-include "inc/functions.php";
 include "power_search.php";
 ?>
 <!DOCTYPE html>
