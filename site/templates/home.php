61776049e602bec36ba68d2c9eae76e5a8c5485e
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-12-03 11:47:03 +0000

    specials home page and db

--- site/templates/home.php
+++ site/templates/home.php
@@ -9,7 +9,7 @@ $manuf = $page->top_deal_manuf;
 $query = "SELECT `id`,`cap_id`,`cap_code`,`src`,`manufacturer`,`model`,`descr`,`term`,`mileage`,`rental`, `vehicle_list_price`, `vehicle_otr_price`,`p11d_price`,`CO2` FROM `team`.`rates_combined_terse` WHERE `special` = true ORDER BY `rental` ASC LIMIT 6";
 $result = $conn->query($query) or die(mysqli_error($conn));
 $data = $result->fetch_assoc();
-var($data);
+var_dump($data);
 // echo $query;
 ob_start();
 include('views/home_main.php');_dump
