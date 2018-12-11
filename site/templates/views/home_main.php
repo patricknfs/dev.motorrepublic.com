15d0b034e11c49b348233318346690aae07ed767
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-12-11 08:54:02 +0000

    updating home page

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -25,52 +25,13 @@
 						);
 					?>
 					<div class="cell small-12 medium-4">
-						<a href="/vehicle/<?=$vehicle['cap_id']?>">
-							<div class="card">
-								<div class="card-section">
-									<h6>Cars</h6>
-								</div>
-								<div class="home_deals card-image">
-									<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$vehicle['cap_id']?>&DATE=2018/09/11&WIDTH=300&HEIGHT=225&IMAGETEXT=&VIEWPOINT=">
-								</div>
-								<div class="card-section">
-									<h6>Business Clients <span class="price">£<?=$bch_rental?></span> excl. VAT</h6>
-									<h6>Personal Clients <span class="price">£<?=$pch_rental?></span> inc. VAT</h6>
-								</div>
-							</div>
-						</a>
+
 					</div>
 					<div class="cell small-12 medium-4">
-						<a href="/vehicle/<?=$vehicle['cap_id']?>">
-							<div class="card">
-								<div class="card-section">
-									<h6>Vans</h6>
-								</div>
-								<div class="home_deals card-image">
-									<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$vehicle['cap_id']?>&DATE=2018/09/11&WIDTH=300&HEIGHT=225&IMAGETEXT=&VIEWPOINT=">
-								</div>
-								<div class="card-section">
-									<h6>Business Clients <span class="price">£<?=$bch_rental?></span> excl. VAT</h6>
-									<h6>Personal Clients <span class="price">£<?=$pch_rental?></span> inc. VAT</h6>
-								</div>
-							</div>
-						</a>
+
 					</div>
 					<div class="cell small-12 medium-4">
-						<a href="/vehicle/<?=$vehicle['cap_id']?>">
-							<div class="card">
-								<div class="card-section">
-									<h6>Pickups</h6>
-								</div>
-								<div class="home_deals card-image">
-									<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$vehicle['cap_id']?>&DATE=2018/09/11&WIDTH=300&HEIGHT=225&IMAGETEXT=&VIEWPOINT=">
-								</div>
-								<div class="card-section">
-									<h6>Business Clients <span class="price">£<?=$bch_rental?></span> excl. VAT</h6>
-									<h6>Personal Clients <span class="price">£<?=$pch_rental?></span> inc. VAT</h6>
-								</div>
-							</div>
-						</a>
+
 					</div>
 				</div>
 			</div>
