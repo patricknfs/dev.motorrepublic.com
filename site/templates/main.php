466df9c53eef13a060802be27342ed9ca98ea621
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-12-18 12:11:21 +0000

    removing power search from best rates

--- site/templates/main.php
+++ site/templates/main.php
@@ -4,7 +4,7 @@ ERROR_REPORTING(E_ALL);
 date_default_timezone_set('CET');
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
 require_once(MR_PATH . "/inc/conn.php");
-if(!$pages->get(1043)){
+if($page->id !== 1043){
   include "power_search.php";
 }
 
@@ -108,7 +108,7 @@ if(!$pages->get(1043)){
       </div>
       <div class="searchbar">
         <?php
-        if(!$pages->get(1043)){
+        if($page->id !== 1043){
           ?>
           <div class="grid-container"><?=$form_out?></div>
           <?php
