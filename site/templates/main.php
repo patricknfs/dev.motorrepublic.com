1679f918bd3086ab478104397cf9b453cbaf62cd
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-12-19 10:01:47 +0000

    excluding live chat

--- site/templates/main.php
+++ site/templates/main.php
@@ -173,34 +173,40 @@ if($page->id !== 1043){
   </body>
   <script type="text/javascript" src="<?=$config->urls->templates?>styles/js/foundation.min.js" ></script>
   <script type="text/javascript" src="<?=$config->urls->templates?>styles/js/app.js" ></script>
-  <!-- Start of Async Drift Code -->
-  <script>
-    "use strict";
+  <?php
+  if($page->id !== 1043){
+    ?>
+    <!-- Start of Async Drift Code -->
+    <script>
+      "use strict";
 
-    !function() {
-      var t = window.driftt = window.drift = window.driftt || [];
-      if (!t.init) {
-        if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
-        t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
-        t.factory = function(e) {
-          return function() {
-            var n = Array.prototype.slice.call(arguments);
-            return n.unshift(e), t.push(n), t;
+      !function() {
+        var t = window.driftt = window.drift = window.driftt || [];
+        if (!t.init) {
+          if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+          t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
+          t.factory = function(e) {
+            return function() {
+              var n = Array.prototype.slice.call(arguments);
+              return n.unshift(e), t.push(n), t;
+            };
+          }, t.methods.forEach(function(e) {
+            t[e] = t.factory(e);
+          }), t.load = function(t) {
+            var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+            o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+            var i = document.getElementsByTagName("script")[0];
+            i.parentNode.insertBefore(o, i);
           };
-        }, t.methods.forEach(function(e) {
-          t[e] = t.factory(e);
-        }), t.load = function(t) {
-          var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
-          o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
-          var i = document.getElementsByTagName("script")[0];
-          i.parentNode.insertBefore(o, i);
-        };
-      }
-    }();
-    drift.SNIPPET_VERSION = '0.3.1';
-    drift.load('zr8i59c3d6dv');
-  </script>
-  <!-- End of Async Drift Code -->
+        }
+      }();
+      drift.SNIPPET_VERSION = '0.3.1';
+      drift.load('zr8i59c3d6dv');
+    </script>
+    <!-- End of Async Drift Code -->
+    <?php
+    }
+    ?>
   <script>
       function populate(s1,s2){
         var s1 = document.getElementById(s1);
