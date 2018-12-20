935aeb8275244ed93a3f18cb6d0205ff7642406f
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-12-20 14:30:29 +0000

    display specials

--- site/templates/get_special_entries.php
+++ site/templates/get_special_entries.php
@@ -13,7 +13,6 @@ foreach($forms->get("specials_upload")->entries->find() as $e){
   $result3 = $conn->query($insert) or die(mysqli_error($conn));
 }
 
-
 // $query = "SELECT 
 //   mr1.code AS `cap_id`, mr1.capcode AS `cap_code`, mr2.src AS source , mr1.man AS `manufacturer`, mr1.mod AS `model`, mr1.cap_desc AS `descr`, mr2.vlp AS `vehicle_list`, mr2.votrp AS `vehicle_otr`, mr2.p11p AS `p11d`, mr2.co2 AS `CO2_no`, mr2.term AS `term`, mr2.mileage AS `mileage`, mr2.rent AS `rental`
 //   FROM
