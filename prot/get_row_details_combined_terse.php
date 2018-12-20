3021dc852b2fba3a78b40981ef3e61923c6f4b30
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-12-20 11:43:27 +0000

    add special column

--- prot/get_row_details_combined_terse.php
+++ prot/get_row_details_combined_terse.php
@@ -16,7 +16,7 @@ $query = "SELECT t1.*
   ) as t2 INNER JOIN team.rates_combined AS t1 ON t1.cap_id = t2.cap_id AND t1.rental = t2.rent
   ORDER BY t1.rental ASC
 ";
-// echo $query; 
+
 $result = $conn->query($query) or die(mysqli_error($conn));
 
 // iterate over every row
@@ -24,7 +24,7 @@ $row = 1;
 
 while ($row = mysqli_fetch_assoc($result)) {
   // for every field in the result..
-  $insert = "INSERT INTO `team`.`rates_combined_terse` VALUES ('','" . $row['cap_id'] . "', '" . $row['cap_code'] . "', '" . $row['src'] . "', '" . $row['manufacturer'] . "', '" . $row['model'] . "', '" . $row['descr'] . "', '" . $row['term'] . "', '" . $row['mileage'] . "', '" . $row['rental'] . "', '" . $row['vehicle_list'] . "', '" . $row['vehicle_otr'] . "', '" . $row['p11d'] . "', '" . $row['CO2_no'] . "')";
+  $insert = "INSERT INTO `team`.`rates_combined_terse` VALUES ('','" . $row['cap_id'] . "', '" . $row['cap_code'] . "', '" . $row['src'] . "', '" . $row['updated'] . "', '" . $row['manufacturer'] . "', '" . $row['model'] . "', '" . $row['descr'] . "', '" . $row['term'] . "', '" . $row['mileage'] . "', '" . $row['rental'] . "', '" . $row['vehicle_list'] . "', '" . $row['vehicle_otr'] . "', '" . $row['p11d'] . "', '" . $row['CO2_no'] . "', '" . $row['lcv'] . "', '" . $row['special'] . "', '" . $row['deal_notes'] . "')";
   echo $insert;
   $result3 = $conn->query($insert) or die(mysqli_error($conn));
   $row++;
