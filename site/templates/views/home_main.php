80ade85a47747d2ba7aaf3fcd8f82e1960331f04
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-12-21 09:56:10 +0000

    add category links to home page

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -43,15 +43,16 @@
 						</a>
 					</div>
 					<div class="cell small-12 medium-6">
-						<h3>Magazine Article</h3>
-						<p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
+					<h3>Magazine Article</h3>
+					<p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
 					</div>
 					<div class="cell small-12 medium-6">
+						<h3>Regional Strength?</h3>
 						<p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
 					</div>
 				</div>
 			</div>
-
+			
 			<div class="cell small-12 medium-4">
 				<div id="message">
 					<h2>Deal of the Day</h2>
