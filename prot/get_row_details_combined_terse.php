05004f3ca8a2b98845678dad328b2f2e114a76b4
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-12-21 11:11:46 +0000

    altering rates tables

--- prot/get_row_details_combined_terse.php
+++ prot/get_row_details_combined_terse.php
@@ -7,8 +7,8 @@ date_default_timezone_set('CET');
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/conn.php';
 
-$truncate = "TRUNCATE TABLE `team`.`rates_combined_terse`";
-$result2 = $conn->query($truncate) or die(mysqli_error());
+// $truncate = "TRUNCATE TABLE `team`.`rates_combined_terse`";
+// $result2 = $conn->query($truncate) or die(mysqli_error());
 
 $query = "SELECT t1.*
   FROM (
@@ -24,7 +24,7 @@ $row = 1;
 
 while ($row = mysqli_fetch_assoc($result)) {
   // for every field in the result..
-  $insert = "INSERT INTO `team`.`rates_combined_terse` VALUES ('','" . $row['cap_id'] . "', '" . $row['cap_code'] . "', '" . $row['src'] . "', '" . $row['updated'] . "', '" . $row['manufacturer'] . "', '" . $row['model'] . "', '" . $row['descr'] . "', '" . $row['term'] . "', '" . $row['mileage'] . "', '" . $row['rental'] . "', '" . $row['vehicle_list'] . "', '" . $row['vehicle_otr'] . "', '" . $row['p11d'] . "', '" . $row['CO2_no'] . "', '" . $row['lcv'] . "', '" . $row['special'] . "', '" . $row['deal_notes'] . "')";
+  $insert = "REPLACE INTO `team`.`rates_combined_terse` VALUES ('','" . $row['cap_id'] . "', '" . $row['cap_code'] . "', '" . $row['src'] . "', '" . $row['updated'] . "', '" . $row['manufacturer'] . "', '" . $row['model'] . "', '" . $row['descr'] . "', '" . $row['term'] . "', '" . $row['mileage'] . "', '" . $row['rental'] . "', '" . $row['vehicle_list'] . "', '" . $row['vehicle_otr'] . "', '" . $row['p11d'] . "', '" . $row['CO2_no'] . "', '" . $row['lcv'] . "', '" . $row['special'] . "', '" . $row['deal_notes'] . "')";
   echo $insert;
   $result3 = $conn->query($insert) or die(mysqli_error($conn));
   $row++;
