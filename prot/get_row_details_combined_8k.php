a1c04cdb23a2b98cb57e79913b1342bbd445a6f8
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-12-28 12:05:24 +0000

    updating special upload

--- prot/get_row_details_combined_8k.php
+++ prot/get_row_details_combined_8k.php
@@ -19,7 +19,8 @@ require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templ
 
 // $manufacturer = "ford";
 
-$truncate = "DELETE FROM `team`.`rates_combined` WHERE `special` != TRUE";
+$truncate = "TRUNCATE `team`.`rates_combined`";
+// $truncate = "DELETE FROM `team`.`rates_combined` WHERE `special` != TRUE";
 $result2 = $conn->query($truncate) or die(mysqli_error());
 
 $query = "SELECT 
