976826c6e198af1f76680f754d2fe927b27d27ba
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-12-30 18:31:37 +0000

    formatting regional page

--- site/templates/views/franchisee_region_main.php
+++ site/templates/views/franchisee_region_main.php
@@ -22,58 +22,58 @@
   <div class="grid-container">
 		<div class="grid-x grid-margin-x">
 			<div class="cell small-12 medium-8">
-      <h2>My Favourite Deals</h2>
-      <?php
-      foreach($result AS $vehicle) {
-        $options = array(
-          'quality' => 80,
-          'upscaling' => false       
-        );
-
-        $bch_rental = number_format(((($vehicle['rental'] * $vehicle['term']) + 300) / ($vehicle['term']+8)), 2, '.', ',');
-        $pch_rental = number_format(((($vehicle['rental'] * $vehicle['term']) + 300) / ($vehicle['term']+8)*1.2), 2, '.', ',');
-
-        $hashcode = strtoupper(md5("173210NfS4JeCAR" . $vehicle['cap_id']));
-        ?>
-        <div class="cell">
-          <a href="/vehicle/<?=$vehicle['cap_id']?>">
-            <div class="card">
-              <div class="card-section">
-                <h6>
-                  <?=$vehicle['manufacturer']?> <?=$vehicle['model']?>
-                </h6>
-                <p><?=$vehicle['descr']?></p>
-              </div>
-              <img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$vehicle['cap_id']?>&DATE=2018/09/11&WIDTH=300&HEIGHT=225&IMAGETEXT=&VIEWPOINT=">
-              <div class="card-section">
-                <h6>Business Clients<br /><span class="price">£<?=$bch_rental?></span> excl. VAT</h6>
-                <h6>Personal Clients<br /><span class="price">£<?=$pch_rental?></span> inc. VAT</h6>
-                <small>click for more details...</small>
-              </div>
-            </div>
-          </a>
-        </div>
+        <!-- <h2>My Favourite Deals</h2>
         <?php
-        }
-        ?>
-        <!-- In this container, add blockquote-left or blockquote-right to make the blockquote left / -->
-        <div class="blockquote-container">
-          <div class="callout">
-            <h4 class="blockquote-title"><?=$testimonial->title?></h4>
-            <blockquote>
-              <span class="blockquote-content"><?=$testimonial_blurb?>... <a href="<?=$testimonial->url?>">more details</a></i></span>
-            </blockquote>
-            <div class="sig">
-              <cite><?=$testimonial->sig?></cite>
+        foreach($result AS $vehicle) {
+          $options = array(
+            'quality' => 80,
+            'upscaling' => false       
+          );
+
+          $bch_rental = number_format(((($vehicle['rental'] * $vehicle['term']) + 300) / ($vehicle['term']+8)), 2, '.', ',');
+          $pch_rental = number_format(((($vehicle['rental'] * $vehicle['term']) + 300) / ($vehicle['term']+8)*1.2), 2, '.', ',');
+
+          $hashcode = strtoupper(md5("173210NfS4JeCAR" . $vehicle['cap_id']));
+          ?>
+          <div class="cell">
+            <a href="/vehicle/<?=$vehicle['cap_id']?>">
+              <div class="card">
+                <div class="card-section">
+                  <h6>
+                    <?=$vehicle['manufacturer']?> <?=$vehicle['model']?>
+                  </h6>
+                  <p><?=$vehicle['descr']?></p>
+                </div>
+                <img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=CAR&CAPID=<?=$vehicle['cap_id']?>&DATE=2018/09/11&WIDTH=300&HEIGHT=225&IMAGETEXT=&VIEWPOINT=">
+                <div class="card-section">
+                  <h6>Business Clients<br /><span class="price">£<?=$bch_rental?></span> excl. VAT</h6>
+                  <h6>Personal Clients<br /><span class="price">£<?=$pch_rental?></span> inc. VAT</h6>
+                  <small>click for more details...</small>
+                </div>
+              </div>
+            </a>
+          </div>
+          <?php
+          }
+          ?> -->
+          <!-- In this container, add blockquote-left or blockquote-right to make the blockquote left / -->
+          <div class="blockquote-container">
+            <div class="callout">
+              <h4 class="blockquote-title"><?=$testimonial->title?></h4>
+              <blockquote>
+                <span class="blockquote-content"><?=$testimonial_blurb?>... <a href="<?=$testimonial->url?>">more details</a></i></span>
+              </blockquote>
+              <div class="sig">
+                <cite><?=$testimonial->sig?></cite>
+              </div>
             </div>
           </div>
-        </div>
 
+        </div>
+        <div class="cell small-12 medium-4">
+          <?=$page->body?>
+        </div>
       </div>
-      <div class="cell small-12 medium-4">
-      <?=$page->body?>
-      </div>
-    </div>
 
     </div>
 </section>
