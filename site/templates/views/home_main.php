024ed3d67e5750a79e99d3a86fea1887cde0eeac
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2018-12-31 15:18:39 +0000

    updating home page deal

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -61,8 +61,8 @@
 			<div class="cell small-12 medium-4">
 				<a href="/vehicle/<?=$data['cap_id']?>">
 					<div class="card">
-						<h3>Hot Deals</h3>
 						<div class="card-section">
+							<h3>Hot Deals</h3>
 							<h6>
 								<?=$data['manufacturer']?> <?=$data['model']?>
 							</h6>
