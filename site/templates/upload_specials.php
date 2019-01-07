7fb113f9dd64e13d8ca2137e7513555e9b177ffb
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-01-07 11:00:02 +0000

    bulk upload specials form

--- site/templates/upload_specials.php
+++ site/templates/upload_specials.php
@@ -11,18 +11,18 @@
 // var_dump($lines);
 
 
-// $row = 1;
-// if (($handle = fopen("test.csv", "r")) !== FALSE) {
-//   while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-//     $num = count($data);
-//     echo "<p> $num fields in line $row: <br /></p>\n";
-//     $row++;
-//     for ($c=0; $c < $num; $c++) {
-//       echo $data[$c] . "<br />\n";
-//     }
-//   }
-//   fclose($handle);
-// }
+$row = 1;
+if (($handle = fopen($_FILES['file']['tmp_name'], "r")) !== FALSE) {
+  while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+    $num = count($data);
+    echo "<p> $num fields in line $row: <br /></p>\n";
+    $row++;
+    for ($c=0; $c < $num; $c++) {
+      echo $data[$c] . "<br />\n";
+    }
+  }
+  fclose($handle);
+}
 
 // $entry = array(
 //   'first_name' => 'John',
