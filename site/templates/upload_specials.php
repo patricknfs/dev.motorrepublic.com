646690677aa8aa97f33f7f52f5b85dc5891f26a4
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-01-07 12:23:11 +0000

    bulk upload specials form

--- site/templates/upload_specials.php
+++ site/templates/upload_specials.php
@@ -13,26 +13,25 @@
 
 $row = 1;
 if (($handle = fopen($_FILES['file']['tmp_name'], "r")) !== FALSE) {
+  fgetcsv($handle);
   while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-    if($row > 1) {
-      $num = count($data);
-      echo "<p> $num fields in line $row: <br /></p>\n";
-      $row++;
-      
-      for ($c=0; $c < $num; $c++) {
-        echo $data[$c] . "<br />\n";
-        // $entry = array(
-        //   'first_name' => 'John',
-        //   'last_name' => 'Smith',
-        //   'email' => 'john@smith.com',
-        //   'message' => 'Hello',
-        // );
-        // $form = $forms->load('contact');
-        // $form->entries()->save($entry);
-      }
-    fclose($handle);
+    $num = count($data);
+    echo "<p> $num fields in line $row: <br /></p>\n";
+    $row++;
+    
+    for ($c=0; $c < $num; $c++) {
+      echo $data[$c] . "<br />\n";
+      // $entry = array(
+      //   'first_name' => 'John',
+      //   'last_name' => 'Smith',
+      //   'email' => 'john@smith.com',
+      //   'message' => 'Hello',
+      // );
+      // $form = $forms->load('contact');
+      // $form->entries()->save($entry);
     }
   }
+  fclose($handle);
 }
 
 
