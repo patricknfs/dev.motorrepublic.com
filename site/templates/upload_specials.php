7ebd3553f6802f53c5710c9b36452db17a13a327
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-01-07 10:55:26 +0000

    bulk upload specials form

--- site/templates/upload_specials.php
+++ site/templates/upload_specials.php
@@ -11,26 +11,26 @@ while( ($row = fgetcsv($fh, 8192)) !== FALSE ) {
 var_dump($lines);
 
 
-$row = 1;
-if (($handle = fopen("test.csv", "r")) !== FALSE) {
-  while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-    $num = count($data);
-    echo "<p> $num fields in line $row: <br /></p>\n";
-    $row++;
-    for ($c=0; $c < $num; $c++) {
-      echo $data[$c] . "<br />\n";
-    }
-  }
-  fclose($handle);
-}
+// $row = 1;
+// if (($handle = fopen("test.csv", "r")) !== FALSE) {
+//   while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+//     $num = count($data);
+//     echo "<p> $num fields in line $row: <br /></p>\n";
+//     $row++;
+//     for ($c=0; $c < $num; $c++) {
+//       echo $data[$c] . "<br />\n";
+//     }
+//   }
+//   fclose($handle);
+// }
 
-$entry = array(
-  'first_name' => 'John',
-  'last_name' => 'Smith',
-  'email' => 'john@smith.com',
-  'message' => 'Hello',
-);
+// $entry = array(
+//   'first_name' => 'John',
+//   'last_name' => 'Smith',
+//   'email' => 'john@smith.com',
+//   'message' => 'Hello',
+// );
 
-$form = $forms->load('contact');
-$form->entries()->save($entry);
+// $form = $forms->load('contact');
+// $form->entries()->save($entry);
 ?>
