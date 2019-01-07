9bd3c6426a5d9570a2628a063936f55eddc323d3
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-01-07 10:51:47 +0000

    bulk upload specials form

--- site/templates/upload_specials.php
+++ site/templates/upload_specials.php
@@ -1,16 +1,27 @@
+<form action="" method="post" accept-charset="utf-8" enctype="multipart/form-data">
+<input type="file" name="file">
+<input type="submit" name="btn_submit" value="Upload File" />
 
 <?php
+$fh = fopen($_FILES['file']['tmp_name'], 'r+');
+$lines = array();
+while( ($row = fgetcsv($fh, 8192)) !== FALSE ) {
+	$lines[] = $row;
+}
+var_dump($lines);
+
+
 $row = 1;
 if (($handle = fopen("test.csv", "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $num = count($data);
-        echo "<p> $num fields in line $row: <br /></p>\n";
-        $row++;
-        for ($c=0; $c < $num; $c++) {
-            echo $data[$c] . "<br />\n";
-        }
+  while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+    $num = count($data);
+    echo "<p> $num fields in line $row: <br /></p>\n";
+    $row++;
+    for ($c=0; $c < $num; $c++) {
+      echo $data[$c] . "<br />\n";
     }
-    fclose($handle);
+  }
+  fclose($handle);
 }
 
 $entry = array(
@@ -23,18 +34,3 @@ $entry = array(
 $form = $forms->load('contact');
 $form->entries()->save($entry);
 ?>
-<table width="600">
-<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post" enctype="multipart/form-data">
-
-<tr>
-<td width="20%">Select file</td>
-<td width="80%"><input type="file" name="file" id="file" /></td>
-</tr>
-
-<tr>
-<td>Submit</td>
-<td><input type="submit" name="submit" /></td>
-</tr>
-
-</form>
-</table>
