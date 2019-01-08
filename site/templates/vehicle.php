fc9b306469ea9958431fbb8a7a5ba8a71dfc8b7e
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-01-08 14:33:29 +0000

    changing vehicles pages format

--- site/templates/vehicle.php
+++ site/templates/vehicle.php
@@ -16,7 +16,7 @@ require_once(MR_PATH . "/inc/conn.php");
 include "inc/functions.php";
 include "inc/manufacturer.php";
 include "power_search.php";
-$_GET['vid'] = $input->urlSegment1;
+$_GET['vid'] = $input->urlSegment();
 try
 {
   $username = '173210';
@@ -24,7 +24,7 @@ try
   $date = date('c');
   $client = get_soap_client_2();
 
-  $params = array('subscriberId' => $username, 'password' => $password, 'database' => 'car', 'capid' => $input->urlSegment1, 'seDate' => $date, 'justCurrent' => true ); //define your parameters here
+  $params = array('subscriberId' => $username, 'password' => $password, 'database' => 'car', 'capid' => $input->urlSegment(), 'seDate' => $date, 'justCurrent' => true ); //define your parameters here
   $client->GetStandardEquipment($params);
   $data = $client->__getLastResponse();
   $xml = str_replace(array("diffgr:","msdata:"),'', trim($data));
@@ -32,7 +32,7 @@ try
   $groups = array_unique($data->xpath('//SE/Dc_Description'));
   $equipment = $data->xpath('//SE');
 
-  $params2 = array('subscriberId' => $username, 'password' => $password, 'database' => 'car', 'capid' => $input->urlSegment1, 'techDate' => $date, 'justCurrent' => true ); //define your parameters here
+  $params2 = array('subscriberId' => $username, 'password' => $password, 'database' => 'car', 'capid' => $input->urlSegment(), 'techDate' => $date, 'justCurrent' => true ); //define your parameters here
   $client->GetTechnicalData($params2);
   $data2 = $client->__getLastResponse();
   $xml2 = str_replace(array("diffgr:","msdata:"),'', trim($data2));
@@ -41,7 +41,7 @@ try
   $tech_data = $data2->xpath('//Tech');
 
   if(isset($input->urlSegment1)) {
-    $query = "SELECT `id`,`cap_id`,`cap_code`,`src`,`manufacturer`,`model`,`descr`,`term`,`mileage`,min(`rental`) AS `rental`,`vehicle_list_price`,`vehicle_otr_price`,`p11d_price`,`CO2`,`lcv`,`upfront` FROM `team`.`rates_combined_terse` WHERE `cap_id` = " . $input->urlSegment1 . " ORDER BY `rental` ASC LIMIT 1";
+    $query = "SELECT `id`,`cap_id`,`cap_code`,`src`,`manufacturer`,`model`,`descr`,`term`,`mileage`,min(`rental`) AS `rental`,`vehicle_list_price`,`vehicle_otr_price`,`p11d_price`,`CO2`,`lcv`,`upfront` FROM `team`.`rates_combined_terse` WHERE `cap_id` = " . $input->urlSegment() . " ORDER BY `rental` ASC LIMIT 1";
     // echo $query;
     $result = $conn->query($query) or die(mysqli_error($conn));
     $data = $result->fetch_assoc();
