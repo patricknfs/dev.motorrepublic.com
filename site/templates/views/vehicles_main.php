2e5136fbb7de8d5dda02058d2d5cdf6bf1942bac
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-01-09 00:45:42 +0000

    Funder Panel

--- site/templates/views/vehicles_main.php
+++ site/templates/views/vehicles_main.php
@@ -31,14 +31,15 @@
 					);
 					$vehicle_type = ($vehicle['lcv'] == 1?"LCV":"CAR");
 					$hashcode = strtoupper(md5("173210NfS4Je" . $vehicle_type . $vehicle['cap_id']));
-					
+
 					// $bch_rental = number_format(((($vehicle['rental'] * $vehicle['term']) + 300) / ($vehicle['term']+8)), 2, '.', ',');
 					// $pch_rental = number_format(((($vehicle['rental'] * $vehicle['term']) + 300) / ($vehicle['term']+8)*1.2), 2, '.', ',');
 
-					$bch_rental = number_format($vehicle['rental'], 2, '.', ',');
-					$pch_rental = number_format(($vehicle['rental']*1.2), 2, '.', ',');
+					// $bch_rental = number_format($vehicle['rental'], 2, '.', ',');
+					// $pch_rental = number_format(($vehicle['rental']*1.2), 2, '.', ',');
 
-					
+					(($vehicle_type == 1)?$bch_rental = number_format($vehicle['rental'], 2, '.', ','):$bch_rental = number_format(((($vehicle['rental'] * $vehicle['term']) + 300) / ($vehicle['term']+8)), 2, '.', ','));
+					(($vehicle_type == 1)?$pch_rental = number_format(($vehicle['rental']*1.2), 2, '.', ','):$pch_rental = number_format(((($vehicle['rental'] * $vehicle['term']) + 300) / ($vehicle['term']+8)*1.2), 2, '.', ','));
 					?>
 					<div class="cell">
 						<a href="/vehicle/<?=$vehicle['cap_id']?>">
