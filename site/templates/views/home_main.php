127b9f7a162bfb53d2db576d969dd7f0979d5c31
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-01-09 02:16:18 +0000

    Home page link buttons

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -94,11 +94,15 @@
 						</a>
 					</div>
 					<div class="cell small-12 card">
-						<h3>Our Panel of Funders</h3>
-						<img src="<?=$config->urls->assets?>graphics/who_we_work_with_email_090118.png">
+						<div class="card-section">
+							<h3>Our Panel of Funders</h3>
+							<img src="<?=$config->urls->assets?>graphics/who_we_work_with_email_090118.png">
+						</div>
 					</div>
 					<div class="cell small-12 card">
-						<?=$page->regional_strength?>
+						<div class="card-section">
+							<?=$page->regional_strength?>
+						</div>
 					</div>
 				</div>
 				
