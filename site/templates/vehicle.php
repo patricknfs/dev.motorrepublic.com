3988ae82f9e08782957c9e4aa0702dff6dee1552
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-01-09 01:25:22 +0000

    Funder Panel

--- site/templates/vehicle.php
+++ site/templates/vehicle.php
@@ -42,8 +42,8 @@ try
     else {
       $bch_rental = number_format(((($data['rental'] * $data['term']) + 300) / ($data['term']+8)), 2, '.', ',');
       $pch_rental = number_format(((($data['rental'] * $data['term']) + 300) / ($data['term']+8)*1.2), 2, '.', ',');
-      $bch_initial = number_format(($data['rental'] * 9), 2, '.', ',');
-      $pch_initial = number_format((($data['rental'] * 9)*1.2), 2, '.', ',');
+      $bch_initial = number_format((((($data['rental'] * $data['term']) + 300) / ($data['term']+8))*9), 2, '.', ',');
+      $pch_initial = number_format((((($data['rental'] * $data['term']) + 300) / ($data['term']+8)*1.2)*9), 2, '.', ',');
     }
     
     // $hashcode = strtoupper(md5("173210NfS4JeCAR" . $input->urlSegment1));
