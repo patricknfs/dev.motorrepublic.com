4899ac7abc148f30e0d292f9a6e37ba0353a7ba5
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-01-14 15:08:46 +0000

    sorting column order

--- site/templates/views/vehicle_main.php
+++ site/templates/views/vehicle_main.php
@@ -5,7 +5,7 @@
 			// var_dump($data);
 			?>
 			<div class="cell small-12 medium-8 small-order-2 medium-order-1">
-				<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=<?=$vehicle_type?>&CAPID=<?=$input->urlSegment1;?>&DATE=2018/09/11&WIDTH=800&HEIGHT=600&IMAGETEXT=&VIEWPOINT=">
+				<img class="large_vehicle_image" src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=<?=$vehicle_type?>&CAPID=<?=$input->urlSegment1;?>&DATE=2018/09/11&WIDTH=800&HEIGHT=600&IMAGETEXT=&VIEWPOINT=">
 				<section id="vehicle_details">
 		<!--<div class="grid-container"> -->
 						<div class="grid-x">
@@ -50,7 +50,7 @@
 				</section>
 			</div>
 			<div class="cell small-12 medium-4 small-order-1 medium-order-2">
-			<img src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=<?=$vehicle_type?>&CAPID=<?=$input->urlSegment1;?>&DATE=2018/09/11&WIDTH=800&HEIGHT=600&IMAGETEXT=&VIEWPOINT=">
+			<img class="small_vehicle_image" src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=<?=$vehicle_type?>&CAPID=<?=$input->urlSegment1;?>&DATE=2018/09/11&WIDTH=800&HEIGHT=600&IMAGETEXT=&VIEWPOINT=">
 				<div class="card card-2">
 					<div class="card-section">
 						<h3><?=$manufacturer?> <?=$model?></h3>
