37720c7aec7c607468baf4de6fd04c1a60f17108
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-01-15 15:40:52 +0000

    reinstate mobile search

--- site/templates/slice_franchisee_data.php
+++ site/templates/slice_franchisee_data.php
@@ -11,19 +11,23 @@ $adminEmail = "patrick.ogorman@nationalfleetservices.net";
 $AdminMessage = "MR Slice Report\n";
 // Now open the local file and loop through it.
 
-
 if (($handle = fopen("repos/" . $csv, "r")) !== FALSE) {
   while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
     $data = array_map('trim',$data);
-    echo $data[0]; // company
-    echo $data[1]; // town
-    echo $data[2]; // county
-    echo $data[3]; // postcode
-    echo $data[4]; // prefix
-    echo $data[5]; // fname
-    echo $data[6]; // sname
-    echo $data[7]; // jobrole
-    echo $data[8]; // email
+    // echo $data[0]; // company
+    // echo $data[1]; // town
+    // echo $data[2]; // county
+    // echo $data[3]; // postcode
+    // echo $data[4]; // prefix
+    // echo $data[5]; // fname
+    // echo $data[6]; // sname
+    // echo $data[7]; // jobrole
+    // echo $data[8]; // email
+    $pc_bhams_worcsn = array('B14','B27','B30','B31','B38','B40','B45','B47','B48','B49','B50','B60','B61','B80','B90','B91','B92','B93','B94','B95','B96','B97','B98','DY13','WR9')
+    $postcode = explode(' ', $data[3]);
+    if(in_array($postcode, $pc_bhams_worcsn)){
+      echo $postcode;
+    }
   }
 }
 ?>
