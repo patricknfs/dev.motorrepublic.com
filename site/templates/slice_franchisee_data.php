5cb236732b9b355b002f82ef859050fa62e25826
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-01-15 11:32:51 +0000

    creating new slice mailing data

---
+++ site/templates/slice_franchisee_data.php
@@ -0,0 +1,20 @@
+<?php
+$csv = "north_uk.csv";
+
+date_default_timezone_set('CET');
+require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
+require_once(MR_PATH . "/inc/conn.php");
+
+//Open local file to write to
+// $fp = fopen("/var/www/vhosts/dootet.com/stats.dootet.com/data/affilired.csv", "w");
+$adminEmail = "patrick.ogorman@nationalfleetservices.net";
+$AdminMessage = "MR Slice Report\n";
+// Now open the local file and loop through it.
+
+
+if (($handle = fopen("inc/" . $csv, "r")) !== FALSE) {
+  while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
+    echo $data['company'];
+  }
+}
+?>
