d88b91d35611f1037f386a684fe8c1f88a8dfa49
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-01-18 15:46:43 +0000

    fix specials issue

--- prot/get_row_details_combined_terse.php
+++ prot/get_row_details_combined_terse.php
@@ -7,24 +7,24 @@ date_default_timezone_set('CET');
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/conn.php';
 
-$truncate = "TRUNCATE TABLE `team`.`rates_combined_terse_copy`";
+$truncate = "TRUNCATE TABLE `team`.`rates_combined_terse`";
 $result2 = $conn->query($truncate) or die(mysqli_error());
 
 $query = "SELECT t1.*
   FROM (
-    SELECT cap_id, src, MIN(rental) AS rent FROM team.rates_combined_copy GROUP BY cap_id
-  ) as t2 INNER JOIN team.rates_combined_copy AS t1 ON t1.cap_id = t2.cap_id AND t1.rental = t2.rent
+    SELECT cap_id, src, MIN(rental) AS rent FROM team.rates_combined GROUP BY cap_id
+  ) as t2 INNER JOIN team.rates_combined AS t1 ON t1.cap_id = t2.cap_id AND t1.rental = t2.rent
   ORDER BY t1.rental ASC
 ";
-echo $query;
+// echo $query;
 $result = $conn->query($query) or die(mysqli_error($conn));
 // iterate over every row
 $row = 1;
 
 while ($row = mysqli_fetch_assoc($result)) {
   // for every field in the result..
-  $insert = "REPLACE INTO `team`.`rates_combined_terse_copy` VALUES ('','" . $row['cap_id'] . "', '" . $row['cap_code'] . "', '" . $row['src'] . "', '" . $row['updated'] . "', '', '" . $row['manufacturer'] . "', '" . $row['model'] . "', '" . $row['descr'] . "', '" . $row['term'] . "', '" . $row['mileage'] . "', '" . $row['rental'] . "', '" . $row['vehicle_list'] . "', '" . $row['vehicle_otr'] . "', '" . $row['p11d'] . "', '" . $row['CO2_no'] . "', '" . $row['lcv'] . "', '" . $row['special'] . "', '" . $row['deal_notes'] . "', '" . $row['upfront'] . "', '" . str_replace(',','',$row['special_upfront']) . "', '" . $row['website_deal_notes'] . "')";
-  echo $insert;
+  $insert = "REPLACE INTO `team`.`rates_combined_terse` VALUES ('','" . $row['cap_id'] . "', '" . $row['cap_code'] . "', '" . $row['src'] . "', '" . $row['updated'] . "', '', '" . $row['manufacturer'] . "', '" . $row['model'] . "', '" . $row['descr'] . "', '" . $row['term'] . "', '" . $row['mileage'] . "', '" . $row['rental'] . "', '" . $row['vehicle_list'] . "', '" . $row['vehicle_otr'] . "', '" . $row['p11d'] . "', '" . $row['CO2_no'] . "', '" . $row['lcv'] . "', '" . $row['special'] . "', '" . $row['deal_notes'] . "', '" . $row['upfront'] . "', '" . str_replace(',','',$row['special_upfront']) . "', '" . $row['website_deal_notes'] . "')";
+  // echo $insert;
   $result3 = $conn->query($insert) or die(mysqli_error($conn));
   $row++;
 } 
