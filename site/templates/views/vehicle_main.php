566a3b280c1944cdb2d7b03596896b365fbe1f6f
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-01-28 12:53:17 +0000

    add robots.txt

--- site/templates/views/vehicle_main.php
+++ site/templates/views/vehicle_main.php
@@ -1,9 +1,6 @@
 <section id="vehicle">
 	<div class="grid-container">
 		<div class="grid-x grid-padding-x">
-			<?php
-			// var_dump($data);
-			?>
 			<div class="cell small-12 medium-8 small-order-2 medium-order-1">
 				<img class="large_vehicle_image" src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=<?=$vehicle_type?>&CAPID=<?=$input->urlSegment1;?>&DATE=2018/09/11&WIDTH=800&HEIGHT=600&IMAGETEXT=&VIEWPOINT=">
 				<section id="vehicle_details">
@@ -70,11 +67,11 @@
 					</div>
 					<?php
 					if($special == 1){
-					?>
-					<div class="card-section special">
-						<h4>Special Deal<h4>
-					</div>
-					<?php
+						?>
+						<div class="card-section special">
+							<h4>Special Deal<h4>
+						</div>
+						<?php
 					}
 					?>
 				</div>
