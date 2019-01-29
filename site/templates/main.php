8f8cc1eafc5c2a59e6ad4547590fcf0f4b688a54
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-01-29 08:15:38 +0000

    change header default sizes

--- site/templates/main.php
+++ site/templates/main.php
@@ -98,7 +98,7 @@ if($page->id !== 1043){
 
       gtag('config', 'UA-81524203-1');
     </script>
-    <link rel="alternate" href="https://www.motorrepublic.com" hreflang="en-GB" />
+    <link rel="alternate" href="https://motorrepublic.com" hreflang="en-gb" />
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/zf/dt-1.10.18/b-1.5.2/b-colvis-1.5.1/b-html5-1.5.2/b-print-1.5.2/cr-1.5.0/fc-3.2.5/fh-3.1.4/kt-2.4.0/r-2.2.2/sc-1.5.0/sl-1.2.6/datatables.min.css"/>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/select/1.2.7/css/select.dataTables.min.css">
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
