0e333f6038152929b03f3ae19d3a1c566858e1ff
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-01-30 08:56:39 +0000

    creating sitemap

--- site/templates/sitemap-xml.php
+++ site/templates/sitemap-xml.php
@@ -1,12 +1,17 @@
 <?php
 	namespace ProcessWire;
 	$pageArray = $pages->find("template=about|basic-page|franchisee-region|vehicle|vehicles|vehicle_v|post|posts|testimonial|testimonials|whatwedo, limit=200");
-	// print_r($pageArray);
-	$out =  '<?xml version="1.0" encoding="UTF-8"?>' . "\n" . '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+	$query = "SELECT `id`,`cap_id`,`cap_code`,`src`,`manufacturer`,`model`,`descr`,`term`,`mileage`, `rental`,`vehicle_list_price`,`vehicle_otr_price`,`p11d_price`,`CO2`, `lcv`, `special` FROM `team`.`rates_combined_terse` GROUP BY `cap_id` ORDER BY `special` DESC, `rental` ASC LIMIT";
+	$result = $conn->query($query) or die(mysqli_error($conn));
 
+	$out =  '<?xml version="1.0" encoding="UTF-8"?>' . "\n" . '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
 	foreach ($pageArray as $post) {
 	  $out .= "\n<url>\n\t<loc>" . $post->httpUrl . "</loc>\n\t<lastmod>" . date("Y-m-d", $post->modified) . "</lastmod>\n</url>";
+	}
+
+	foreach ($result as $post2) {
+		$out .= "\n<url>\n\t<loc>https://motorrepublic.com/vehicle/" . $post2['cap_id'] . "</loc>\n\t<lastmod>" . date("Y-m-d", $post2->modified) . "</lastmod>\n</url>";
 	}
 
 	$out .= "\n</urlset>";
