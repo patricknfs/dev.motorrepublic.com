0b4126f4da4be2fc0031dd1573b5c9146a8bf150
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-01-30 08:18:26 +0000

    creating sitemap

--- site/templates/sitemap-xml.php
+++ site/templates/sitemap-xml.php
@@ -1,10 +1,10 @@
 <?php
 	namespace ProcessWire;
-
+	$pageArray = $pages->find("template=about|basic-page|franchisee-region|vehicle|vehicles|vehicle_v|post|posts|testimonila|testimonials|whatwedo, limit=200");
+	print_r($pageArray);
 	$out =  '<?xml version="1.0" encoding="UTF-8"?>' . "\n" . '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
-	$pageArray = $pages->find("template=about|basic-page|franchisee-region|vehicle|vehicles|vehicle_v|post|posts|testimonila|testimonials|whatwedo, limit=200");
-	// print_r($pageArray);
+
 	foreach ($pageArray as $post) {
 	  $out .= "\n<url>\n\t<loc>" . $post->httpUrl . "</loc>\n\t<lastmod>" . date("Y-m-d", $post->modified) . "</lastmod>\n</url>";
 	}
