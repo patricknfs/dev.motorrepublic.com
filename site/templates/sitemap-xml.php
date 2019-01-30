be8cd9613adba1fb23ac664efea141ff2043efa1
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-01-30 08:18:56 +0000

    creating sitemap

--- site/templates/sitemap-xml.php
+++ site/templates/sitemap-xml.php
@@ -2,16 +2,16 @@
 	namespace ProcessWire;
 	$pageArray = $pages->find("template=about|basic-page|franchisee-region|vehicle|vehicles|vehicle_v|post|posts|testimonila|testimonials|whatwedo, limit=200");
 	print_r($pageArray);
-	$out =  '<?xml version="1.0" encoding="UTF-8"?>' . "\n" . '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+	// $out =  '<?xml version="1.0" encoding="UTF-8"?>' . "\n" . '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
 
-	foreach ($pageArray as $post) {
-	  $out .= "\n<url>\n\t<loc>" . $post->httpUrl . "</loc>\n\t<lastmod>" . date("Y-m-d", $post->modified) . "</lastmod>\n</url>";
-	}
+	// foreach ($pageArray as $post) {
+	//   $out .= "\n<url>\n\t<loc>" . $post->httpUrl . "</loc>\n\t<lastmod>" . date("Y-m-d", $post->modified) . "</lastmod>\n</url>";
+	// }
 
-	$out .= "\n</urlset>";
+	// $out .= "\n</urlset>";
 
-	header("Content-Type: text/xml");
+	// header("Content-Type: text/xml");
 
-	echo $out;
+	// echo $out;
 ?>
