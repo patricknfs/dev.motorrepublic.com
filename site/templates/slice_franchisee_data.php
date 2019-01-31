eb16217f4d5241b161d1be68a6a53762f1567428
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-01-31 15:35:53 +0000

    updated slice franchisee script

--- site/templates/slice_franchisee_data.php
+++ site/templates/slice_franchisee_data.php
@@ -1,5 +1,6 @@
 <?php
-$csv = "repos/mailing_north_uk.csv";
+// $csv = "repos/mailing_north_uk.csv";#
+$csv = "repos/mailing_south_uk.csv";
 
 date_default_timezone_set('CET');
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
