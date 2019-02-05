60b99468d6c1981a9199dacd2f03726423bf6aea
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-02-05 14:59:37 +0000

    working on best rates drop down

--- site/templates/views/rate_tools_main.php
+++ site/templates/views/rate_tools_main.php
@@ -101,37 +101,21 @@
       "scroller": true,
       "select": true
 		});
-
-    var detailRows = [];
     // Add event listener for opening and closing details
     $('#mr_rates tbody').on('click', 'td.details-control', function () {
       var tr = $(this).closest('tr');
-        var row = dt.row( tr );
-        var idx = $.inArray( tr.attr('id'), detailRows );
- 
-        if ( row.child.isShown() ) {
-            tr.removeClass( 'details' );
-            row.child.hide();
- 
-            // Remove from the 'open' array
-            detailRows.splice( idx, 1 );
-        }
-        else {
-            tr.addClass( 'details' );
-            row.child( format( row.data() ) ).show();
- 
-            // Add to the 'open' array
-            if ( idx === -1 ) {
-                detailRows.push( tr.attr('id') );
-            }
-        }
-    } );
- 
-    // On each draw, loop over the `detailRows` array and show any child rows
-    dt.on( 'draw', function () {
-        $.each( detailRows, function ( i, id ) {
-            $('#'+id+' td.details-control').trigger( 'click' );
-        } );
-    } );
+      var row = table.row( tr );
+
+      if ( row.child.isShown() ) {
+          // This row is already open - close it
+          row.child.hide();
+          tr.removeClass('shown');
+      }
+      else {
+          // Open this row
+          row.child( format(row.data()) ).show();
+          tr.addClass('shown');
+      }
+    });
 	});
 </script>
