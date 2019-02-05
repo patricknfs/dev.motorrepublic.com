7092dfec7e1eaddc670d923ad8f312d4259ba196
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-02-05 09:20:36 +0000

    update alpahabet ratebooks

--- site/templates/alphabet_rates_read.php
+++ site/templates/alphabet_rates_read.php
@@ -19,9 +19,9 @@ $row = 1;
 $csv = "repos/alphabet_rates_all.csv";
 if (($handle = fopen($csv , "r")) !== FALSE) {
   while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
-    // $data = preg_replace('/\s+/', '', $data);
+    $data = preg_replace('/\s+/', '', $data);
     $data = str_replace('£','',$data);
-    // $data = str_replace('#N/A',NULL,$data);
+    $data = str_replace('#N/A',NULL,$data);
     if($row > 7){
       $cap_query = "SELECT `cap_id` FROM `team`.`vehicles` WHERE `cap_code` = '" . trim($data[0]) . "' LIMIT 1";
       // echo $cap_query . "</n>";
