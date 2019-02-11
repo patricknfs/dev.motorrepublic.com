47ab053e1d10047011350b88eea5d79deaef860a
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-02-11 10:34:52 +0000

    main

--- site/templates/inc/config.php
+++ site/templates/inc/config.php
@@ -8,6 +8,7 @@ switch($http_host){
     define("MR_DB_SERVER", "localhost");
     define("MR_DB_DATABASE", "team");
     define("MR_PATH", "/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates");
+    define("MR_PATH2", "/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/");
     break;
 
   default :
--- site/templates/main.php
+++ site/templates/main.php
@@ -2,7 +2,7 @@
 <?php 
 ERROR_REPORTING(E_ALL);
 date_default_timezone_set('CET');
-require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
+require_once(MR_PATH2 . "/site/templates/inc/config.php";
 require_once(MR_PATH . "/inc/conn.php");
 if($page->id !== 1043){
   include "power_search.php";
