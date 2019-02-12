cef567d573b6194a304bcb07f7fa2e7d1e6e074a
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-02-12 21:17:36 +0000

    update home_main

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -20,12 +20,12 @@
 <section id="block_one">
 	<div class="grid-container">
 		<div class="grid-x grid-margin-x">
-			<div class="cell small-12 medium-6" style="background: url('<?=$config->urls->assets . "/images/mercedes_a-class_2018_120219_600.jpg"?>') cover">
+			<div class="cell small-12 medium-6" style="background: url('<?=$config->urls->assets . "images/mercedes_a-class_2018_120219_600.jpg"?>')  no-repeat center center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;">
 				<div class="cell small-4" style="height:10em;">
 					<h1>Click here to go straight to our car specials</h1>
 				</div>
 			</div>
-			<div class="cell small-12 medium-6" style="background: url('<?=$config->urls->assets . '/images/ford_transit_120219_600.jpg'?>') cover">>
+			<div class="cell small-12 medium-6" style="background: url('<?=$config->urls->assets . 'images/ford_transit_120219_600.jpg'?>')  no-repeat center center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;">>
 				<div class="cell small-4" style="height:10em;">
 					<h1>Click here to go straight to our van and pickup specials</h1>
 				</div>
