76f40ae49664bd81cd8f048ed5863dc99167de8e
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-02-14 22:48:14 +0000

    updating home page

--- site/templates/main.php
+++ site/templates/main.php
@@ -321,7 +321,7 @@ if($page->id !== 1043){
         echo " else if(s1.value == '" . $row['manufacturer'] . "'){
           var optionArray = ['|',";
           $query2 = "SELECT DISTINCT(`model`), `lcv` FROM `team`.`rates_combined_terse` WHERE `manufacturer` = '" . $row['manufacturer'] . "' AND `special` = 1 ORDER BY `model` ASC";
-          // echo $query2;
+          echo $query2;
           $result2 = mysqli_query($conn, $query2);
           while ($row2 = mysqli_fetch_array($result2)) {
             echo "'" . $row2['model'] . "|" . $row2['model'] . "|" . $row2['lcv'] . "',";
