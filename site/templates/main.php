806002d355015e8ab9182a5e543be30f5792b72f
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-02-14 22:43:17 +0000

    updating home page

--- site/templates/main.php
+++ site/templates/main.php
@@ -319,11 +319,11 @@ if($page->id !== 1043){
       while ($row = mysqli_fetch_assoc($result)) {
         echo " else if(s1.value == '" . $row['manufacturer'] . "'){
           var optionArray = ['|',";
-          $query2 = "SELECT DISTINCT(`model`) FROM `team`.`rates_combined_terse` WHERE `manufacturer` = '" . $row['manufacturer'] . "' AND `special` = 1 ORDER BY `model` ASC";
+          $query2 = "SELECT DISTINCT(`model`), `lcv` FROM `team`.`rates_combined_terse` WHERE `manufacturer` = '" . $row['manufacturer'] . "' AND `special` = 1 ORDER BY `model` ASC";
           // echo $query2;
           $result2 = mysqli_query($conn, $query2);
           while ($row2 = mysqli_fetch_array($result2)) {
-            echo "'" . $row2['model'] . "|" . $row2['model'] . "',";
+            echo "'" . $row2['model'] . "|" . $row2['model'] . "|" . $row2['lcv'] . "',";
           }
         echo "]}";
       }
@@ -337,6 +337,7 @@ if($page->id !== 1043){
         newOption.value = pair[0];
         newOption.innerHTML = pair[1];
         s2.options.add(newOption);
+        s3.value = pair[2];
       }
     }
   </script>
