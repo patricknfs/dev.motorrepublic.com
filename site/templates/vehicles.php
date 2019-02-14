178e9d40f310fd01498fb7ba1b73e4dc6284c83a
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-02-14 23:45:16 +0000

    updating vehicle search

--- site/templates/vehicles.php
+++ site/templates/vehicles.php
@@ -33,6 +33,13 @@ if($manuf){
   $lcv = $mdllcv[1];
 }
 
+if($input->urlSegment1() == "van-leasing-hgv"){
+  $lcv2 = 1;
+}
+else{
+  $lcv2 = 0;
+}
+
 // echo $mdl;
 
 if( !empty($manuf) ) {
@@ -50,7 +57,7 @@ else {
   $countres = $conn->query($total_pages_sql);
   $total_rows = $countres->num_rows;
   $total_pages = ceil($total_rows / $no_of_records_per_page);
-  $query = "SELECT `id`,`cap_id`,`cap_code`,`src`,`manufacturer`,`model`,`descr`,`term`,`mileage`, `rental`,`vehicle_list_price`,`vehicle_otr_price`,`p11d_price`,`CO2`, `lcv`, `special` FROM `team`.`rates_combined_terse` WHERE `lcv` = 0 AND `special` = 1 GROUP BY `cap_id` ORDER BY `special` DESC, `rental` ASC LIMIT $offset, $no_of_records_per_page";
+  $query = "SELECT `id`,`cap_id`,`cap_code`,`src`,`manufacturer`,`model`,`descr`,`term`,`mileage`, `rental`,`vehicle_list_price`,`vehicle_otr_price`,`p11d_price`,`CO2`, `lcv`, `special` FROM `team`.`rates_combined_terse` WHERE `lcv` = '" . $lcv2 . "' AND `special` = 1 GROUP BY `cap_id` ORDER BY `special` DESC, `rental` ASC LIMIT $offset, $no_of_records_per_page";
   // echo "else " . $query;
 }
 
