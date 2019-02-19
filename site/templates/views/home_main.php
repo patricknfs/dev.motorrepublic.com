793dacbc71eb55f2e64dd8a10dbc1d8671b0c901
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-02-19 10:19:35 +0000

    updating landrover specials

--- site/templates/views/home_main.php
+++ site/templates/views/home_main.php
@@ -34,13 +34,13 @@
 		<div class="grid-x grid-margin-x">
 			<div class="cell small-12 medium-6 grid-margin-y">
 				<div class="cell small-12">
-					<a href="http://dev.motorrepublic.com/van-leasing-hgv/?slct1=BMW&slct2=-undefined"><img src="<?=$config->urls->assets?>graphics/bmw_specials_600.png" /></a>
+					<a href="/van-leasing-hgv/?slct1=BMW&slct2=-undefined"><img src="<?=$config->urls->assets?>graphics/bmw_specials_600.png" /></a>
 				</div>
 				<div class="cell small-12 medium-6">
-					<a href="http://dev.motorrepublic.com/van-leasing-hgv/?slct1=MERCEDES&slct2=-undefined"><img src="<?=$config->urls->assets?>graphics/mercedes_specials_600.png" /></a>
+					<a href="/van-leasing-hgv/?slct1=MERCEDES&slct2=-undefined"><img src="<?=$config->urls->assets?>graphics/mercedes_specials_600.png" /></a>
 				</div>
 				<div class="cell small-12 medium-6">
-					<a href="http://dev.motorrepublic.com/van-leasing-hgv/?slct1=LAND+ROVER&slct2=-undefined"><img src="<?=$config->urls->assets?>graphics/landrover_specials_600.png" /></a>
+					<a href="/van-leasing-hgv/?slct1=LAND+ROVER&slct2=-undefined"><img src="<?=$config->urls->assets?>graphics/landrover_specials_600.png" /></a>
 				</div>
 				<div class="cell small-12 medium-6">
 
