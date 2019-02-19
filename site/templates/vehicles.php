e0419e8f4e8b5e4467293f2077e6f9995172966f
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-02-19 15:17:24 +0000

    fixing pagination

--- site/templates/vehicles.php
+++ site/templates/vehicles.php
@@ -45,16 +45,11 @@ else{
 if( !empty($manuf) ) {
   $total_pages_sql = $conn->query("SELECT COUNT(*) FROM `team`.`rates_combined_terse` WHERE `manufacturer` = '" . $manuf . "' AND `model` LIKE '%" . $mdl . "%'  AND `lcv` = '" . $lcv . "' AND `special` = 1");
   $total_rows = $total_pages_sql->fetch_row();
-// echo '#: ', $row[0];
-  
-//   $countres = $conn->query($total_pages_sql);
-//   print_r($countres);
-//   $total_rows = $countres->num_rows;
   $total_pages = ceil($total_rows / $no_of_records_per_page);
   $query = "SELECT `id`,`cap_id`,`cap_code`,`src`,`manufacturer`,`model`,`descr`,`term`,`mileage`, `rental`,`vehicle_list_price`,`vehicle_otr_price`,`p11d_price`,`CO2`, `lcv`, `special` FROM `team`.`rates_combined_terse` WHERE `manufacturer` = '" . $manuf . "' AND `model` LIKE '%" . $mdl . "%' AND `lcv` = '" . $lcv . "' AND `special` = 1 GROUP BY `cap_id` ORDER BY `special` DESC, `rental` ASC LIMIT $offset, $no_of_records_per_page";
 }
 else {
-  $total_pages_sql = "SELECT COUNT(*) FROM `team`.`rates_combined_terse` WHERE `special` = 1 AND `lcv` = '" . $lcv2 . "'";
+  $total_pages_sql = $conn->query("SELECT COUNT(*) FROM `team`.`rates_combined_terse` WHERE `manufacturer` = '" . $manuf . "' AND `model` LIKE '%" . $mdl . "%'  AND `lcv` = '" . $lcv . "' AND `special` = 1");
   $total_rows = $total_pages_sql->fetch_row();
   $total_pages = ceil($total_rows / $no_of_records_per_page);
   $query = "SELECT `id`,`cap_id`,`cap_code`,`src`,`manufacturer`,`model`,`descr`,`term`,`mileage`, `rental`,`vehicle_list_price`,`vehicle_otr_price`,`p11d_price`,`CO2`, `lcv`, `special` FROM `team`.`rates_combined_terse` WHERE `lcv` = '" . $lcv2 . "' AND `special` = 1 GROUP BY `cap_id` ORDER BY `special` DESC, `rental` ASC LIMIT $offset, $no_of_records_per_page";
