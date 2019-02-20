48caf7460fe1adf7f7585ae417c2c900943404aa
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-02-20 13:47:18 +0000

    testing home

--- site/templates/views/vehicles_main.php
+++ site/templates/views/vehicles_main.php
@@ -12,7 +12,21 @@
 				<ul class="pagination text-center">
 					<li><a href="?pageno=1">First</a></li>
 					<li class="pagination-previous <?php if($pageno <= 1){ echo 'disabled'; } ?>">
-						<a href="<?php echo ($pageno <= 1 ? '#' : '?pageno=' . ($pageno - 1)) ?>">Previous <span class="show-for-sr">page</span></a>
+						
+						<a href="?
+							<?php
+							if($pageno <= $total_pages){
+								echo "#";
+							}
+							else {
+								if(!empty($manuf)){
+									echo "slct1=" . $manuf . "&slct2=" . $mdl . "&";
+								}
+								echo "pageno=" . ($pageno - 1);
+							}
+							?> 
+							aria-label="Next page">Next <span class="show-for-sr">page</span>
+						</a>
 					</li>
 					<li class="pagination-next <?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
 						<a href="?
