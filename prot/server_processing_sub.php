7754c71a54d47d2b2d9494a463ea07674fd6aff3
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-02-25 11:38:48 +0000

    update best rates deal notes

--- prot/server_processing_sub.php
+++ prot/server_processing_sub.php
@@ -3,14 +3,14 @@ session_start();
 date_default_timezone_set('GMT');
 require_once $_SERVER['DOCUMENT_ROOT'] . '/site/templates/inc/config.php';
 echo $_SERVER['DOCUMENT_ROOT'];
-require_once(PPW_PATH . "/inc/conn2.php");
+require_once(PPW_PATH . "/inc/conn.php");
 require_once(PPW_PATH . "/inc/functions.php");
 
 $tid = inputGet('tid');
-mysqli_query($conn2, 'SET CHARACTER SET utf8');
+mysqli_query($conn, 'SET CHARACTER SET utf8');
 $query2 = "SELECT `vehicle_list_price`,`vehicle_otr_price`, `p11d_price`, `CO2` FROM `rates_combined` WHERE `id` = " . $tid . " LIMIT 1";
 // echo $query2;
-$result2 = $conn2->query($query2)  or die(mysqli_error());
+$result2 = $conn->query($query2)  or die(mysqli_error());
 // iterate over every row
 while ($row = mysqli_fetch_assoc($result2)) {
 	// for every field in the result..
