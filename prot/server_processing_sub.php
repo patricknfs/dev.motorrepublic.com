fd0234734b98034c806d1fcce28f524221e710d7
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-02-25 11:36:22 +0000

    update best rates deal notes

--- prot/server_processing_sub.php
+++ prot/server_processing_sub.php
@@ -2,6 +2,7 @@
 session_start();
 date_default_timezone_set('GMT');
 require_once $_SERVER['DOCUMENT_ROOT'] . '/site/templates/inc/config.php';
+echo $_SERVER['DOCUMENT_ROOT'];
 require_once(PPW_PATH . "/inc/conn2.php");
 require_once(PPW_PATH . "/inc/functions.php");
 
