546af8e30400ecf3a77a8347b93145cf2a884ba9
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-02-26 09:52:31 +0000

    update best rates deal notes

--- prot/server_processing_sub.php
+++ prot/server_processing_sub.php
@@ -8,7 +8,7 @@ require_once(MR_PATH . "/inc/functions.php");
 // echo "tid is " . $_GET['tid'];
 $tid = filter_input(INPUT_GET, "tid", FILTER_SANITIZE_STRING);
 mysqli_query($conn, 'SET CHARACTER SET utf8');
-$query2 = "SELECT `vehicle_list_price`,`vehicle_otr_price`, `p11d_price`, `CO2`, `deal_notes` FROM `rates_combined` WHERE `id` = " . $_GET['tid'] . " LIMIT 1";
+$query2 = "SELECT `vehicle_list_price`,`vehicle_otr_price`, `p11d_price`, `CO2`, `deal_notes` FROM `rates_combined` WHERE `id` = '" . $tid . "' LIMIT 1";
 // echo $query2;
 $result2 = $conn->query($query2)  or die(mysqli_error($query2));
 // iterate over every row
