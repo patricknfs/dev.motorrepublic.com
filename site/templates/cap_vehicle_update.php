79b827f7ee292b486115b5d90d846e93b4fe1566
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-03-01 11:26:52 +0000

    working on cap data updata script

--- site/templates/cap_vehicle_update.php
+++ site/templates/cap_vehicle_update.php
@@ -3,7 +3,7 @@ date_default_timezone_set('CET');
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
 require_once(MR_PATH . "/inc/conn.php");
 include "inc/functions.php";
-include "inc/manufacturer.php";
+// include "inc/manufacturer.php";
 
 $params = array('subscriberId' => $username, 'password' => $password, 'database' => $carlcv, 'capid' => $input->urlSegment(), 'seDate' => $date, 'justCurrent' => true ); //define your parameters here
 $client->GetBulkTechnicalDataResponse($params);
