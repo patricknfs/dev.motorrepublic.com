1bd2b559466990511231be3ef34882fc17b33a7f
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-03-01 10:42:18 +0000

    fix bad urls home page

---
+++ site/templates/cap_vehicle_update.php
@@ -0,0 +1,15 @@
+<?php
+date_default_timezone_set('CET');
+require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
+require_once(MR_PATH . "/inc/conn.php");
+include "inc/functions.php";
+include "inc/manufacturer.php";
+
+$params = array('subscriberId' => $username, 'password' => $password, 'database' => $carlcv, 'capid' => $input->urlSegment(), 'seDate' => $date, 'justCurrent' => true ); //define your parameters here
+$client->GetBulkTechnicalDataResponse($params);
+$data = $client->__getLastResponse();
+$xml = str_replace(array("diffgr:","msdata:"),'', trim($data));
+$data = new SimpleXMLElement($xml);
+$groups = array_unique($data->xpath('//SE/Dc_Description'));
+$equipment = $data->xpath('//SE');
+
