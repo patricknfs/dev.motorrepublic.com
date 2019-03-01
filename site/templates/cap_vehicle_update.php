1eccf7a6758056ae4698a8efc787eedb86d5a2de
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-03-01 11:28:26 +0000

    working on cap data updata script

--- site/templates/cap_vehicle_update.php
+++ site/templates/cap_vehicle_update.php
@@ -5,13 +5,13 @@ require_once(MR_PATH . "/inc/conn.php");
 include "inc/functions.php";
 // include "inc/manufacturer.php";
 
-$params = array('subscriberId' => $username, 'password' => $password, 'database' => $carlcv, 'capid' => $input->urlSegment(), 'seDate' => $date, 'justCurrent' => true ); //define your parameters here
-$client->GetBulkTechnicalDataResponse($params);
-$data = $client->__getLastResponse();
-$xml = str_replace(array("diffgr:","msdata:"),'', trim($data));
-$data = new SimpleXMLElement($xml);
-$groups = array_unique($data->xpath('//SE/Dc_Description'));
-$equipment = $data->xpath('//SE');
+// $params = array('subscriberId' => $username, 'password' => $password, 'database' => $carlcv, 'capid' => $input->urlSegment(), 'seDate' => $date, 'justCurrent' => true ); //define your parameters here
+// $client->GetBulkTechnicalDataResponse($params);
+// $data = $client->__getLastResponse();
+// $xml = str_replace(array("diffgr:","msdata:"),'', trim($data));
+// $data = new SimpleXMLElement($xml);
+// $groups = array_unique($data->xpath('//SE/Dc_Description'));
+// $equipment = $data->xpath('//SE');
 
 if ($result = $mysqli->query("SELECT * FROM `team`.`vehicles`")) {
   printf("Select returned %d rows.\n", $result->num_rows);
