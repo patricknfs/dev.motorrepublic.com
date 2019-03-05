9269880872f4605e0a5b5d80fc6897b147b003b5
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-03-05 10:49:08 +0000

    update best rates cap data

--- site/templates/cap_vehicle_update.php
+++ site/templates/cap_vehicle_update.php
@@ -20,9 +20,9 @@ try
       $params = array('subscriberId' => $username, 'password' => $password, 'database' => 'car', 'capidList' => $cap_id, 'specDateList' => $date, 'techDataList' => 'CC,ENGINEPOWER_PS,CO2,MPG_COMBINED,INSURANCEGROUP1-50,STANDARDMANWARRANTY_MILEAGE,STANDARDMANWARRANTY_YEARS', 'returnVehicleDescription' => true, 'returnCaPcodeTechnicalItems' => true,  'returnCostNew' => true ); //define your parameters here
       $client->GetBulkTechnicalData($params);
       $data = $client->__getLastResponse();
-      var_dump($data);
-      $xml = str_replace(array("diffgr:","msdata:"),'', trim($data));
-      $data = new SimpleXMLElement($xml);
+      // $xml = str_replace(array("diffgr:","msdata:"),'', trim($data));
+      // $data = new SimpleXMLElement($xml);
+      $xml = simplexml_load_string($data);
       // $groups = array_unique($data->xpath('//SE/Dc_Description'));
       // $equipment = $data->xpath('//SE');
       $cc = $data->xpath('//CC');
