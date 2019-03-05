97b4a039acbaf191b1ff1595d2f3d98b8d651bbc
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-03-05 08:29:15 +0000

    update best rates cap data

--- site/templates/cap_vehicle_update.php
+++ site/templates/cap_vehicle_update.php
@@ -26,9 +26,9 @@ try
       // $groups = array_unique($data->xpath('//SE/Dc_Description'));
       // $equipment = $data->xpath('//SE');
       $cc = $data->xpath('//CC');
-      print_r($data->xpath('//CC'));
+      var_dump((string)$cc[0]);
       $co2 = $data->xpath('//CO2');
-      print_r($data->xpath('//CO2'));
+      print_r($co2);
       $enginepower_ps = '';
       $mpg_combined = '';
       $insurancegroup150 = ''; 
