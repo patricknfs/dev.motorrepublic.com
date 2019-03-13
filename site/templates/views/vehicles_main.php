6cd6132aca2442d5be53039536778877c6690eba
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-03-13 12:00:46 +0000

    changing search

--- site/templates/views/vehicles_main.php
+++ site/templates/views/vehicles_main.php
@@ -53,7 +53,12 @@
 								if(!empty($manuf)){
 									echo "?slct1=" . $manuf . "&slct2=" . $mdl . "&";
 								}
-								echo "?pageno=" . ($pageno + 1);
+								if(!empty($manuf)){
+									echo "pageno=" . ($pageno + 1);
+								}
+								else {
+									echo "?pageno=" . ($pageno + 1);
+								}
 							}
 							?> 
 							aria-label="Next page">Next <span class="show-for-sr">page</span>
