82c9cda0bb7dd8cffeb5497e4da11c9edd7807bf
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-03-20 22:04:30 +0000

    redo power search

--- prot/get_vehicle_data.php
+++ prot/get_vehicle_data.php
@@ -1,11 +1,12 @@
 <?php
 date_default_timezone_set('CET');
-require_once "/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php";
-require_once MR_PATH . "/inc/conn.php";
-require_once MR_PATH . "/inc/functions.php";
+
 
 function get_data()
 {
+  require_once "/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php";
+  require_once MR_PATH . "/inc/conn.php";
+  require_once MR_PATH . "/inc/functions.php";
   $query = "SELECT DISTINCT(`manufacturer`) FROM `team`.`rates_combined_terse` WHERE `special` = 1 ORDER BY `manufacturer` ASC";
   $result = mysqli_query($conn, $query);
   $marque_data = array();
