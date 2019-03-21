08238ee6ef6a696e036a870669adbdbe2db1ef8e
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-03-21 20:37:36 +0000

    redo power search

--- site/templates/get_vehicle_data.php
+++ site/templates/get_vehicle_data.php
@@ -19,11 +19,11 @@ function get_data()
   $result = mysqli_query($conn, $query);
   $marque_data = array();
   while ($row = mysqli_fetch_array($result)) {
-    $marque_data[] = array(
+    array_push($marque_data, array(
       'id' => $row["id"],
       'name' => $row["model"],
       'parent_id' => 1
-    );
+    ));
   }
   return json_encode($marque_data, JSON_PRETTY_PRINT);
 }
