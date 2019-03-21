5d19395caf97cebf7f9b2c3b07dd897f0fb4ce64
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-03-21 13:40:25 +0000

    fixing non special page appearance

--- site/templates/views/vehicle_main.php
+++ site/templates/views/vehicle_main.php
@@ -1,10 +1,10 @@
 
 <section id="vehicle">
-<?php
-if($special == 1)
-{
-?>
 	<div class="grid-container">
+		<?php
+		if($special == 1)
+		{
+		?>
 		<div class="grid-x grid-padding-x">
 			<div class="cell small-12 medium-8 small-order-2 medium-order-1">
 				<img class="large_vehicle_image" src="https://soap.cap.co.uk/images/vehicleimage.aspx?SUBID=173210&HASHCODE=<?=$hashcode?>&DB=<?=$vehicle_type?>&CAPID=<?=$input->urlSegment1;?>&DATE=2018/09/11&WIDTH=800&HEIGHT=600&IMAGETEXT=&VIEWPOINT=" alt="<?=$manufacturer?> <?=$model?> - <?=$input->urlSegment1?>" />
@@ -100,12 +100,12 @@ if($special == 1)
 				</div>
 			</div>
 		</div>
+		<?php
+		}
+		else
+		{
+			echo "This offer is no longer available";
+		}
+		?>
 	</div>
-	<?php
-}
-else
-{
-	echo "This offer is no longer available";
-}
-?>
 </section>
