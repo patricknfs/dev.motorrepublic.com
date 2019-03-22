b9e9489ac1893833cf6cea5b237bad25e647abf1
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-03-22 15:38:03 +0000

    redo power search

--- site/templates/main.php
+++ site/templates/main.php
@@ -139,6 +139,13 @@ print_r($_GET);
                 html_code += '<option value="'+value.id+'">'+value.name+'</option>';
               }
             }
+            else if (value.id > 500000)
+            {
+              if(value.parent_id == parent_id)
+              {
+                html_code += '<option value="'+value.id+'">'+value.name+'</option>';
+              }
+            }
             else
             {
               if(value.parent_id == parent_id)
