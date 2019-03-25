c074be7934dbd26e25ceedc6c5a02bd5b625f2a1
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-03-25 15:31:08 +0000

    changing cap api call

--- site/templates/cap_vehicle_update_2.php
+++ site/templates/cap_vehicle_update_2.php
@@ -23,14 +23,37 @@ try
   while ($data = mysqli_fetch_array($result)) 
   {
     $capidset[] = $data['cap_id'];
-    $specdateset[] = '2019/03/24';
   }
   // $capidList = implode(",", array_chunk($capidset, 1000));
   // $specdatelist = implode(",",array_chunk($specdateset,1000));
   $capidlist = array_chunk($capidset, 1000);
+  $specdatelist = str_repeat("2019/03/24");
+  $specdatelist = rtrim($specdatelist,',');
   foreach($capidlist AS $capidchunk)
   {
     print_r($capidchunk);
+    implode(",",$capidchunk);
+    $params = array('subscriberId' => $username, 'password' => $password, 'database' => 'car', 'capidList' => $capidchunk, 'specDateList' => $specdatelist, 'techDataList' => 'CC,ENGINEPOWER_PS,CO2,MPG_COMBINED,INSURANCEGROUP1-50,STANDARDMANWARRANTY_MILEAGE,STANDARDMANWARRANTY_YEARS', 'returnVehicleDescription' => true, 'returnCaPcodeTechnicalItems' => true,  'returnCostNew' => true ); //define your parameters here
+    $client->GetBulkTechnicalData($params);
+    // echo "Response:\n" . $client->__getLastResponse() . "\n";
+    $data_x = $client->__getLastResponse();
+    $xml = str_replace(array("diffgr:","msdata:"),'', trim($data_x));
+    $data = new SimpleXMLElement($xml);
+    print_r($xml);
+    // $groups = array_unique($data->xpath('//SE/Dc_Description'));
+    // $equipment = $data->xpath('//SE');
+    $cc = $data->xpath('//CC');
+    print_r($cc);
+    $co2 = $data->xpath('//CO2');
+    $enginepower_ps = $data->xpath('//ENGINEPOWER_PS');
+    $mpg_combined = $data->xpath('//MPG_COMBINED');
+    $insurancegroup150 = $data->xpath('//INSURANCEGROUP150');
+    $standardmanwarranty_mileage = $data->xpath('//STANDARDMANWARRANTY_MILEAGE');
+    $standardmanwarranty_years = $data->xpath('//STANDARDMANWARRANTY_YEARS');
+    $bodystyle = $data->xpath('//BodyStyle');
+
+    $query2 = "UPDATE `team`.`vehicles` SET `cc` = '" . trim($cc[0]) . "', `co2` = '" . trim($co2[0]) . "', `enginepower_ps` = '" . trim($enginepower_ps[0]) . "', `mpg_combined` = '" . trim($mpg_combined[0]) . "', `insurancegroup1-50` = '" . trim($insurancegroup150[0]) ."', `standardmanwarranty_mileage` = '" . trim($standardmanwarranty_mileage[0]) . "', `standardmanwarranty_years` = '" . trim($standardmanwarranty_years[0]) . "', `bodystyle` = '" . trim($bodystyle[0]) . "'"; 
+    echo $query2;
   }
   print_r($capidList);
   print_r($specdatelist);
