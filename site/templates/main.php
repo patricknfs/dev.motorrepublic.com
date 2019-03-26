d4e55b5457f3e66ddf3490509306f81eb33dbcc3
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-03-26 14:26:50 +0000

    updating power search dropdowns

--- site/templates/main.php
+++ site/templates/main.php
@@ -141,7 +141,7 @@ print_r($_GET);
             }
             else if (id == 'bodystyle')
             {
-              if(value.id >= 500000)
+              if(value.parent_id == parent_id && value.id >= 500000)
               {
                 html_code += '<option value="'+value.id+'">'+value.name+'</option>';
               }
