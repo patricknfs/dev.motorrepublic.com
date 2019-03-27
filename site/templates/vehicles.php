da3ec6a992e1886408462bf94293946c43e750da
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-03-27 08:07:19 +0000

    updating power search dropdowns

--- site/templates/vehicles.php
+++ site/templates/vehicles.php
@@ -29,7 +29,7 @@ if ($page->marque->title != ''){
   $manuf = strtoupper($page->marque->title);
 }
 else {
-  $manuf = $sanitizer->text($input->get->marque);
+  $manuf = $sanitizer->text($input->get->manufacturer);
 }
 
 // echo "Manuf is: " . $manuf;
