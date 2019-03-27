58e1b5f5b59d684c3e1f6b9aa85f1f2a37c26ce5
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-03-27 09:37:00 +0000

    updating power search dropdowns

--- site/templates/main.php
+++ site/templates/main.php
@@ -143,14 +143,14 @@ if($page->id !== 1043){
             {
               if(value.parent_id == parent_id && value.id >= 500000)
               {
-                html_code += '<option value="'+value.name+'">'+value.name+'</option>';
+                html_code += '<option value="'+value.id+'">'+value.name+'</option>';
               }
             }
             else
             {
               if(value.parent_id == parent_id && value.id < 500000)
               {
-                html_code += '<option value="'+value.name+'">'+value.name+'</option>';
+                html_code += '<option value="'+value.id+'">'+value.name+'</option>';
               }
             }
           });
