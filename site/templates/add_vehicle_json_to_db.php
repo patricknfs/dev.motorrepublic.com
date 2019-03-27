0971067fc1f986c80099cdf844e0cdf04da00a5e
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-03-27 11:43:36 +0000

    upload add_vehicle_json_to_db.php

--- site/templates/add_vehicle_json_to_db.php
+++ site/templates/add_vehicle_json_to_db.php
@@ -2,17 +2,14 @@
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
 require_once MR_PATH . "/inc/conn.php";
 
-// Read JSON file
+$truncate = "TRUNCATE TABLE `team`.`vehicle_json`";
+$result2 = $conn->query($truncate) or die(mysqli_error());
+
 $json = file_get_contents('vehicle.json');
-
-//Decode JSON
 $json_data = json_decode($json,true);
-
-//Print data
-// print_r($json_data);
 
 foreach($json_data AS $vehicle_json)
 {
-  $query = "INSERT INTO team.vehicle_json (`json_id`,`name`,`parent_id`) VALUES ('$vehicle_json[id]','$vehicle_json[name]','$vehicle_json[parent_id]')";
+  $query = "INSERT INTO team.vehicle_json (`id`,`json_id`,`name`,`parent_id`) VALUES ('$vehicle_json[id]','$vehicle_json[name]','$vehicle_json[parent_id]')";
   $result = $conn->query($query);
 }
