688db216d7efd71b76a2de8a338ce9428214f937
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-03-27 08:16:05 +0000

    updating power search dropdowns

--- site/templates/vehicles.php
+++ site/templates/vehicles.php
@@ -15,7 +15,7 @@ date_default_timezone_set('CET');
 require_once '/var/www/vhosts/motorrepublic.com/dev.motorrepublic.com/site/templates/inc/config.php';
 require_once(MR_PATH . "/inc/conn.php");
 
-include "power_search.php";
+// include "power_search_2.php";
 
 if (isset($_GET['pageno'])) {
   $pageno = $_GET['pageno'];
