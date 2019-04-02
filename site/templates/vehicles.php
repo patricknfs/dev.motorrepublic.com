f1130aa8888500ea6e7041b2eccb0b08161e4b8b
Patrick O'Gorman <patrick.ogorman@nationalfleetservices.net>
2019-04-02 09:23:59 +0100

    styling selects

--- site/templates/vehicles.php
+++ site/templates/vehicles.php
@@ -128,7 +128,22 @@ elseif(!empty($bodystyle)) {
   $total_pages_sql = $conn->query("SELECT COUNT(*) FROM `team`.`rates_combined_terse` WHERE `bodystyle` LIKE '%" . $bodystyle . "%'  AND `lcv` = '0' AND `special` = 1");
   $total_rows = $total_pages_sql->fetch_row();
   $total_pages = ceil($total_rows[0] / $no_of_records_per_page);
-  $query = "SELECT `id`,`cap_id`,`cap_code`,`src`,`manufacturer`,`model`,`descr`,`term`,`mileage`, `rental`,`vehicle_list_price`,`vehicle_otr_price`,`p11d_price`,`CO2`, `lcv`, `special`, `biz_only` FROM `team`.`rates_combined_terse` WHERE `model` LIKE '%" . $mod . "%' AND `lcv` = '0' AND `special` = 1 GROUP BY `cap_id` ORDER BY `special` DESC, `rental` ASC LIMIT $offset, $no_of_records_per_page";
+  $query = "SELECT 
+  t1.*, t2.bodystyle
+  FROM
+    `team`.`rates_combined_terse` AS t1
+  INNER JOIN
+  `team`.`vehicles` AS t2
+  ON t1.cap_id = t2.cap_id
+  WHERE
+    t1.manufacturer = '" . $manuf . "'
+        AND t1.model LIKE '%" . $mod . "%'
+        AND t1.lcv = '0'
+        AND t1.special = 1 
+        AND t2.bodystyle LIKE '%" . $bodystyle . "%'
+  GROUP BY t1.cap_id
+  ORDER BY t1.special DESC , t1.rental ASC
+  LIMIT $offset, $no_of_records_per_page";";
   echo $query;
 }
 else {
